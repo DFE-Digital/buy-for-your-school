068c0f60f0b18b749e14836fdc61b726d301b90e
Tom Hipkin <tomhipkin@gmail.com>
2020-10-23 15:47:40 +0100

    The application is now named BuyForYourSchool

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,7 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-# TODO: Name the application
-module RailsTemplate
+module BuyForYourSchool
   class Application < Rails::Application
     config.generators do |g|
       g.test_framework :rspec,
