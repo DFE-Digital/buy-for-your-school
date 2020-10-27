e8be5343c7fad786e3eae53ba99f9831917c557b
Nick Jackson <nick@dxw.com>
2020-10-27 15:09:29 +0000

    Swap Bootstrap look and feel for GOV.UK Frontend

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,6 +7,13 @@ Rails.application.config.assets.version = "1.0"
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
+
+# Add the GOVUK Frontend images path
+Rails.application.config.assets.paths << Rails.root.join("node_modules", "govuk-frontend", "govuk", "assets", "images")
+
+# Add the GOVUK Frontend fonts path
+Rails.application.config.assets.paths << Rails.root.join("node_modules", "govuk-frontend", "govuk", "assets", "fonts")
+
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join("node_modules")
 
@@ -14,3 +21,17 @@ Rails.application.config.assets.paths << Rails.root.join("node_modules")
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+# Add GOVUK assets by name, these are assets not loaded via sass
+Rails.application.config.assets.precompile += [
+  "favicon.ico",
+  "govuk-apple-touch-icon-152x152.png",
+  "govuk-apple-touch-icon-167x167.png",
+  "govuk-apple-touch-icon-180x180.png",
+  "govuk-apple-touch-icon.png",
+  "govuk-crest-2x.png",
+  "govuk-crest.png",
+  "govuk-logotype-crown.png",
+  "govuk-mask-icon.svg",
+  "govuk-opengraph-image.png"
+]
