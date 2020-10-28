6d2e777f6b37d522c1531e21af8b8c17abe0f4c5
Tom Hipkin <tomhipkin@gmail.com>
2020-10-28 15:49:33 +0000

    Enable UUID for use in the database
    In order to use UUID as the primary key we need to enable this postgres extension.

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
 end
