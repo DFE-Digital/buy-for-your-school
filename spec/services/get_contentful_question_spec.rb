5e8d86b8c98de3d5764df760e035960d75fc9c5f
Tom Hipkin <tomhipkin@gmail.com>
2020-10-29 17:04:29 +0000

    Use climate_control to test Contentful client setup
    ..includes the values from the expected environment variables

--- spec/services/get_contentful_question_spec.rb
+++ spec/services/get_contentful_question_spec.rb
@@ -1,6 +1,15 @@
 require "rails_helper"
 
 RSpec.describe GetContentfulQuestion do
+  let(:contentful_space) { "abc" }
+  let(:contentful_access_token) { "123" }
+
+  around do |example|
+    ClimateControl.modify CONTENTFUL_SPACE: contentful_space, CONTENTFUL_ACCESS_TOKEN: contentful_access_token do
+      example.run
+    end
+  end
+
   describe "#call" do
     it "returns the contents of Contentful fixture (for now)" do
       raw_response = File.read("#{Rails.root}/spec/fixtures/contentful/radio-question-example.json")
@@ -8,7 +17,7 @@ RSpec.describe GetContentfulQuestion do
 
       contentful_client = instance_double(Contentful::Client)
       expect(Contentful::Client).to receive(:new)
-        .with(space: anything, access_token: anything)
+        .with(space: contentful_space, access_token: contentful_access_token)
         .and_return(contentful_client)
 
       contentful_response = double(Contentful::Entry, id: "1UjQurSOi5MWkcRuGxdXZS")
