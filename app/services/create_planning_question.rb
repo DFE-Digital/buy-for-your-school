75edf33310510e35ef108feeccc335537a193341
Tom Hipkin <tomhipkin@gmail.com>
2020-11-09 12:09:36 +0000

    Rename contentful_response variable to better reflect it's type
    It's now an object rather than a blob of json. Using `entry` as the suffix makes it clearer that this variable is an object of type `Contentful::Entry`.

--- app/services/create_planning_question.rb
+++ app/services/create_planning_question.rb
@@ -15,15 +15,15 @@ class CreatePlanningQuestion
     end
 
     question = Question.create(
-      title: contentful_response.title,
-      help_text: contentful_response.help_text,
-      contentful_type: contentful_response.type,
-      options: contentful_response.options,
-      raw: contentful_response.raw,
+      title: contentful_entry.title,
+      help_text: contentful_entry.help_text,
+      contentful_type: contentful_entry.type,
+      options: contentful_entry.options,
+      raw: contentful_entry.raw,
       plan: plan
     )
 
-    plan.update(next_entry_id: contentful_response.next.id)
+    plan.update(next_entry_id: contentful_entry.next.id)
 
     question
   end
@@ -31,17 +31,17 @@ class CreatePlanningQuestion
   private
 
   def content_entry_id
-    contentful_response.id
+    contentful_entry.id
   end
 
-  def contentful_response
-    @contentful_response ||= GetContentfulEntry
+  def contentful_entry
+    @contentful_entry ||= GetContentfulEntry
       .new(entry_id: plan.next_entry_id)
       .call
   end
 
   def content_type
-    contentful_response.content_type.id
+    contentful_entry.content_type.id
   end
 
   def expected_question_type?
