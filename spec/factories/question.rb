95c32187a77017d7b18c938527c2e15a2a15e966
Tom Hipkin <tomhipkin@gmail.com>
2020-11-09 15:55:54 +0000

    Fix question factory so it works out of the box
    The real response payload is pretty verbose. It's essentially the contents of a fixture. Arguably it won't be useful in the majority of factory usage so I've included a small snippet of json as a placeholder.

--- spec/factories/question.rb
+++ spec/factories/question.rb
@@ -2,6 +2,8 @@ FactoryBot.define do
   factory :question do
     title { "What is your favourite colour?" }
     help_text { "Choose the primary colour closest to your choice" }
+    raw { "{\"sys\":{}}" }
+
     association :plan, factory: :plan
 
     trait :radio do
