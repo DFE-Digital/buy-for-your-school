2c2493548a25c8756c6c2ebbff6896b7cbe7cbb0
Tom Hipkin <tomhipkin@gmail.com>
2020-11-11 10:29:20 +0000

    App can be configured to show a preview banner
    It replaces the beta banner to bring attention to the fact that it's not the main staging environment and only to be used for checking unpublished Contentful changes.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,17 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def custom_banner_tag_class
+    return "preview-tag" if ENV["CONTENTFUL_PREVIEW_APP"].eql?("true")
+  end
+
+  def banner_tag
+    return I18n.t("banner.preview.tag") if ENV["CONTENTFUL_PREVIEW_APP"].eql?("true")
+    I18n.t("banner.beta.tag")
+  end
+
+  def banner_message
+    return I18n.t("banner.preview.message") if ENV["CONTENTFUL_PREVIEW_APP"].eql?("true")
+    I18n.t("banner.beta.message")
+  end
 end
--- spec/features/visitors/anyone_can_see_the_service_banners_spec.rb
+++ spec/features/visitors/anyone_can_see_the_service_banners_spec.rb
@@ -5,4 +5,21 @@ feature "Users can see the service banners" do
     expect(page).to have_content(I18n.t("banner.beta.tag"))
     expect(page).to have_content(I18n.t("banner.beta.message"))
   end
+
+  context "when the app is configured as a Contenetful preview app" do
+    around do |example|
+      ClimateControl.modify(
+        CONTENTFUL_PREVIEW_APP: "true"
+      ) do
+        example.run
+      end
+    end
+
+    it "renders a preview banner" do
+      visit root_path
+
+      expect(page).to have_content(I18n.t("banner.preview.tag"))
+      expect(page).to have_content(I18n.t("banner.preview.message"))
+    end
+  end
 end
