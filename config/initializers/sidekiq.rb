5ca6924243eb097ad90c4abe9c5fc36187cb0728
Tom Hipkin <tomhipkin@gmail.com>
2020-12-03 11:34:37 +0000

    Sidekiq Cron installed to enable scheduled tasks
    Sidekiq can manage and process routine tasks in a similar way to Cron. The advantage to using Sidekiq for this now is that it makes it easier to extend the service with other async tasks. There's a good chance we'll need more of these and to then have to configure and look after a cron _and_ sidekiq process is more costly and introduces another box, which makes the overal architecture a little more complex.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,11 @@
 Sidekiq.configure_server do |config|
   config.redis = {url: ENV["REDIS_SIDEKIQ_URL"]}
+
+  # Sidekiq Cron
+  schedule_file = "config/schedule.yml"
+  if File.exist?(schedule_file)
+    Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
+  end
 end
 
 Sidekiq.configure_client do |config|
