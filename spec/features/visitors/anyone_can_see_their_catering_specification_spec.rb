da90fa71d43ca1791bbae92bfe1b8e30ec2e2df6
Tom Hipkin <tomhipkin@gmail.com>
2021-01-12 14:46:09 +0000

    Refactor stubbing of Liquid templates into resuable helper
    We'd like to use this in a feature test so extract this method into a mixin.

--- spec/features/visitors/anyone_can_see_their_catering_specification_spec.rb
+++ spec/features/visitors/anyone_can_see_their_catering_specification_spec.rb
@@ -1,6 +1,7 @@
 feature "Users can see their catering specification" do
   scenario "HTML" do
-    journey = create(:journey, :catering, liquid_template: stub_liquid_template)
+    liquid_template = stub_liquid_template(filename: "food_catering.liquid")
+    journey = create(:journey, :catering, liquid_template: liquid_template)
     step = create(:step, :long_text, long_text_answer: nil, journey: journey, contentful_id: "NxJWpbiFeEAmvcw17EysX")
     answer = create(:long_text_answer, step: step, response: "Red tractor")
 
@@ -14,16 +15,5 @@ feature "Users can see their catering specification" do
       expect(page).to have_content("The school also requires the service to comply with the following non-mandatory food standards or schemes:")
       expect(page).to have_content(answer.response)
     end
-  end
-
-  def stub_liquid_template
-    fake_liquid_template = File.read("#{Rails.root}/spec/fixtures/specification_templates/food_catering.liquid")
-
-    finder = instance_double(FindLiquidTemplate)
-    allow(FindLiquidTemplate).to receive(:new).with(category: "catering")
-      .and_return(finder)
-    allow(finder).to receive(:call).and_return(fake_liquid_template)
-
-    fake_liquid_template
   end
 end
