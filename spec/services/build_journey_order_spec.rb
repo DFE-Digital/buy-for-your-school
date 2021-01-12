66157e42ee39d98ae716fbeb50c4a2f9f475a032
Nick Jackson <nick@dxw.com>
2021-01-12 17:34:16 +0000

    Remove unnecessary test for order of entries in BuildJourneyOrder
    This test does not appear to be necessary; it is checking that the order
    of entries returned from `BuildJourneyOrder` matches the order of
    entries in the fixture, but the order of entries returned from
    Contentful is not guaranteed. This test appears to be checking that our
    fixture has entries in the order we would expect them to appear, and not
    that the application itself puts them in the right order.
    
    Co-authored-by: Lorna Harwood <lorna@dxw.com>

--- spec/services/build_journey_order_spec.rb
+++ spec/services/build_journey_order_spec.rb
@@ -41,21 +41,6 @@ RSpec.describe BuildJourneyOrder do
       end
     end
 
-    context "when there are multiple entries" do
-      it "returns each ID in the order they appear" do
-        fake_entries = fake_contentful_entry_array(
-          contentful_fixture_filename: "closed-path-with-multiple-example.json"
-        )
-
-        result = described_class.new(
-          entries: fake_entries,
-          starting_entry_id: "5kZ9hIFDvNCEhjWs72SFwj"
-        ).call
-
-        expect(result).to match([fake_entries.first, fake_entries.last])
-      end
-    end
-
     context "when the journey visits the same node twice" do
       around do |example|
         ClimateControl.modify(
