2eafa3104619031d6ba3d6260000a32b39303a20
Tom Hipkin <tomhipkin@gmail.com>
2021-01-15 10:44:46 +0000

    Refactor edit question spec to allow for different contexts
    We have a test for `short_text_answer` which works great but we managed to ship a bug where checkboxes were not editable so I'm adding some more coverage. Before I do I shift the existing test into a context block so the new test can be added clearly.

--- spec/features/visitors/anyone_can_edit_their_answers_spec.rb
+++ spec/features/visitors/anyone_can_edit_their_answers_spec.rb
@@ -2,6 +2,7 @@ require "rails_helper"
 
 feature "Users can edit their answers" do
   let(:answer) { create(:short_text_answer, response: "answer") }
+
   scenario "The journey summary page displays a change link" do
     visit journey_path(answer.step.journey)
 
@@ -9,16 +10,38 @@ feature "Users can edit their answers" do
     expect(page).to have_content("Change")
   end
 
-  scenario "The edited answer is saved" do
-    visit journey_path(answer.step.journey)
+  context "when the question is short_text" do
+    let(:answer) { create(:short_text_answer, response: "answer") }
 
-    click_on(I18n.t("generic.button.change_answer"))
+    scenario "The edited answer is saved" do
+      visit journey_path(answer.step.journey)
 
-    fill_in "answer[response]", with: "email@example.com"
+      click_on(I18n.t("generic.button.change_answer"))
 
-    click_on(I18n.t("generic.button.update"))
+      fill_in "answer[response]", with: "email@example.com"
 
-    expect(page).to have_content("email@example.com")
+      click_on(I18n.t("generic.button.update"))
+
+      expect(page).to have_content("email@example.com")
+    end
+  end
+
+  context "when the question is single_date" do
+    let(:answer) { create(:single_date_answer, response: 1.year.ago) }
+
+    scenario "The edited answer is saved" do
+      visit journey_path(answer.step.journey)
+
+      click_on(I18n.t("generic.button.change_answer"))
+
+      fill_in "answer[response(3i)]", with: "12"
+      fill_in "answer[response(2i)]", with: "8"
+      fill_in "answer[response(1i)]", with: "2020"
+
+      click_on(I18n.t("generic.button.update"))
+
+      expect(page).to have_content("12 Aug 2020")
+    end
   end
 
   context "An error is thrown" do
