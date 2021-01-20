dd65a8721a3ec30b69b0485699f1d3d7bcafe18e
Nick Jackson <nick@dxw.com>
2021-01-20 13:46:40 +0000

    Previewing a question now specifies a liquid_template
    `liquid_template` is a required part of a `Journey` which was not being
    provided whilst previewing a question, leading to the `Journey` not
    being persisted and a redirect to the step within the journey being
    impossible.
    
    This commit adds a very short stub template to preview journeys.
    
    It also adds a feature spec to ensure that a user attempting to preview
    a step does reach the intended view.

---
+++ spec/features/visitors/anyone_can_preview_a_journey_step_spec.rb
@@ -0,0 +1,14 @@
+feature "Users can preview a journey step" do
+  scenario "the appropriate step is displayed" do
+    stub_get_contentful_entry(
+      entry_id: "contentful-radio-question",
+      fixture_filename: "radio-question-example.json"
+    )
+
+    visit preview_entry_path("contentful-radio-question")
+
+    expect(page).to have_content("Which service do you need?")
+    expect(page).to have_content("Catering")
+    expect(page).to have_content("Cleaning")
+  end
+end
