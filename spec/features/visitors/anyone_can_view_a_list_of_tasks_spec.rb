c11abc3f8762ade8292018f93f78f1adf0fa0e92
Nick Jackson <nick@dxw.com>
2021-01-26 16:09:35 +0000

    Improve test coverage in task list
    We did not previously test that an unstarted task would be displayed as
    such; we now do.

--- spec/features/visitors/anyone_can_view_a_list_of_tasks_spec.rb
+++ spec/features/visitors/anyone_can_view_a_list_of_tasks_spec.rb
@@ -1,6 +1,16 @@
 require "rails_helper"
 
 feature "Users can view the task list" do
+  context "When a question has not been answered" do
+    let(:step) { create(:step, :short_text) }
+
+    scenario "The task is marked as not started" do
+      visit journey_path(step.journey)
+
+      expect(page).to have_content(I18n.t("task_list.status.not_started"))
+    end
+  end
+
   context "When a question has been answered" do
     let(:answer) { create(:short_text_answer, response: "answer") }
 
