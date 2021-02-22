1de417b785ae232b2a02ef37b1eaf6ae7fba882e
Tom Hipkin <tomhipkin@gmail.com>
2021-02-22 11:29:57 +0000

    Multiple optional questions can be shown and hidden
    Before this change we allowed the content user to "bolt on" one additional question if the answer matched an expected pattern.
    
    This change allows them to show as many additional questions as they like. Though the current need is for 2 additonal questions. We do this by accepting an array of identifiers rather than a single string.
    
    The amount of calculation required to determine and follow the question branches through to their resolution is not ideal and we know this. A better approach would be to keep track of the state of the tree of questions and make the required database updates with fewer database queries. This would require more time than we have in the last sprint so we are opting for something that works and accepting the technical debt will need to be repaid.

--- app/services/toggle_additional_steps.rb
+++ app/services/toggle_additional_steps.rb
@@ -26,12 +26,12 @@ class ToggleAdditionalSteps
     step.additional_step_rule
   end
 
-  def additional_step_id
+  def additional_step_ids
     additional_step_rule["question_identifier"]
   end
 
   def additional_step
-    journey_steps.find_by(contentful_id: additional_step_id)
+    journey_steps.find_by(contentful_id: additional_step_ids)
   end
 
   def matching_answer?(a:, b:)
@@ -45,41 +45,43 @@ class ToggleAdditionalSteps
   end
 
   def check_to_show_additional_steps!
-    recursively_show_additional_steps!(current_step: step, next_step_id: additional_step_id)
+    recursively_show_additional_steps!(current_step: step, next_step_ids: additional_step_ids)
   end
 
   def check_to_hide_additional_steps!
     return if matching_answer?(a: step.additional_step_rule["required_answer"], b: step.answer.response)
 
-    recursively_hide_additional_steps!(current_step: step, next_step_id: additional_step_id)
+    recursively_hide_additional_steps!(next_step_ids: additional_step_ids)
   end
 
-  def recursively_hide_additional_steps!(current_step:, next_step_id:)
-    if next_step_id
-
-      next_step = journey_steps.find_by(contentful_id: next_step_id)
-      next_step.update(hidden: true)
+  def recursively_hide_additional_steps!(next_step_ids:)
+    if next_step_ids.compact.present?
+      next_steps = journey_steps.where(contentful_id: next_step_ids)
+      next_steps.update_all(hidden: true)
 
       recursively_hide_additional_steps!(
-        current_step: next_step,
-        next_step_id: next_step.additional_step_rule&.fetch("question_identifier", nil)
+        next_step_ids: next_steps.map { |next_step|
+          next_step.additional_step_rule&.fetch("question_identifier", nil)
+        }.flatten.compact
       )
     end
   end
 
-  def recursively_show_additional_steps!(current_step:, next_step_id:)
+  def recursively_show_additional_steps!(current_step:, next_step_ids:)
     return unless current_step.answer && current_step.additional_step_rule
 
-    if next_step_id
+    if next_step_ids
       return unless matching_answer?(a: current_step.additional_step_rule["required_answer"], b: current_step.answer.response)
 
-      next_step = journey_steps.find_by(contentful_id: next_step_id)
-      next_step.update(hidden: false)
+      next_steps = journey_steps.where(contentful_id: next_step_ids)
+      next_steps.update_all(hidden: false)
 
-      recursively_show_additional_steps!(
-        current_step: next_step,
-        next_step_id: next_step.additional_step_rule&.fetch("question_identifier", nil)
-      )
+      next_steps.map { |next_step|
+        recursively_show_additional_steps!(
+          current_step: next_step,
+          next_step_ids: next_step.additional_step_rule&.fetch("question_identifier", nil)
+        )
+      }
     end
   end
 end
