64ff6f4d027377b8f6967ea32b97a16480137194
Tom Hipkin <tomhipkin@gmail.com>
2021-03-08 12:16:21 +0000

    Checkbox answers are not automatically joined by a comma
    Instead of performing this operation in Rails, we leave the presentational logic to Contentful.
    
    The same logic can be achieved by defining the following in the `specification_template`:
    
    ```
    {{ options | join: ", " }}
    ```
    
    This enables the user to present the options in different ways. Not only as a single paragraph but as a list of options.

--- spec/presenters/checkboxes_answer_presenter_spec.rb
+++ spec/presenters/checkboxes_answer_presenter_spec.rb
@@ -2,10 +2,10 @@ require "rails_helper"
 
 RSpec.describe CheckboxesAnswerPresenter do
   describe "#response" do
-    it "returns all none nil values, capitalised as a comma separated string" do
-      step = build(:checkbox_answers, response: ["yes", "no", "morning_break", ""])
+    it "returns all none nil values as an array" do
+      step = build(:checkbox_answers, response: ["Yes", "No", "Morning break", ""])
       presenter = described_class.new(step)
-      expect(presenter.response).to eq("Yes, No, Morning break")
+      expect(presenter.response).to eq(["Yes", "No", "Morning break"])
     end
   end
 end
