f38ae2dc5522285e6864b09203018830e61c92d0
Tom Hipkin <tomhipkin@gmail.com>
2021-03-09 14:42:05 +0000

    Number presenter returns the number as a string

--- app/presenters/number_answer_presenter.rb
+++ app/presenters/number_answer_presenter.rb
@@ -1,4 +1,8 @@
 class NumberAnswerPresenter < SimpleDelegator
+  def response
+    super.to_s
+  end
+  
   def to_param
     response
   end
--- spec/presenters/number_answer_presenter_spec.rb
+++ spec/presenters/number_answer_presenter_spec.rb
@@ -2,10 +2,12 @@ require "rails_helper"
 
 RSpec.describe NumberAnswerPresenter do
   describe "#response" do
-    it "returns the response" do
+    it "returns the response as a string" do
       step = build(:number_answer, response: 100)
       presenter = described_class.new(step)
-      expect(presenter.response).to eq(100)
+      expect(presenter.response).to eq("100")
+    end
+  end
     end
   end
 end
