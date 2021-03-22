52f61de00b6ef6af6763ef3dd964e8316d2482be
Tom Hipkin <tomhipkin@gmail.com>
2021-03-22 16:23:41 +0000

    Turn on protect_from_forgery to enable CSRF protection
    Unsure why this wasn't already there by default. The service should only accept signed requests from itself or from whitelisted hosts.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@
 class ApplicationController < ActionController::Base
   default_form_builder GOVUKDesignSystemFormBuilder::FormBuilder
 
+  before_action :authenticate_user!, except: :health_check
+
+  protect_from_forgery
+
   def health_check
     render json: {rails: "OK"}, status: :ok
   end
