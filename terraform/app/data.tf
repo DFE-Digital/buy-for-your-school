dd7d10e9bfa4a79a48317651f00b984608a792be
Tom Hipkin <tomhipkin@gmail.com>
2021-03-23 16:03:37 +0000

    (WIP) Terraform Infrastructure

---
+++ terraform/app/data.tf
@@ -0,0 +1,20 @@
+data "cloudfoundry_org" "org" {
+  name = local.cloudfoundry_org
+}
+
+data "cloudfoundry_domain" "shared" {
+  name = local.shared_cloudfoundry_domain
+}
+
+data "cloudfoundry_service" "redis" {
+  name = "redis"
+}
+
+data "cloudfoundry_service" "postgres" {
+  name = "postgres"
+}
+
+data "cloudfoundry_space" "space" {
+  name = local.cloudfoundry_space
+  org  = data.cloudfoundry_org.org.id
+}
