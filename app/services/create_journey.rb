uk-gov-mirror/DFE-Digital.buy-for-your-school

Fetch Tasks from Contentful & create them in the database
When creating a new Journey and fetching the Sections & Steps from Contentful,
also fetch any Tasks associated with a Section and create them in the database.

Included in this commit is the new Task model, and a CreateTask service.

app/services/create_journey.rb
@@ -31,6 +31,13 @@ class CreateJourney
       end
     end
 
+    contentful_sections.each do |section|
+      tasks = GetTasksFromSection.new(section: section).call
+      tasks.each do |task|
+        CreateTask.new(section: section, contentful_task: task)
+      end
+    end
+
     journey
   end
 

DFE-Digital/buy-for-your-school

app/services/create_journey.rb
@@ -22,14 +22,17 @@ class CreateJourney
 
     contentful_sections.each do |contentful_section|
       CreateSection.new(journey: journey, contentful_section: contentful_section).call
-    end
 
-    contentful_sections.each do |section|
-      question_entries = GetStepsFromSection.new(section: section).call
+      question_entries = GetStepsFromSection.new(section: contentful_section).call
       question_entries.each do |entry|
         CreateJourneyStep.new(
           journey: journey, contentful_entry: entry
         ).call
+      end
+
+      tasks = GetTasksFromSection.new(section: contentful_section).call
+      tasks.each do |task|
+        CreateTask.new(section: contentful_section, contentful_task: task)
       end
     end
 