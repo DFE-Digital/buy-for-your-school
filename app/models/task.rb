fbacbde162b0a5069db5fe094450a59449aab36e
Nick Jackson <nick@dxw.com>
2021-04-27 14:42:33 +0100

    Add display of task completion to task list
    This adds display of "not started", "in progress" or "completed" labels
    to task groups on the task list.

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,11 +5,45 @@ class Task < ApplicationRecord
 
   validates :title, :contentful_id, presence: true
 
+  attr_accessor
+
+  NOT_STARTED = 0
+  IN_PROGRESS = 1
+  COMPLETED = 2
+
   def visible_steps
     steps.where(hidden: false)
   end
 
+  def visible_steps_count
+    visible_steps.count
+  end
+
   def has_single_visible_step?
-    visible_steps.count == 1
+    visible_steps_count == 1
+  end
+
+  def answered_questions_count
+    visible_steps.includes([
+      :short_text_answer,
+      :long_text_answer,
+      :radio_answer,
+      :checkbox_answers,
+      :currency_answer,
+      :number_answer,
+      :single_date_answer
+    ]).count { |step| step.answered? }
+  end
+
+  def status
+    if answered_questions_count == visible_steps_count
+      return COMPLETED
+    end
+
+    if answered_questions_count > 0
+      return IN_PROGRESS
+    end
+
+    NOT_STARTED
   end
 end
