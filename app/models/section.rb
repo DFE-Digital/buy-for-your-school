1fe6704562ca8135209489a9c4a8babbef11e27b
Tom Hipkin <tomhipkin@gmail.com>
2021-05-10 15:07:16 +0100

    Sections are ordered by created_at ASC
    It turns out that `self.implicit_order_column` is only sorting by `created_at` (over `id`) when `first` or `last` methods are used. If we use `all` or other, then `ORDER` is not used in the SQL query. This change corrects this.

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,5 +1,7 @@
 class Section < ApplicationRecord
   self.implicit_order_column = "created_at"
+  default_scope { order('created_at ASC') }
+
   belongs_to :journey
   has_many :tasks, dependent: :destroy
   has_many :steps, through: :tasks, class_name: "Step"
--- spec/models/section_spec.rb
+++ spec/models/section_spec.rb
@@ -7,4 +7,18 @@ RSpec.describe Section, type: :model do
     it { is_expected.to validate_presence_of(:title) }
     it { is_expected.to validate_presence_of(:contentful_id) }
   end
+
+  describe "default_order" do
+    it "orders by created_at ASC" do
+      oldest_section = create(:section, created_at: 3.days.ago)
+      middle_aged_section = create(:section, created_at: 2.days.ago)
+      youngest_section = create(:section, created_at: 1.days.ago)
+
+      result = described_class.all
+
+      expect(result.first).to eq(oldest_section)
+      expect(result.second).to eq(middle_aged_section)
+      expect(result.third).to eq(youngest_section)
+    end
+  end
 end
