c1753fbb3fa553936f5b67ff8ff9827403b0c00f
Peter David Hamilton <pete@peterdavidhamilton.com>
2021-06-29 08:57:49 +0100

    Add class comment to ApplicationRecord

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# Abstract base class for all persisted models
+#
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
   self.implicit_order_column = "created_at"
