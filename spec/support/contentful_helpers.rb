47179f20bd4506c8683a3d3688cc420ad2d9b919
Edwin Kruglov <edwin.kruglov@education.gov.uk>
2021-07-23 10:39:56 +0100

    Make changes as per feedback

--- spec/support/contentful_helpers.rb
+++ spec/support/contentful_helpers.rb
@@ -182,17 +182,15 @@ module ContentfulHelpers
   def fake_contentful_step(contentful_fixture_filename:)
     raw_response = File.read(Rails.root.join("spec/fixtures/contentful/004-#{contentful_fixture_filename}"))
     hash_response = JSON.parse(raw_response)
-    transformed_response = hash_response.deep_transform_keys(&:underscore).deep_symbolize_keys
 
     double(
       Contentful::Entry,
-      id: transformed_response.dig(:sys, :id),
-      title: transformed_response.dig(:fields, :title),
-      type: transformed_response.dig(:fields, :title),
-      next: double(id: transformed_response.dig(:fields, :next, :sys, :id)),
+      id: hash_response.dig("sys", "id"),
+      title: hash_response.dig("fields", "title"),
+      type: hash_response.dig("fields", "type"),
+      content_type: double(id: hash_response.dig("sys", "contentType", "sys", "id")),
       raw: hash_response,
-      content_type: double(id: transformed_response.dig(:sys, :content_type, :sys, :id)),
-      fields: transformed_response[:fields],
+      fields: hash_response["fields"].dup.deep_transform_keys!(&:underscore).deep_symbolize_keys!,
     )
   end
 end
