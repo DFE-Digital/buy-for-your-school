4352129e3e5e913588524fec9666ba7ec5b6feb2
Peter David Hamilton <pete@peterdavidhamilton.com>
2021-07-30 16:18:30 +0100

    Add missing data migrations and spec coverage required to update existing journeys
    - establish pattern for migration specs using shared context
    - ensure migrated Journeys are associated to the "Catering" category
    - populate Task `step_tally` and `order`

---
+++ db/migrate/20210729145738_associate_existing_journeys.rb
@@ -0,0 +1,32 @@
+class AssociateExistingJourneys < ActiveRecord::Migration[6.1]
+  def up
+    return if Journey.where(category_id: nil).none? && Category.one?
+
+    journeys_total = Journey.count
+    journeys_updated = Journey.where(category_id: nil).count
+
+    catering_entry = ContentfulConnector.new.by_slug("category", "catering")
+    contentful_category = GetCategory.new(category_entry_id: catering_entry.id).call
+
+    Category.create!(
+      title: contentful_category.title,
+      description: contentful_category.description,
+      liquid_template: contentful_category.combined_specification_template,
+      contentful_id: contentful_category.id,
+    )
+
+    catering = Category.where(contentful_id: catering_entry.id).first
+
+    Journey.where(category_id: nil).update_all(category_id: catering.id)
+
+    Category.reset_counters(catering.id, :journeys)
+
+    Rollbar.info("Migration: Journeys associated to a category",
+                 journeys_total: journeys_total,
+                 journeys_updated: journeys_updated,
+                 contentful_category_id: catering_entry.id,
+                 contentful_category_title: catering_entry.title)
+  end
+
+  def down; end
+end
---
+++ spec/migrations/associate_existing_journeys_spec.rb
@@ -0,0 +1,35 @@
+require Rails.root.join("db/migrate/20210729145738_associate_existing_journeys.rb")
+
+RSpec.describe AssociateExistingJourneys do
+  include_context "with data migrations"
+
+  let(:previous_version) { 20_210_729_144_528 }
+  let(:current_version) { 20_210_729_145_738 }
+
+  context "when there are broken journeys and no category" do
+    before do
+      create_list(:journey, 10)
+      Category.delete_all
+      Journey.update_all(category_id: nil)
+      stub_contentful_category(fixture_filename: "journey-with-multiple-entries.json")
+    end
+
+    it "creates and associates to the catering category" do
+      expect(Journey.count).to eq 10
+      expect(Category.count).to eq 0
+      expect(Journey.first.category_id).to eq nil
+
+      expect(Rollbar).to receive(:info)
+          .with("Migration: Journeys associated to a category",
+                journeys_total: 10,
+                journeys_updated: 10,
+                contentful_category_id: "contentful-category-entry",
+                contentful_category_title: "Catering")
+
+      expect { up }.to change(Category, :count).from(0).to(1)
+
+      expect(Category.first.title).to eq "Catering"
+      expect(Journey.first.category_id).not_to eq nil
+    end
+  end
+end
