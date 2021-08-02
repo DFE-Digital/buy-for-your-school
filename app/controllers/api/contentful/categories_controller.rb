b816360b47a688de8a5eb1992ab2c2752df056ee
Andrew Cowle <acowle@byondsoftware.co.uk>
2021-08-02 15:59:23 +0100

    Help with test

--- app/controllers/api/contentful/categories_controller.rb
+++ app/controllers/api/contentful/categories_controller.rb
@@ -4,6 +4,7 @@ class Api::Contentful::CategoriesController < Api::Contentful::BaseController
   def changed
     contentful_category = GetCategory.new(category_entry_id: category_params[:id]).call
 
+
     category = Category.find_or_create_by!(contentful_id: contentful_category.id) do |cat|
       cat.title = contentful_category.title
       cat.description = contentful_category.description
--- spec/requests/api/contentful/webhook_upsert_category_spec.rb
+++ spec/requests/api/contentful/webhook_upsert_category_spec.rb
@@ -7,21 +7,36 @@ RSpec.describe "Webhook upserts category", type: :request do
     end
   end
 
-  let!(:fake_contentful_webook_payload) do
-    {
-      entityId: "6zeSz4F4YtD66gT5SFpnSB",
-      spaceId: "rwl7tyzv9sys",
-      parameters: {
-        text: "Entity version: 62",
-      },
-    }
+
+  let!(:contentful_category_stub) { stub_contentful_category(fixture_filename: "category-with-dynamic-liquid-template.json") }
+
+  before do
+    allow(contentful_category).to receive(:new).and_return(contentful_category_stub)
   end
 
   it "creates a new category" do
-    # TODO: create new cat from params
+
+    credentials = ActionController::HttpAuthentication::Token
+                    .encode_credentials(ENV["CONTENTFUL_WEBHOOK_API_KEY"])
+    headers = { "AUTHORIZATION" => credentials }
+
+
+    fake_contentful_webhook_payload = {
+      sys: {
+        id: "contentful-category-entry",
+      },
+    }
+
+    post "/api/contentful/category",
+         params: fake_contentful_webhook_payload,
+         headers: headers,
+         as: :json
+
+    ## TODO ensure controller var 'contentful_category' is stubbed with contentful_category_stub
+    ## check that Category is present with Catertering title
+    expect(response).to have_http_status(:ok)
   end
-  
-  it "updates an existing category" do
-    # TODO: update a new cat from params
-  end
+
+
+
 end
