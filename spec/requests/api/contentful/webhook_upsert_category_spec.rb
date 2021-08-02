001a78a036e3c5660e0b78a5ce78948d66c25bd0
Peter David Hamilton <pete@peterdavidhamilton.com>
2021-08-02 18:12:49 +0100

    Just fire in the before block

--- spec/requests/api/contentful/webhook_upsert_category_spec.rb
+++ spec/requests/api/contentful/webhook_upsert_category_spec.rb
@@ -1,18 +1,19 @@
 RSpec.describe "Webhook upserts category", type: :request do
 
-  let!(:contentful_category_stub) { stub_contentful_category(fixture_filename: "category-with-dynamic-liquid-template.json") }
+  around do |example|
+    ClimateControl.modify(CONTENTFUL_WEBHOOK_API_KEY: "an API key") do
+      example.run
+    end
+  end
 
   before do
-    controller.stub!(authenticate_api_user!: true)
-    allow(contentful_category).to receive(:new).and_return(contentful_category_stub)
+    stub_contentful_category(fixture_filename: "category-with-dynamic-liquid-template.json")
   end
 
   it "creates a new category" do
-
     credentials = ActionController::HttpAuthentication::Token
                     .encode_credentials(ENV["CONTENTFUL_WEBHOOK_API_KEY"])
     headers = { "AUTHORIZATION" => credentials }
-
 
     fake_contentful_webhook_payload = {
       sys: {
@@ -20,16 +21,15 @@ RSpec.describe "Webhook upserts category", type: :request do
       },
     }
 
+    expect(Category.count).to be_zero
+
     post "/api/contentful/category",
          params: fake_contentful_webhook_payload,
          headers: headers,
          as: :json
 
-    ## TODO ensure controller var 'contentful_category' is stubbed with contentful_category_stub
-    ## check that Category is present with Catertering title
     expect(response).to have_http_status(:ok)
+
+    expect(Category.first.title).to eql "Catering"
   end
-
-
-
 end
