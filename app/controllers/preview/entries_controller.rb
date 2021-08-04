18fb831f27576a8053d2bfd8725932ad8673c3f1
Peter David Hamilton <pete@peterdavidhamilton.com>
2021-08-04 15:14:07 +0100

    Fix shared preview journey and add missing spec context for multiple designers

--- app/controllers/preview/entries_controller.rb
+++ app/controllers/preview/entries_controller.rb
@@ -1,34 +1,33 @@
 class Preview::EntriesController < ApplicationController
   def show
-    client = Content::Client.new(:preview)
-
     contentful_step = GetEntry.new(entry_id: params[:id], client: client).call
 
-    category = Category.find_or_create_by!(title: "Designer Preview Category") do |c|
-      c.contentful_id = 0
-      c.description = "Used to demo a step before publishing"
-      c.liquid_template = "<p>N/A</p>"
-    end
+    category = Category.find_or_create_by!(
+      title: "Designer Preview Category",
+      contentful_id: 0,
+      description: "Used to demo a step before publishing",
+      liquid_template: "<p>N/A</p>",
+    )
 
-    # flagged for deletion
-    journey = Journey.find_or_create_by!(category: category) do |j|
-      j.user = current_user
-      j.state = 3
-    end
+    journey = Journey.find_or_create_by!(
+      category: category,
+      user: current_user,
+      state: 3, # flagged for deletion
+    )
 
-    section = Section.find_or_create_by!(title: "Designer Preview Section") do |s|
-      s.contentful_id = 0
-      s.order = 0
-      s.journey = journey
-    end
+    section = Section.find_or_create_by!(
+      title: "Designer Preview Section",
+      contentful_id: 0,
+      order: 0,
+      journey: journey,
+    )
 
-    task = Task.find_or_create_by!(title: "Designer Preview Task") do |t|
-      t.contentful_id = 0
-      t.order = 0
-      t.section = section
-      # TODO: add migration for step_tally to default to an empty Hash
-      t.step_tally = {}
-    end
+    task = Task.find_or_create_by!(
+      title: "Designer Preview Task",
+      contentful_id: 0,
+      order: 0,
+      section: section,
+    )
 
     step = CreateStep.new(
       task: task,
@@ -37,5 +36,11 @@ class Preview::EntriesController < ApplicationController
     ).call
 
     redirect_to journey_step_path(journey, step, preview: true)
+  end
+
+private
+
+  def client
+    Content::Client.new(:preview)
   end
 end
