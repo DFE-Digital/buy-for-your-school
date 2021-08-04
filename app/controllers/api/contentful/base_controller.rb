f4e30fa6d7feb75c311e7263282290d2a32f06c7
Andrew Cowle <7682746+AndrewCowle@users.noreply.github.com>
2021-08-04 10:58:23 +0100

    Incoming webhook to upsert category publish via contentful (#448)
    - Added webhook to receive category publish event

---
+++ app/controllers/api/contentful/base_controller.rb
@@ -0,0 +1,18 @@
+class Api::Contentful::BaseController < ApplicationController
+  before_action :authenticate_api_user!
+
+  skip_before_action :authenticate_user!
+  skip_before_action :verify_authenticity_token
+
+  def auth
+    render json: { status: "OK" }, status: :ok
+  end
+
+private
+
+  def authenticate_api_user!
+    authenticate_or_request_with_http_token do |token, _options|
+      token == ENV["CONTENTFUL_WEBHOOK_API_KEY"]
+    end
+  end
+end
--- app/controllers/api/contentful/entries_controller.rb
+++ app/controllers/api/contentful/entries_controller.rb
@@ -1,9 +1,6 @@
-class Api::Contentful::EntriesController < ApplicationController
-  before_action :authenticate_api_user!
+# frozen_string_literal: true
 
-  skip_before_action :authenticate_user!
-  skip_before_action :verify_authenticity_token
-
+class Api::Contentful::EntriesController < Api::Contentful::BaseController
   def changed
     Rollbar.info("Accepted request to cache bust Contentful Entry", cache_key: cache_key)
 
@@ -19,11 +16,5 @@ private
 
   def changed_params
     params.permit("entityId")
-  end
-
-  def authenticate_api_user!
-    authenticate_or_request_with_http_token do |token, _options|
-      token == ENV["CONTENTFUL_WEBHOOK_API_KEY"]
-    end
   end
 end
