5fa331b95494c4b821390c964755e0f79acb1bd0
Peter David Hamilton <pete@peterdavidhamilton.com>
2021-08-05 08:50:28 +0100

    Fix sign out bug introduced by addition of rails_ujs
    - change session destroy route to delete verb
    - rename session routes helpers
    - refactor session service objects and controllers
    - clean related specs
    - add feature spec for signing out to capture future bugs

--- spec/features/landing/body_spec.rb
+++ spec/features/landing/body_spec.rb
@@ -1,11 +1,28 @@
-RSpec.feature "Users can see a start page for specifying their purchase" do
+RSpec.feature "Users can see a start page" do
   before do
     visit "/"
   end
 
-  scenario "Start page content is shown on the root path" do
-    expect(find("h1.govuk-heading-xl")).to have_text "Create a specification to procure something for your school"
+  it "has a start button to login" do
+    within "main.govuk-main-wrapper" do
+      expect(find("form.button_to")["action"]).to eql "/auth/dfe"
+      # generic.button.start
+      expect(page).to have_button "Start", class: "govuk-button"
+    end
+  end
 
+  it "has a heading" do
+    expect(find("h1.govuk-heading-xl")).to have_text "Create a specification to procure something for your school"
+  end
+
+  it "links to catering procurements" do
+    expect(page).to have_link("procuring a new catering service", href: "https://buy-for-your-school-prototypes.herokuapp.com/beta/phase-5/catering")
+  end
+
+  # Who this service is for
+  # How this service works
+  # TODO: replace with raw strings and find tags
+  scenario do
     I18n.t("specifying.start_page.overview_body").each do |paragraph|
       expect(page).to have_content(paragraph)
     end
@@ -14,42 +31,29 @@ RSpec.feature "Users can see a start page for specifying their purchase" do
     expect(page).to have_content(I18n.t("specifying.start_page.who_for_can_use_body"))
 
     expect(page).to have_content("are responsible for procuring a new catering service for a school")
-    expect(page).to have_link("procuring a new catering service", href: "https://buy-for-your-school-prototypes.herokuapp.com/beta/phase-5/catering")
+
     expect(page).to have_content(I18n.t("specifying.start_page.who_for_can_use_list")[1])
     expect(page).to have_content(I18n.t("specifying.start_page.who_for_can_use_list")[2])
 
     expect(page).to have_content(I18n.t("specifying.start_page.who_for_cannot_use_body"))
+
     I18n.t("specifying.start_page.who_for_cannot_use_list").each do |list_item|
       expect(page).to have_content(list_item)
     end
 
     expect(page).to have_content(I18n.t("specifying.start_page.how_service_works_title"))
     expect(page).to have_content(I18n.t("specifying.start_page.how_service_works_document_body"))
+
     I18n.t("specifying.start_page.how_service_works_document_list").each do |list_item|
       expect(page).to have_content(list_item)
     end
+
     expect(page).to have_content(I18n.t("specifying.start_page.how_service_works_themes_body"))
+
     I18n.t("specifying.start_page.how_service_works_themes_list").each do |list_item|
       expect(page).to have_content(list_item)
     end
 
     expect(page).to have_content(I18n.t("specifying.start_page.pause_and_resume_body"))
-  end
-
-  specify "the start button leads to the dashboard" do
-    within "main.govuk-main-wrapper" do
-      expect(find("form.button_to")["action"]).to eql "/auth/dfe"
-      # generic.button.start
-      expect(page).to have_button "Start", class: "govuk-button"
-
-      click_start
-    end
-
-    # FIXME: DfE Sign In should appear here
-
-    # dashboard.header
-    expect(find("h1.govuk-heading-xl")).to have_content "Specifications dashboard"
-
-    expect(page).to have_current_path "/dashboard"
   end
 end
