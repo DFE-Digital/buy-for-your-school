6abcc41f6ba0974cd38192b1442118754af45410
Edwin Kruglov <edwin.kruglov@education.gov.uk>
2021-08-23 17:22:54 +0100

    Add navigation fix and test

--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -74,6 +74,9 @@ class StepsController < ApplicationController
     journey = current_journey
     @step = Step.find(params[:id])
 
+    # go back to the task page if this is the last skipped step
+    return redirect_to journey_task_path(journey, parent_task) if @step.id == parent_task.skipped_ids.last
+
     @step.skip!
 
     # allow the user to skip a step and come back to it later
@@ -82,16 +85,13 @@ class StepsController < ApplicationController
     if parent_task.has_single_visible_step?
       # return to the journey page if we only have one step
       redirect_to journey_path(journey, anchor: @step.id)
+    elsif parent_task.steps.last == @step
+      # return to the task page if we're on the last step
+      redirect_to journey_task_path(journey, parent_task)
     elsif parent_task.all_unanswered_questions_skipped?
-      # all the unanswered questions have been skipped
-      # cycle through the skipped questions
-      # or return to the task page if there is only one skipped quesiton left
+      # go to the next skipped step if all steps have been skipped
       next_step_id = parent_task.next_skipped_id(@step.id)
-      if next_step_id.nil?
-        redirect_to journey_task_path(journey, parent_task)
-      else
-        redirect_to journey_step_path(journey, next_step_id)
-      end
+      next_step_id ? redirect_to(journey_step_path(journey, next_step_id)) : redirect_to(journey_task_path(journey, parent_task))
     else
       # continue to the next incomplete step
       redirect_to journey_step_path(journey, parent_task.next_incomplete_step_id)
