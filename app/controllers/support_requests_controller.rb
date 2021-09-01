5ec5fad1b2a28fe55e609bd067e60d5e9aaa63bd
Andrew Cowle <acowle@byondsoftware.co.uk>
2021-09-01 13:36:37 +0100

    Wizard methods to controller

--- app/controllers/support_requests_controller.rb
+++ app/controllers/support_requests_controller.rb
@@ -38,4 +38,20 @@ private
       :journey_id, :category_id, :message
     ).merge(user: current_user)
   end
+
+  def load_support_request_wizard
+    @support_request_wizard = support_wizard_for_step(action_name)
+  end
+
+  def support_wizard_for_step(step)
+    raise InvalidStep unless step.in?(SupportRequestForm::STEPS)
+
+    "SupportRequestForm::#{step.camelize}".constantize.new(session[:user_attributes])
+  end
+
+  def support_wizard_next_step(step)
+    SupportRequestForm::STEPS[SupportRequestForm::STEPS.index(step) + 1]
+  end
+
+  class InvalidStep < StandardError; end
 end
