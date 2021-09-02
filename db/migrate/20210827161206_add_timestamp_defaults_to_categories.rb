efa3552a9503a79f18bf4fcd07037956832e7c57
Edwin Kruglov <edwin.kruglov@education.gov.uk>
2021-09-02 16:48:59 +0100

    Fix rubocop issue

--- db/migrate/20210827161206_add_timestamp_defaults_to_categories.rb
+++ db/migrate/20210827161206_add_timestamp_defaults_to_categories.rb
@@ -1,6 +1,8 @@
 class AddTimestampDefaultsToCategories < ActiveRecord::Migration[6.1]
   def change
-    change_column_default :categories, :created_at, from: nil, to: -> { "current_timestamp" }
-    change_column_default :categories, :updated_at, from: nil, to: -> { "current_timestamp" }
+    change_table :categories, bulk: true do |t|
+      t.change_default :created_at, from: nil, to: -> { "current_timestamp" }
+      t.change_default :updated_at, from: nil, to: -> { "current_timestamp" }
+    end
   end
 end
