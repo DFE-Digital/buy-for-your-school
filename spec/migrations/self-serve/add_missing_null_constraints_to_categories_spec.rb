26e4e718af8fa14c3d772abd553cb9b90a332606
Edwin Kruglov <edwin.kruglov@education.gov.uk>
2021-09-07 16:23:34 +0100

    Rubocop fixes

--- spec/migrations/self-serve/add_missing_null_constraints_to_categories_spec.rb
+++ spec/migrations/self-serve/add_missing_null_constraints_to_categories_spec.rb
@@ -7,25 +7,31 @@ RSpec.describe AddMissingNullConstraintsToCategories do
   let(:current_version) { 20_210_906_101_533 }
 
   context "when creating a Category without a description" do
-    it "fails due to null constraint" do      
-      expect { up }.to change { Category.reset_column_information; Category.columns.find { |c| c.name == "description" }.null }.from(true).to(false)
+    it "fails due to null constraint" do
+      expect { up }.to change {
+        Category.reset_column_information
+        Category.columns.find { |c| c.name == "description" }.null
+      }.from(true).to(false)
 
       category = build(:category, description: nil)
       expect { category.save!(validate: false) }.to raise_error(
         ActiveRecord::NotNullViolation,
-        /PG::NotNullViolation: ERROR:  null value in column "description" of relation "categories" violates not-null constraint/
+        /PG::NotNullViolation: ERROR:  null value in column "description" of relation "categories" violates not-null constraint/,
       )
     end
   end
 
   context "when creating a Category without a slug" do
-    it "fails due to null constraint" do      
-      expect { up }.to change { Category.reset_column_information; Category.columns.find { |c| c.name == "slug" }.null }.from(true).to(false)
+    it "fails due to null constraint" do
+      expect { up }.to change {
+        Category.reset_column_information
+        Category.columns.find { |c| c.name == "slug" }.null
+      }.from(true).to(false)
 
       category = build(:category, slug: nil)
       expect { category.save!(validate: false) }.to raise_error(
         ActiveRecord::NotNullViolation,
-        /PG::NotNullViolation: ERROR:  null value in column "slug" of relation "categories" violates not-null constraint/
+        /PG::NotNullViolation: ERROR:  null value in column "slug" of relation "categories" violates not-null constraint/,
       )
     end
   end
