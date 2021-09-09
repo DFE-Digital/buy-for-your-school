d9c74fe987665134ca4b4143800bb13887424ba0
Edwin Kruglov <edwin.kruglov@education.gov.uk>
2021-09-09 13:43:12 +0100

    Disable slug validation

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,7 +3,7 @@
 class Category < ApplicationRecord
   has_many :journeys, dependent: :destroy
 
-  validates :title, :description, :contentful_id, :liquid_template, :slug, presence: true
+  validates :title, :description, :contentful_id, :liquid_template, presence: true
   # TODO: re-enable when no longer clashes with data migrations
   # validates :slug, presence: true
   validates :contentful_id, uniqueness: true
