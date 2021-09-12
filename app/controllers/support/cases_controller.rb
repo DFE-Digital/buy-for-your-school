49d83764df5f4f7010fa72a527bf92fc74e8a6d0
Claudio Carmeli <claudiocarmeli@Claudios-MacBook.local>
2021-09-12 22:02:37 +0100

    Take care of formatting and code styling issues

--- app/controllers/support/cases_controller.rb
+++ app/controllers/support/cases_controller.rb
@@ -1,29 +1,29 @@
 module Support
   class CasesController < ApplicationController
+    before_action :find_case_by_id, only: %i[show edit update]
+
     def index
       @cases = Support::Case.all.map { |c| CasePresenter.new(c) }
     end
 
     def show
-      @case = CasePresenter.new(case_by_id)
     end
 
     def edit
-      case_by_id
       @agents = Support::Agent.all.map { |a| AgentPresenter.new(a) }
     end
 
     def update
       # TODO: Actually assign worker to a case
-      case_by_id.update!(state: "open")
+      @case.update!(state: "open")
 
       redirect_to support_cases_path
     end
 
   private
 
-    def case_by_id
-      @case = Support::Case.find_by(id: params[:id])
+    def find_case_by_id
+      @case ||= CasePresenter.new(Support::Case.find_by(id: params[:id]))
     end
   end
 end
--- spec/features/support/case_edit_spec.rb
+++ spec/features/support/case_edit_spec.rb
@@ -1,19 +1,21 @@
 RSpec.feature "Case Management Dashboard - edit" do
   before do
+    support_case = create(:support_case)
+
     user_is_signed_in
-    visit "/support/cases/#{create(:support_case).id}/edit"
+    visit "/support/cases/#{support_case.id}/edit"
   end
 
-  it "renders a title" do
-    expect(find(".govuk-fieldset__heading", visible: true)).to have_text "Assign to caseworker"
+  it "displays a title" do
+    expect(find("h1.govuk-fieldset__heading", visible: true)).to have_text "Assign to caseworker"
   end
 
-  it "renders list of agents" do
-    expect(all(".govuk-radios__item", visible: true).count).to eq(1)
-    expect(find(".govuk-radios__item")).to have_text "John Kendle"
+  it "lists agents by name" do
+    expect(all("div.govuk-radios__item", visible: true).count).to eq(1)
+    expect(find("div.govuk-radios__item")).to have_text "John Kendle"
   end
 
-  it "renders the submit button" do
+  it "displays the submit button" do
     expect(find_button("Assign")).to be_present
   end
 end
