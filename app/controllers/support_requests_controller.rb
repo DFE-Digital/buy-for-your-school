61084c783153f463f4d1fdd47600ba8629ac835b
Peter David Hamilton <pete@peterdavidhamilton.com>
2021-09-18 15:16:56 +0100

    Attempt to rework the form logic
    - the current implementation needs to step back and forth
    - validation is insufficient and needs to be conditional
    - this code can not be reused easily

--- app/controllers/support_requests_controller.rb
+++ app/controllers/support_requests_controller.rb
@@ -3,32 +3,79 @@
 class SupportRequestsController < ApplicationController
   before_action :set_support_request, only: %i[show edit update]
 
+  # start the process
+  def index; end
+
+  # check your answers before submission
   def show; end
 
+  # first question
   def new
-    @support_form = SupportForm.new(step: 1)
-    # @support_form = SupportForm::Step1.new(step: 1)
+    # binding.pry
+    # @support_form = SupportForm::Step1.new
+      # @support_form = SupportForm::Step1.new(step: 1)
+    # @support_form = SupportForm.new(step: 1)
+    # @support_form = form_class.new
+    @support_form = SupportForm.build
   end
 
-  def create
-    form_class = "SupportForm::Step#{form_params[:step]}".constantize
-    @support_form = form_class.new(form_params)
 
-    if @support_form.save
+  # questions 2 onwards until complete
+  def create
+    @categories = Category.all
+
+
+
+    # form_class = "SupportForm::Step#{form_params[:step]}".constantize
+    # @support_form = form_class.new(**form_params)
+
+    @support_form = SupportForm.build(form_params[:step], form_params)
+
+
+
+    # return to edit if validation fails
+    if @support_form.invalid?
+      render :new
+
+    else
+      # binding.pry
+      # save if the last
       if @support_form.last_step?
-        redirect_to user_support_request_path(current_user, @support_form.support_request),
-                    notice: I18n.t("support_request.created_flash")
+        @support_form.support_request.save!
+
+        redirect_to support_request_path(@support_form.support_request),
+                    notice: I18n.t("support_requests.flash.created")
+
+      # increment if not last
       else
+        @support_form.increment!
         render :new
       end
-    else
-      render :new
     end
+
+
+
+    # if @support_form.save
+    #   if @support_form.last_step?
+    #     redirect_to support_request_path(@support_form.support_request),
+    #                 notice: I18n.t("support_requests.flash.created")
+    #   else
+    #     render :new
+    #   end
+    # else
+    #   render :new
+    # end
   end
 
+
+
+
+
+
   def edit
-    form_class = "SupportForm::Step#{safe_step}".constantize
-    @support_form = form_class.new(step: safe_step,
+    form_class = "SupportForm::Step#{params[:step]}".constantize
+    @support_form = form_class.new(step: params[:step],
+    # @support_form = SupportForm.build(params[:step],
                                    journey_id: @support_request.journey_id,
                                    category_id: @support_request.category_id,
                                    phone_number: @support_request.phone_number,
@@ -36,30 +83,42 @@ class SupportRequestsController < ApplicationController
   end
 
   def update
-    if @support_request.update(form_params.except(:step))
+    @support_form = SupportForm.build(form_params[:step], form_params)
 
-      redirect_to user_support_request_path(current_user, @support_request),
-                  notice: I18n.t("support_request.updated_flash")
+    if @support_form.valid? && @support_request.update(form_params.except(:step))
+
+      redirect_to support_request_path(@support_request),
+                  notice: I18n.t("support_requests.flash.updated")
     else
       render :edit
     end
+
+
+    # if @support_request.update(form_params.except(:step))
+
+    #   redirect_to support_request_path(@support_request),
+    #               notice: I18n.t("support_requests.flash.updated")
+    # else
+    #   render :edit
+    # end
   end
 
 private
 
-  def safe_step
-    SupportForm::STEPS.fetch(params[:step].to_i) - 1
-  end
+  # def form_class
+  #   "SupportForm::Step#{params[:step]}".constantize
+  # end
 
+  # @return [SupportRequest] restricted to the current user
+  #
   def set_support_request
-    @support_request = SupportRequest.find(params[:id])
+    @support_request = SupportRequest.where(user_id: current_user.id, id: params[:id]).first
   end
 
-  # @return [ActionController::Parameters] get request attribute names
+  # @return [ActionController::Parameters] validate request attribute names from form
   #
   def form_params
-    params.require(:support_form).permit(
-      :phone_number, :journey_id, :category_id, :message, :step
-    ).merge(user: current_user)
+    # params.require(:support_form).permit(*SupportForm.new.attributes).merge(user: current_user)
+    params.require(:support_form).permit(*SupportForm.new.attributes)
   end
 end
--- app/forms/support_form.rb
+++ app/forms/support_form.rb
@@ -5,30 +5,82 @@
 # @see SupportRequest
 #
 class SupportForm
+
+  # def self.build(step = 0, params = {})
+  #   "SupportForm::Step#{step}".constantize.new(step: step.to_i, **params)
+  # end
+
   include ActiveModel::Model
 
   attr_accessor :step
 
-  STEPS = [1, 2, 3, 4].freeze
+  # form params
+  attr_accessor :phone_number # step 1
+  attr_accessor :journey_id   # step 2
+  attr_accessor :category_id  # step 3
+  attr_accessor :message      # step 4
 
+  def position
+    # return @step.to_i unless @step.nil?
+    # 1
+    self.class.name.match(/(\d)/)[0].to_i
+  end
+
+
+  # def next_step
+  #   position + 1
+  # end
+
+  # @param phone_number
+  # @param journey_id
+  # @param category_id
+  # @param message
+  # @param user
+  # @param step [Integer] defaults to 1
+  #
   def initialize(*)
     attr_accessors
     super
   end
 
-  # @return [false, Integer]
-  #
-  def save
-    return false unless valid?
 
-    next_step
+
+  # @return [Array<Symbol>] strong params
+  #
+  def attributes
+    %i[phone_number journey_id category_id message step]
   end
 
-  # @return [Integer]
+
+  # def proceed
+  #   next_step! unless last_step?
+  # end
+
+  # Validate then proceed or save
   #
-  def next_step
+  # @return [Boolean, Integer]
+  #
+  # def save
+  #   if last_step?
+  #     valid? ? support_request.save! : false
+  #   else
+  #     valid? ? next_step! : false
+  #     # valid? ? next_step : false
+  #   end
+  # end
+
+  # @return [Integer] increment the step counter
+  #
+  def increment!
     @step = @step.to_i + 1
+    # @position = @position.to_i + 1
   end
+
+  # # @return [Integer] decrement the step counter
+  # #
+  # def previous_step!
+  #   @step = @step.to_i - 1
+  # end
 
   # @return [false]
   #
@@ -42,20 +94,46 @@ class SupportForm
     @support_request ||= SupportRequest.new(params.except(:step))
   end
 
+
+# private
+
+  # def attr_accessors
+  #   attributes.each { |attr| class_eval { attr_accessor attr } }
+  # end
+
+  # @return [Hash] retrieve the values for all the attributes
+  #
+  # @example
+  #   { phone_number: "1234567890", step: 3 }
+  #
+  def params
+    attributes.index_with { |attr| send(attr) }.reject { |_, v| v.blank? }
+  end
+
   #
   # Confirm user's telephone number - not available from DSI
   #
   class Step1 < SupportForm
-    # TODO: add validation for phone_number format
-    validates :phone_number, presence: true
+    validates :phone_number,
+      presence: true,
+      numericality: true,
+      length: {
+        minimum: 10,
+        maximum: 11
+      }
+
   end
 
   #
   # Select the specification document
-  # Checklist of specs
   #
   class Step2 < Step1
     validates :journey_id, presence: true
+
+
+    def checked?(category)
+      support_request.has_journey? && category.has_journey?(journey_id)
+    end
   end
 
   #
@@ -64,20 +142,21 @@ class SupportForm
   #
   class Step3 < Step2
     validates :category_id, presence: true
+
+    # def checked?(category)
+    #   support_request.has_journey? && category.has_journey?(journey_id)
+    # end
   end
 
   #
   # "How can we help?"
   #
   class Step4 < Step3
-    validates :message, presence: true
-    validates :user, presence: true
-
-    # @return [Boolean]
-    #
-    def save
-      valid? && support_request.save!
-    end
+    validates :message,
+      presence: true,
+      length: {
+        minimum: 20
+      }
 
     # @return [true]
     #
@@ -86,19 +165,4 @@ class SupportForm
     end
   end
 
-private
-
-  # @return [Array<Symbol>] strong params
-  #
-  def attributes
-    %i[phone_number journey_id category_id message step user]
-  end
-
-  def attr_accessors
-    attributes.each { |attr| class_eval { attr_accessor attr } }
-  end
-
-  def params
-    attributes.index_with { |attr| send(attr) }.reject { |_, v| v.blank? }
-  end
 end
