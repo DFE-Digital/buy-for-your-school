ad627928019aedd38e12c8f35547e647a1540394
Claudio Carmeli <claudiocarmeli@Claudios-MacBook.local>
2021-09-23 20:47:39 +0100

    Add case action links at the bottom of show screen

--- spec/features/support/case_show_spec.rb
+++ spec/features/support/case_show_spec.rb
@@ -1,7 +1,8 @@
 RSpec.feature "Case Management Dashboard - show" do
-  before do
-    support_case = create(:support_case)
+  let(:state) { "initial" }
+  let(:support_case) { create(:support_case, state: state) }
 
+  before do
     user_is_signed_in
     visit "/support/cases/#{support_case.id}"
   end
@@ -23,6 +24,42 @@ RSpec.feature "Case Management Dashboard - show" do
   it "School details section contain contact name" do
     within "#school-details" do
       expect(find(".govuk-summary-list")).to have_text "Contact name"
+    end
+  end
+
+  it "shows the assign link" do
+    expect(find("a.assign-owner")).to have_text "Assign to case worker"
+  end
+
+  it "shows the resolve link" do
+    expect(find("a.resolve")).to have_text "Resolve case"
+  end
+
+  context "when the case is open" do
+    let(:state) { "open" }
+
+    it "shows the change owner link" do
+      expect(find("a.change-owner")).to have_text "Change case owner"
+    end
+
+    it "shows the add note link" do
+      expect(find("a.add-note")).to have_text "Add a case note"
+    end
+
+    it "shows the send email link" do
+      expect(find("a.send-email")).to have_text "Send email"
+    end
+
+    it "show the log contact link" do
+      expect(find("a.log-contact")).to have_text "Log contact with school"
+    end
+  end
+
+  context "when the case is resolved" do
+    let(:state) { "resolved" }
+
+    it "shows the reopen case link" do
+      expect(find("a.reopen")).to have_text "Reopen case"
     end
   end
 end
