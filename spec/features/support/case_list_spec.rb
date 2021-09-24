1bb513b8a5122cd9a7c376320a5bcec7b0bf8bb2
Andrew Cowle <acowle@byondsoftware.co.uk>
2021-09-24 12:46:45 +0100

    Fixing specs rebase

--- spec/features/support/case_list_spec.rb
+++ spec/features/support/case_list_spec.rb
@@ -16,6 +16,7 @@ RSpec.feature "Case management dashboard" do
   end
 
   it "lists cases" do
+    binding.pry
     expect(find("#my-cases .govuk-table")).to be_visible
     expect(all("#my-cases .govuk-table__row").count).to eq(2)
   end
