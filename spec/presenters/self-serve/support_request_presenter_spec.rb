bcbbb2a4339d8ad457a98b02cf0b324ca5a43423
Edwin Kruglov <edwin.kruglov@education.gov.uk>
2021-09-24 11:48:03 +0100

    Rubocop fixes

--- spec/presenters/self-serve/support_request_presenter_spec.rb
+++ spec/presenters/self-serve/support_request_presenter_spec.rb
@@ -1,7 +1,8 @@
 RSpec.describe SupportRequestPresenter do
+  subject(:presenter) { described_class.new(support_request) }
+
   let(:journey) { build(:journey) }
   let(:support_request) { build(:support_request, journey: journey) }
-  subject(:presenter) { described_class.new(support_request) }
 
   describe "#journey" do
     it "returns a journey presenter" do
--- spec/presenters/self-serve/user_presenter_spec.rb
+++ spec/presenters/self-serve/user_presenter_spec.rb
@@ -2,7 +2,7 @@ RSpec.describe UserPresenter do
   subject(:presenter) { described_class.new(user) }
 
   describe "#full_name" do
-    context "if a full name exists" do
+    context "when a full name exists" do
       let(:user) { build(:user, full_name: "Ms Phoebe Buffay", first_name: "Phoebe", last_name: "Buffay") }
 
       it "returns the full name" do
@@ -10,7 +10,7 @@ RSpec.describe UserPresenter do
       end
     end
 
-    context "if a full name does not exist" do
+    context "when a full name does not exist" do
       let(:user) { build(:user, full_name: nil, first_name: "Phoebe", last_name: "Buffay") }
 
       it "returns the first name and last name" do
@@ -24,7 +24,7 @@ RSpec.describe UserPresenter do
 
     it "returns journey presenters" do
       expect(presenter.journeys).to be_kind_of(Array)
-      expect(presenter.journeys.all? { |j| j.class == JourneyPresenter }).to be true
+      expect(presenter.journeys.all? { |j| j.instance_of?(JourneyPresenter) }).to be true
     end
   end
 end
