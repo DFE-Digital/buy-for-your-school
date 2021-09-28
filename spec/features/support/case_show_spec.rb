2f116020987e3f74c93cc2f640e75944a647ab76
Andrew Cowle <7682746+AndrewCowle@users.noreply.github.com>
2021-09-28 09:11:12 +0100

    Add case action links at the bottom of show screen (#542)
    * Add case action links at the bottom of show screen
    * Merge fixes to interactions PR

--- spec/features/support/case_show_spec.rb
+++ spec/features/support/case_show_spec.rb
@@ -108,4 +108,40 @@ RSpec.feature "Case Management Dashboard - show" do
       expect(find("a.reopen")).to have_text "Reopen case"
     end
   end
+
+  it "shows the assign link" do
+    expect(find("a.assign-owner")).to have_text "Assign to case worker"
+  end
+
+  it "shows the resolve link" do
+    expect(find("a.resolve")).to have_text "Resolve case"
+  end
+
+  context "when the case is open" do
+    let(:state) { "open" }
+
+    it "shows the change owner link" do
+      expect(find("a.change-owner")).to have_text "Change case owner"
+    end
+
+    it "shows the add note link" do
+      expect(find("a.add-note")).to have_text "Add a case note"
+    end
+
+    it "shows the send email link" do
+      expect(find("a.send-email")).to have_text "Send email"
+    end
+
+    it "show the log contact link" do
+      expect(find("a.log-contact")).to have_text "Log contact with school"
+    end
+  end
+
+  context "when the case is resolved" do
+    let(:state) { "resolved" }
+
+    it "shows the reopen case link" do
+      expect(find("a.reopen")).to have_text "Reopen case"
+    end
+  end
 end
