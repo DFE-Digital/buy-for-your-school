aedf224791205f5a3ef58650b5acacaa144a8eb4
Peter David Hamilton <pete@peterdavidhamilton.com>
2021-09-30 12:07:37 +0100

    Bug fix data migration spec where Category.description is null

--- db/migrate/20210825142733_add_slugs_to_existing_categories.rb
+++ db/migrate/20210825142733_add_slugs_to_existing_categories.rb
@@ -5,7 +5,12 @@ class AddSlugsToExistingCategories < ActiveRecord::Migration[6.1]
     categories = Category.where(slug: nil)
     categories.each do |category|
       contentful_category = Content::Client.new.by_id(category.contentful_id)
-      category.update!(slug: contentful_category.slug) unless contentful_category.nil?
+      next if contentful_category.nil?
+
+      category.update!(
+        slug: contentful_category.slug,
+        description: contentful_category.description,
+      )
     end
 
     categories = Category.where(description: nil)
