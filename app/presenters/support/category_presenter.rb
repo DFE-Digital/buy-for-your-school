25759bd302d091e9994ab4483451fa413fde0d54
Andrew Cowle <acowle@byondsoftware.co.uk>
2021-09-30 09:08:27 +0100

    typo fix

--- app/presenters/support/category_presenter.rb
+++ app/presenters/support/category_presenter.rb
@@ -1,7 +1,7 @@
 # :nocov:
 module Support
   class CategoryPresenter < BasePresenter
-    # TODO: this will replaced when we map categories when creating enquires
+    # TODO: this will be replaced when we map categories when creating enquires
     # @return [String]
     def title
       "n/a"
