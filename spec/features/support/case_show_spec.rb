6a051a6dcf69986e202dfe9845dd343c051eb311
Andrew Cowle <7682746+AndrewCowle@users.noreply.github.com>
2021-09-30 10:50:39 +0100

    Add interactions to case (#541)
    * Added models, views, controller and draft specs
    * Add case action links at the bottom of show screen

--- spec/features/support/case_show_spec.rb
+++ spec/features/support/case_show_spec.rb
@@ -1,5 +1,6 @@
 RSpec.feature "Case Management Dashboard - show" do
-  let(:support_case) { create(:support_case) }
+  let(:state) { "initial" }
+  let(:support_case) { create(:support_case, state: state) }
   let(:base_url) { "/support/cases/#{support_case.id}" }
 
   before do
@@ -34,7 +35,8 @@ RSpec.feature "Case Management Dashboard - show" do
   describe "Request details" do
     before { visit "#{base_url}#request-details" }
 
-    it "lists request details" do
+    # TODO: add request details in next PR
+    xit "lists request details" do
       within "#request-details" do
         expect(all(".govuk-summary-list__row")[0]).to have_text "Category"
         expect(all(".govuk-summary-list__row")[1]).to have_text "Description of problem"
@@ -69,12 +71,41 @@ RSpec.feature "Case Management Dashboard - show" do
         expect(page).not_to have_selector("p.govuk-body")
       end
     end
+  end
 
-    it "shows accordion" do
-      within "#case-history" do
-        expect(find(".govuk-accordion"))
-          .to have_text "Phone"
-      end
+  it "shows the assign link" do
+    expect(find("a.assign-owner")).to have_text "Assign to case worker"
+  end
+
+  it "shows the resolve link" do
+    expect(find("a.resolve")).to have_text "Resolve case"
+  end
+
+  context "when the case is open" do
+    let(:state) { "open" }
+
+    it "shows the change owner link" do
+      expect(find("a.change-owner")).to have_text "Change case owner"
+    end
+
+    it "shows the add note link" do
+      expect(find("a.add-note")).to have_text "Add a case note"
+    end
+
+    it "shows the send email link" do
+      expect(find("a.send-email")).to have_text "Send email"
+    end
+
+    it "show the log contact link" do
+      expect(find("a.log-contact")).to have_text "Log contact with school"
+    end
+  end
+
+  context "when the case is resolved" do
+    let(:state) { "resolved" }
+
+    it "shows the reopen case link" do
+      expect(find("a.reopen")).to have_text "Reopen case"
     end
   end
 end
