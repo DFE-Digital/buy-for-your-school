8c0bd34901ca4f06551979c7665e2d06d42e89a2
Ryan Kendall <ryankendallt@gmail.com>
2021-10-21 16:17:18 +0100

    move support form validation strings to own folder

--- app/forms/support/concerns/translatable_form_schema.rb
+++ app/forms/support/concerns/translatable_form_schema.rb
@@ -7,8 +7,8 @@ module Support
         import_predicates_as_macros
 
         config.messages.backend = :i18n
-        config.messages.top_namespace = :supported_forms
-        config.messages.load_paths << Rails.root.join("config/locales/validation/en.yml")
+        config.messages.top_namespace = :forms
+        config.messages.load_paths << Rails.root.join("config/locales/validation/support/en.yml")
       end
     end
   end
