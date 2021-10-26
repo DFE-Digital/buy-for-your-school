d8c3f3594317c98cdaec90c90137782aa7736237
Ryan Kendall <ryankendallt@gmail.com>
2021-10-26 16:19:17 +0100

    agent can send a basic email

---
+++ app/controllers/support/cases/application_controller.rb
@@ -0,0 +1,11 @@
+module Support
+  class Cases::ApplicationController < ApplicationController
+    before_action :current_case
+
+  private
+
+    def current_case
+      @current_case ||= Case.find_by(id: params[:case_id])
+    end
+  end
+end
