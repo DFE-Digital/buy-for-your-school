1a519e77f761834a0393c4ddf635f525fcb423a0
Ryan Kendall <ryankendallt@gmail.com>
2021-10-27 11:06:42 +0100

    defensive coding

--- app/presenters/support_request_presenter.rb
+++ app/presenters/support_request_presenter.rb
@@ -6,7 +6,7 @@ class SupportRequestPresenter < SimpleDelegator
   # @return [String] the name of the school
   def selected_school
     found_school = user.supported_schools.find { |school| school.urn == school_urn }
-    found_school.name
+    found_school&.name || "None"
   end
 
   # return [JourneyPresenter, nil]
