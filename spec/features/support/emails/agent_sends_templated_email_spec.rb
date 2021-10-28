9ae761d2c12d1ef1f477f311f66f67b6d8cabee0
Ryan Kendall <ryankendallt@gmail.com>
2021-10-28 13:59:15 +0100

    wip: email templates listed for selection

--- spec/features/support/emails/agent_sends_templated_email_spec.rb
+++ spec/features/support/emails/agent_sends_templated_email_spec.rb
@@ -2,6 +2,7 @@ require "rails_helper"
 
 describe "Support agent sends a templated email" do
   include_context "with an agent"
+  include_context "with notify email templates"
 
   let(:support_case) { create(:support_case, :open) }
 
@@ -9,11 +10,25 @@ describe "Support agent sends a templated email" do
     click_button "Agent Login"
     visit support_case_path(support_case)
     click_link "Send email"
-  end
-
-  it "renders a message explaining the feature is not yet implemented" do
     choose "Template"
     click_button "Save"
-    expect(page).to have_content("not yet implemented")
+  end
+
+  describe "list of templates" do
+    it "displays all available email templates to choose from" do
+      available_email_templates.each do |email_template|
+        expect(page).to have_link(email_template.name, href: support_case_email_content_path(support_case, template: email_template.id))
+      end
+    end
+  end
+
+  describe "selecting a template" do
+    before { click_link "What is a framework?" }
+
+    it "previews the email with variables substituted" do
+      within ".email-preview" do
+        expect(page).to have_content("Hi #{support_case.contact.first_name} #{support_case.contact.last_name}, here is information regarding frameworks")
+      end
+    end
   end
 end
