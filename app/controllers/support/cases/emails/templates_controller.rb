5b9691ffa7459da052ba6bae7bf653118857c5bb
Ryan Kendall <ryankendallt@gmail.com>
2021-10-28 16:06:20 +0100

    wip: templates work for the most part

--- app/controllers/support/cases/emails/templates_controller.rb
+++ app/controllers/support/cases/emails/templates_controller.rb
@@ -1,8 +1,10 @@
 module Support
-  class Cases::Emails::TemplatesController < Cases::Emails::ApplicationController
+  class Cases::Emails::TemplatesController < Cases::ApplicationController
     def index
       @back_url = new_support_case_email_type_path(@current_case)
-      @email_templates = Notifications::Client.new(ENV["NOTIFY_API_KEY"]).get_all_templates(type: "email").collection
+      @email_templates = Notifications::Client.new(ENV["NOTIFY_API_KEY"])
+        .get_all_templates(type: "email")
+        .collection
     end
   end
 end
