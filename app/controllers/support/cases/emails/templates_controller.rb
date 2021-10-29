75c74db935c57b437c3cd75125533cea7fca0811
Ryan Kendall <ryankendallt@gmail.com>
2021-10-29 15:31:25 +0100

    simplified template index show only fixed templates

--- app/controllers/support/cases/emails/templates_controller.rb
+++ app/controllers/support/cases/emails/templates_controller.rb
@@ -2,18 +2,6 @@ module Support
   class Cases::Emails::TemplatesController < Cases::ApplicationController
     def index
       @back_url = new_support_case_email_type_path(@current_case)
-      @email_templates = acceptable_email_templates
-    end
-
-  private
-
-    def acceptable_email_templates
-      templates_to_ignore = %w[auto-reply blank default]
-
-      Notifications::Client.new(ENV["NOTIFY_API_KEY"])
-        .get_all_templates(type: "email")
-        .collection
-        .reject { |template| template.name.downcase.in?(templates_to_ignore) }
     end
   end
 end
