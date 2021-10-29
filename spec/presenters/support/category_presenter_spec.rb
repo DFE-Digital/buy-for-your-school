dc55f8855abc7b64c87d916ab7046b9fe7b5bbd7
Andrew Cowle <7682746+AndrewCowle@users.noreply.github.com>
2021-10-29 16:19:59 +0100

    mapped category (#611)

--- spec/presenters/support/category_presenter_spec.rb
+++ spec/presenters/support/category_presenter_spec.rb
@@ -1,11 +1,23 @@
 RSpec.describe Support::CategoryPresenter do
   subject(:presenter) { described_class.new(category) }
 
-  let(:category) { OpenStruct.new(title: "Catering") }
+  context "with category defined" do
+    let(:category) { OpenStruct.new(title: "Catering") }
 
-  describe "#title" do
-    it "returns a placeholder for title" do
-      expect(presenter.title).to eq("n/a")
+    describe "#title" do
+      it "returns a placeholder for title" do
+        expect(presenter.title).to eq("Catering")
+      end
+    end
+  end
+
+  context "with no category defined" do
+    let(:category) { nil }
+
+    describe "#title" do
+      it "returns a placeholder for title" do
+        expect(presenter.title).to eq("n/a")
+      end
     end
   end
 end
