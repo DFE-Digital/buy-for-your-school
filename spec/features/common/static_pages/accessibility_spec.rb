9555f6696e6afc34abb6a78bfe79d256269fa0dc
David Georgiou <david.georgiou@gmail.com>
2021-10-29 13:58:09 +0100

    footer specs for accessibility

--- spec/features/common/static_pages/accessibility_spec.rb
+++ spec/features/common/static_pages/accessibility_spec.rb
@@ -14,4 +14,14 @@ RSpec.feature "Accessibility" do
       expect(page).to have_title("Accessibility")
     end
   end
+
+  describe "footer" do
+    scenario "provides an email address for the service and expected links" do
+      within("footer") do
+        expect(page).to have_text "For privacy information for this service, or to request the deletion of any personal data, email email@example.gov.uk"
+        expect(page).to have_link "email@example.gov.uk", href: "mailto:email@example.gov.uk", class: "govuk-footer__link"
+        expect(page).to have_link "Accessibility", href: "/accessibility", class: "govuk-footer__link"
+      end
+    end
+  end
 end
