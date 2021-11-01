189cf1922d5cbf33b96a24d3b3c28fae00519468
Simon Piper <simonpiper@Simons-MacBook-Pro.local>
2021-11-01 10:25:01 +0000

    further work on black_field_spec

--- spec/features/self-serve/steps/blank_field_spec.rb
+++ spec/features/self-serve/steps/blank_field_spec.rb
@@ -1,11 +1,20 @@
 RSpec.feature "Back link works after failed form validations" do
   let(:user) { create(:user) }
+  let(:category) { create(:category, :catering) }
+  let(:journey) { create(:journey, user: user, category: category) }
+  let(:section) { create(:section, title: "Catering", journey: journey) }
 
-  before { user_is_signed_in(user: user) }
+  before do
+    user_is_signed_in(user: user)
+    task_with_every_kind_of_step = create(:task, title: "Task containing every type of step", section: section)
+    create(:step, :long_text, title: "Describe what you need", contentful_id: "contentful-category-entry", task: task_with_every_kind_of_step)
+
+    visit "/journeys/#{journey.id}"
+  end
 
   it "clears the validation error rather than raise a routing error" do
     # TODO: replace fixture with factory
-    start_journey_from_category(category: "section-with-multiple-tasks.json")
+    # start_journey_from_category(category: "section-with-multiple-tasks.json")
 
     within ".app-task-list" do
       click_on "Task containing every type of step"
@@ -16,6 +25,7 @@ RSpec.feature "Back link works after failed form validations" do
 
     # would raise routing error without routes guard clause
     click_back
+    pp page.source
 
     expect(page).to have_content "Describe what you need"
   end
