1b8451cc32dd02db0b25c8c4a3fb7eca0467fea0
Simon Piper <simonpiper@Simons-MacBook-Pro.local>
2021-11-03 10:21:03 +0000

    refactored blank_field_spec - test passing

--- spec/features/self-serve/steps/blank_field_spec.rb
+++ spec/features/self-serve/steps/blank_field_spec.rb
@@ -1,32 +1,37 @@
 RSpec.feature "Back link works after failed form validations" do
   let(:user) { create(:user) }
-  let(:category) { create(:category, :catering) }
+  let(:category) { create(:category, :catering, contentful_id: "contentful-category-entry") }
   let(:journey) { create(:journey, user: user, category: category) }
-  let(:section) { create(:section, title: "Catering", journey: journey) }
+  let(:section) { create(:section, title: "Catering", journey: journey, contentful_id: "contentful-section-entry") }
 
   before do
     user_is_signed_in(user: user)
-    task_with_every_kind_of_step = create(:task, title: "Task containing every type of step", section: section)
-    create(:step, :long_text, title: "Describe what you need", contentful_id: "contentful-category-entry", task: task_with_every_kind_of_step)
-
-    visit "/journeys/#{journey.id}"
   end
 
-  it "clears the validation error rather than raise a routing error" do
-    # TODO: replace fixture with factory
-    # start_journey_from_category(category: "section-with-multiple-tasks.json")
+  context "when there is a task containing every type of step" do
+    before do
+      task_with_every_type_of_step = create(:task, title: "Task containing every type of step", section: section)
+      create(:step, :long_text, title: "Describe what you need", task: task_with_every_type_of_step, order: 0)
+      create(:step, :short_text, title: "What email address did you use?", task: task_with_every_type_of_step, order: 1)
+      create(:step, :checkbox, title: "Everyday services that are required and need to be considered", options: [{ "value" => "Breakfast" }], task: task_with_every_type_of_step, order: 2)
+      create(:step, :radio, title: "Which service do you need?", options: [{ "value" => "Catering" }], task: task_with_every_type_of_step, order: 3)
 
-    within ".app-task-list" do
-      click_on "Task containing every type of step"
+      visit "/journeys/#{journey.id}"
     end
 
-    # form validation errors because not answered
-    click_continue
+    it "clears the validation error rather than raise a routing error" do
+      within ".app-task-list" do
+        click_on "Task containing every type of step"
+      end
 
-    # would raise routing error without routes guard clause
-    click_back
-    pp page.source
+      # form validation errors because not answered
+      click_continue
 
-    expect(page).to have_content "Describe what you need"
+      # would raise routing error without routes guard clause
+      click_back
+      pp page.source
+
+      expect(page).to have_content "Describe what you need"
+    end
   end
 end
