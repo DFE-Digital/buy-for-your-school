1494257c5a0d11fcf28d08d998339ea5ec8f5500
David Georgiou <david.georgiou@gmail.com>
2021-11-17 18:27:32 +0000

    rubocop

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_03_110826) do
+ActiveRecord::Schema.define(version: 2021_11_17_133254) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -99,8 +99,11 @@ ActiveRecord::Schema.define(version: 2021_11_03_110826) do
     t.string "title"
     t.text "body"
     t.string "slug"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", default: -> { "CURRENT_TIMESTAMP" }, null: false
+    t.datetime "updated_at", default: -> { "CURRENT_TIMESTAMP" }, null: false
+    t.string "contentful_id", null: false
+    t.text "sidebar"
+    t.index ["contentful_id"], name: "index_pages_on_contentful_id", unique: true
   end
 
   create_table "radio_answers", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
