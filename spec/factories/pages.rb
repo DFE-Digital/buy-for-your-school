d2c865dbff9c4ba5e777d4102867f695f269ac05
Dave G <david.georgiou@gmail.com>
2021-11-18 12:21:48 +0000

    [36] Update Page ActiveRecord model (#661)
    https://trello.com/c/Q5LS2Gdk/36-update-page-activerecord-model

--- spec/factories/pages.rb
+++ spec/factories/pages.rb
@@ -1,7 +1,9 @@
 FactoryBot.define do
   factory :page do
-    title { "MyString" }
-    body { "MyText" }
-    slug { "MyString" }
+    title { Faker::Name.name }
+    body { Faker::Markdown.sandwich }
+    slug { Faker::Internet.slug }
+    sidebar { Faker::Markdown.sandwich }
+    contentful_id { Faker::Alphanumeric.alphanumeric }
   end
 end
