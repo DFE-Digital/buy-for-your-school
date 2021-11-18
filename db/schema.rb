eac6bd7ddb5261686e8fd736046654e7ce08383f
Dave G <david.georgiou@gmail.com>
2021-11-18 12:21:48 +0000

    [36] Update Page ActiveRecord model (#661)
    https://trello.com/c/Q5LS2Gdk/36-update-page-activerecord-model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_03_110826) do
+ActiveRecord::Schema.define(version: 2021_11_18_115424) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -45,7 +45,7 @@ ActiveRecord::Schema.define(version: 2021_11_03_110826) do
     t.datetime "created_at", precision: 6, default: -> { "CURRENT_TIMESTAMP" }, null: false
     t.datetime "updated_at", precision: 6, default: -> { "CURRENT_TIMESTAMP" }, null: false
     t.integer "journeys_count"
-    t.string "slug"
+    t.string "slug", null: false
     t.index ["contentful_id"], name: "index_categories_on_contentful_id", unique: true
   end
 
@@ -98,9 +98,13 @@ ActiveRecord::Schema.define(version: 2021_11_03_110826) do
   create_table "pages", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "title"
     t.text "body"
-    t.string "slug"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.string "slug", null: false
+    t.datetime "created_at", default: -> { "CURRENT_TIMESTAMP" }, null: false
+    t.datetime "updated_at", default: -> { "CURRENT_TIMESTAMP" }, null: false
+    t.string "contentful_id", null: false
+    t.text "sidebar"
+    t.index ["contentful_id"], name: "index_pages_on_contentful_id", unique: true
+    t.index ["slug"], name: "index_pages_on_slug", unique: true
   end
 
   create_table "radio_answers", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
@@ -202,7 +206,7 @@ ActiveRecord::Schema.define(version: 2021_11_03_110826) do
     t.string "slug"
     t.string "description"
     t.uuid "parent_id"
-    t.index ["slug"], name: "index_support_categories_on_slug"
+    t.index ["slug"], name: "index_support_categories_on_slug", unique: true
     t.index ["title", "parent_id"], name: "index_support_categories_on_title_and_parent_id", unique: true
   end
 
