953bd691784f7f12ca07d64c4e0082b721ba7473
Edwin Kruglov <edwin.kruglov@education.gov.uk>
2021-11-19 17:22:35 +0000

    Page services (#664)
    - add get and build services for Pages

---
+++ app/services/content/page/build.rb
@@ -0,0 +1,29 @@
+# Persist Page entries from Contentful
+#
+class Content::Page::Build
+  extend Dry::Initializer
+
+  # @!attribute [r] contentful_page
+  #   @return [Contentful::Entry] Contentful "Page" entity
+  #   @api private
+  option :contentful_page, reader: :private
+
+  # @return [Page]
+  def call
+    page = Page.upsert(
+      {
+        title: contentful_page.title,
+        body: contentful_page.body,
+        contentful_id: contentful_page.id,
+        slug: contentful_page.slug,
+        sidebar: contentful_page.sidebar,
+      },
+      unique_by: :contentful_id,
+      returning: %w[title contentful_id slug],
+    )
+
+    Rollbar.info("Built Contentful page", **page.first) if page
+
+    Page.find_by(contentful_id: contentful_page.id)
+  end
+end
