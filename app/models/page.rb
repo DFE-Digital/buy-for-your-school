b6a41534d5489adebadae4e9137edb8969a06b9c
David Georgiou <david.georgiou@gmail.com>
2021-11-22 17:07:18 +0000

    remove redundant code in Page

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,13 +1,2 @@
 class Page < ApplicationRecord
-  # Pages are definded in Contentful. If a new page is created
-  # or page slug is updated, this refreshed Rails routes to refelct the change
-  after_commit :refresh_routes, only: %i[create update]
-
-private
-
-  def refresh_routes
-    # return unless slug_changed?
-
-    Rails.application.reload_routes!
-  end
 end
