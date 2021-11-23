b75611e761da6976d4386a3d697a2da6ef826e8e
Peter David Hamilton <pete@peterdavidhamilton.com>
2021-11-23 11:10:25 +0000

    Interim solution to export KPI data

--- app/models/activity_log_item.rb
+++ app/models/activity_log_item.rb
@@ -1,3 +1,5 @@
+require "csv"
+
 # Capture user activity metrics
 #
 # @see RecordAction
@@ -5,4 +7,15 @@ class ActivityLogItem < ApplicationRecord
   self.table_name = "activity_log"
 
   validates :user_id, :journey_id, :action, presence: true
+
+  # @return [String]
+  def self.to_csv
+    CSV.generate(headers: true) do |csv|
+      csv << column_names
+
+      all.order(:created_at).each do |record|
+        csv << record.attributes.values
+      end
+    end
+  end
 end
