2c2510819abda0d8d44f4d1bfe3a4116aaecf753
Chris Gannon <cjgannon@gmail.com>
2021-11-23 15:17:27 +0000

    Add CSV export for Support::ActivityLogItems

---
+++ lib/tasks/export.rake
@@ -0,0 +1,11 @@
+namespace :support do
+  namespace :activity_log_items do
+    desc "Export Support ActivityLog items"
+    task export: :environment do
+      file = Rails.root.join("public/support_activity_log.csv")
+      data = Support::ActivityLogItem.to_csv
+
+      File.open(file, "w+") { |f| f.write(data) }
+    end
+  end
+end
--- spec/models/support/activity_log_item_spec.rb
+++ spec/models/support/activity_log_item_spec.rb
@@ -3,4 +3,12 @@ RSpec.describe Support::ActivityLogItem, type: :model do
     it { is_expected.to validate_presence_of(:support_case_id) }
     it { is_expected.to validate_presence_of(:action) }
   end
+
+  describe "#to_csv" do
+    it "includes headers" do
+      expect(described_class.to_csv).to eql(
+        "id,support_case_id,action,data,created_at,updated_at\n",
+      )
+    end
+  end
 end
