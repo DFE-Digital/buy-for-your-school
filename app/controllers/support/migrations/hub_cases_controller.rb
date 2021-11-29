7e92108c31853504b751529a5d0d322e7712d7c0
Andrew Cowle <acowle@byondsoftware.co.uk>
2021-11-29 11:55:45 +0000

    create interaction service

--- app/controllers/support/migrations/hub_cases_controller.rb
+++ app/controllers/support/migrations/hub_cases_controller.rb
@@ -11,10 +11,18 @@ module Support
       @form = CaseHubMigrationForm.from_validation(validation)
       if validation.success? && params[:button] == "create"
         kase = CreateCase.new(@form.to_h).call
+        create_interaction(kase.id, "hub_notes", form_params["hub_notes"]) if form_params["hub_notes"].present?
+        create_interaction(kase.id, "hub_progress_notes", form_params["progress_notes"]) if form_params["progress_notes"].present?
         redirect_to support_case_path(kase)
       else
         render :new
       end
+    end
+
+  private
+
+    def create_interaction(kase_id, event_type, body)
+      CreateInteraction.new(kase_id, event_type, current_agent.id, { body: body }).call
     end
   end
 end
--- app/controllers/support/migrations/previews_controller.rb
+++ app/controllers/support/migrations/previews_controller.rb
@@ -5,6 +5,7 @@ module Support
     def create
       @form = CaseHubMigrationFormPresenter.new(CaseHubMigrationForm.from_validation(validation))
       if validation.success?
+        binding.pry
         render "support/migrations/previews/new"
       else
         render "support/migrations/hub_cases/new"
