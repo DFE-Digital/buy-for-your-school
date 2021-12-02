d1d14c9ff235ef4b7bdbf8bc50adcaf8cc6e144c
David Georgiou <david.georgiou@gmail.com>
2021-12-02 15:02:50 +0000

    fix factory

--- spec/factories/contracts.rb
+++ spec/factories/contracts.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :contract do
+    initialize_with { type.present? ? type.constantize.new : Contract.new }
+
     supplier { "MyString" }
     started_at { "2020-12-01" }
     ended_at { "2021-12-01" }
