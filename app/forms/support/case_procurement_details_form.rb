9203fcda8e6459aad6f03760f36aa8d09ad9bc65
Edwin Kruglov <edwin.kruglov@education.gov.uk>
2021-12-06 16:17:20 +0000

    Add update method and form validation

--- app/forms/support/case_procurement_details_form.rb
+++ app/forms/support/case_procurement_details_form.rb
@@ -3,15 +3,26 @@ module Support
     extend Dry::Initializer
     include Concerns::ValidatableForm
 
-    # @!attribute [r] procurement_id
-    # @return [String]
-    # option :procurement_id, Types::Params::String
+    # @!attribute [r] required_agreement_type
+    #   @return [Symbol, nil]
     option :required_agreement_type, optional: true
+    # @!attribute [r] route_to_market
+    #   @return [Symbol, nil]
     option :route_to_market, optional: true
+    # @!attribute [r] reason_for_route_to_market
+    #   @return [Symbol, nil]
     option :reason_for_route_to_market, optional: true
+    # @!attribute [r] framework_name
+    #   @return [Symbol, nil]
     option :framework_name, optional: true
+    # @!attribute [r] started_at
+    #   @return [Date, nil]
     option :started_at, optional: true
+    # @!attribute [r] ended_at
+    #   @return [Date, nil]
     option :ended_at, optional: true
+    # @!attribute [r] stage
+    #   @return [Symbol, nil]
     option :stage, optional: true
   end
 end
---
+++ app/forms/support/case_procurement_details_form_schema.rb
@@ -0,0 +1,21 @@
+module Support
+  class CaseProcurementDetailsFormSchema < Dry::Validation::Contract
+    config.messages.backend = :i18n
+    config.messages.top_namespace = :case_procurement_details_form
+    config.messages.load_paths << Rails.root.join("config/locales/validation/support/en.yml")
+
+    params do
+      optional(:required_agreement_type).value(:symbol)
+      optional(:route_to_market).value(:symbol)
+      optional(:reason_for_route_to_market).value(:symbol)
+      optional(:framework_name).value(:string)
+      optional(:stage).value(:symbol)
+      optional(:started_at).value(:date)
+      optional(:ended_at).value(:date)
+    end
+
+    rule(:started_at, :ended_at) do
+      key.failure(:before_end_date) if values[:started_at] > values[:ended_at]
+    end
+  end
+end
