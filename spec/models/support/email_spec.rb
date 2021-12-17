e9c26facdd7bfd9d984e6e4fb201752900b0d7c3
Chris Gannon <cjgannon@gmail.com>
2021-12-17 15:28:53 +0000

    Correct cintexts in email spec

--- spec/models/support/email_spec.rb
+++ spec/models/support/email_spec.rb
@@ -5,13 +5,13 @@ RSpec.describe Support::Email, type: :model do
 
   it { is_expected.to belong_to(:case).optional }
 
-  context "that has been assigned to a case" do
+  context "that has not been assigned to a case" do
     it "does not create a new support interaction" do
       expect { subject.save! }.to change { Support::Interaction.where(event_type: "email_from_school").count }.by(0)
     end
   end
 
-  context "that has not been assigned to a case" do
+  context "that has been assigned to a case" do
     it "does creates a new support interaction" do
       kase = create(:support_case)
       subject.case = kase
