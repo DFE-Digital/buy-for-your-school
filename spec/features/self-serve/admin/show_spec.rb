7af514b6a49eb525ce2f0cf4adf1653af1f99da8
Edwin Kruglov <edwin.kruglov@education.gov.uk>
2021-12-20 16:21:34 +0000

    Add content to admin page

--- spec/features/self-serve/admin/show_spec.rb
+++ spec/features/self-serve/admin/show_spec.rb
@@ -7,10 +7,17 @@ RSpec.feature "Admin page" do
   end
 
   context "when the user is an analyst" do
-    let(:user) { create(:user, :analyst) }
+    let(:user) { create(:user, :analyst, created_at: Time.zone.parse("2021-12-20")) }
 
     it "shows the page content" do
-      expect(find("h1.govuk-heading-l")).to have_text "Admin"
+      expect(page).to have_title "User activity data"
+      expect(find("h1.govuk-heading-l")).to have_text "User activity data"
+      expect(all("th.govuk-table__header")[0]).to have_text "Total number of users"
+      expect(all("td.govuk-table__cell")[0]).to have_text "1"
+      expect(all("th.govuk-table__header")[1]).to have_text "Total number of specifications"
+      expect(all("td.govuk-table__cell")[1]).to have_text "0"
+      expect(all("th.govuk-table__header")[2]).to have_text "Last user registration date"
+      expect(all("td.govuk-table__cell")[2]).to have_text "20 December 2021"
     end
 
     it "reports access to Rollbar" do
@@ -23,6 +30,7 @@ RSpec.feature "Admin page" do
     let(:user) { create(:user) }
 
     it "shows a missing role error" do
+      expect(page).to have_title "Missing user role"
       expect(find("h1.govuk-heading-l")).to have_text "Missing user role"
       expect(find("p.govuk-body")).to have_text "You do not have the required role to access this page."
     end
