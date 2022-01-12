780783deb552ecce3e6095a428aa33e0b3a512b3
Simon Piper <simonpiper@C02FL95VMD6M.local>
2022-01-12 15:19:46 +0000

    start page - controller, route and view - tests passing

---
+++ app/controllers/faf_controller.rb
@@ -0,0 +1,6 @@
+class FafController < ApplicationController
+  skip_before_action :authenticate_user!
+
+  def new
+  end
+end
---
+++ spec/features/self-serve/faf/start_page_spec.rb
@@ -0,0 +1,23 @@
+RSpec.feature "Faf start page" do
+  context "when the user is not signed in" do
+    before do
+      visit "/procurement-support/new"
+    end
+
+    it "loads the page" do
+      expect(find("h1")).to have_text "Request help and support with your procurement"
+    end
+  end
+
+  context "when the user is signed in" do
+    before do
+      user_is_signed_in
+      visit "/procurement-support/new"
+    end
+
+    it "loads the page" do
+      expect(find("h1")).to have_text "Request help and support with your procurement"
+    end
+  end
+
+end
