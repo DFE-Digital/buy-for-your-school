77fd34ccad45d1b273ff4507d81f6a61cb204128
Dave G <david.georgiou@gmail.com>
2022-01-14 16:42:24 +0000

    [219] FaF do you have dfe sign in (#762)
    * step logic and FaF form/schema
    * dsi_or_search route, action and view

---
+++ app/controllers/fafs_controller.rb
@@ -0,0 +1,33 @@
+class FafsController < ApplicationController
+  skip_before_action :authenticate_user!
+
+  def index; end
+
+  def show; end
+
+  def new
+    @faf_form = FafForm.new(step: 1)
+  end
+
+  def create
+    @faf_form = form
+    @faf_form.advance! if validation.success?
+    render :new
+  end
+
+private
+
+  # @return [FafForm] form object populated with validation messages
+  def form
+    FafForm.new(step: form_params[:step], messages: validation.errors(full: true).to_h, **validation.to_h)
+  end
+
+  def form_params
+    params.require(:faf_form).permit(:step, :dsi)
+  end
+
+  # @return [FafFormSchema] validated form input
+  def validation
+    FafFormSchema.new.call(**form_params)
+  end
+end
---
+++ spec/features/self-serve/faf/dsi_or_search_spec.rb
@@ -0,0 +1,32 @@
+RSpec.feature "Faf - dsi or search" do
+  context "when the user is not signed in" do
+    before do
+      visit "/procurement-support/new"
+    end
+
+    it "loads the page" do
+      expect(find("h1")).to have_text "Do you have a DfE Sign-in account linked to the school you're requesting support for?"
+    end
+
+    it "errors if no selection is given" do
+      click_continue
+      expect(find(".govuk-error-summary__body")).to have_text "Select whether you want to use a DfE Sign-in account"
+    end
+  end
+
+  context "when the user is signed in" do
+    before do
+      user_is_signed_in
+      visit "/procurement-support/new"
+    end
+
+    it "loads the page" do
+      expect(find("h1")).to have_text "Do you have a DfE Sign-in account linked to the school you're requesting support for?"
+    end
+
+    it "errors if no selection is given" do
+      click_continue
+      expect(find(".govuk-error-summary__body")).to have_text "Select whether you want to use a DfE Sign-in account"
+    end
+  end
+end
