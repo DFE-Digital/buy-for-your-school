6305e2d7815a2a30b26fd31d987eafd087f84b02
Chris Gannon <cjgannon@gmail.com>
2022-01-17 16:54:29 +0000

    Add "about your school" step for FaF

--- app/controllers/fafs_controller.rb
+++ app/controllers/fafs_controller.rb
@@ -10,8 +10,7 @@ class FafsController < ApplicationController
   end
 
   def create
-    @faf_form = form
-    @faf_form.advance! if validation.success?
+    advance_form
     render :new
   end
 
@@ -23,11 +22,28 @@ private
   end
 
   def form_params
-    params.require(:faf_form).permit(:step, :dsi)
+    params.require(:faf_form).permit(:step, :dsi, :school_urn)
   end
 
   # @return [FafFormSchema] validated form input
   def validation
     FafFormSchema.new.call(**form_params)
+  end
+
+  # @return [UserPresenter] adds form view logic
+  def current_user
+    @current_user = UserPresenter.new(super)
+  end
+
+  # @return [FafForm] form object with updated step number if validation successful
+  def advance_form
+    if form_params[:step].to_i == 2 && current_user.supported_schools.count == 1
+      form_params[:school_urn] = current_user.supported_schools.first[:urn]
+      @faf_form = form
+      @faf_form.advance! 2 if validation.success?
+    else
+      @faf_form = form
+      @faf_form.advance! if validation.success?
+    end
   end
 end
---
+++ spec/features/self-serve/faf/user_organisation_page_spec.rb
@@ -0,0 +1,38 @@
+RSpec.feature "Faf - user organisation page" do
+  before do
+    user_is_signed_in(user: user)
+    visit "/procurement-support/new"
+    find("label", text: I18n.t("faf.dsi_or_search.radios.dsi")).click
+    click_continue
+    click_continue
+  end
+
+  context "when the user belongs to only one supported school" do
+    let(:user) { create(:user) }
+
+    it "skips step 3 because the school is implicit" do
+      expect(page).not_to have_unchecked_field "Specialist School for Testing"
+      expect(find("span.govuk-caption-l")).to have_text "Step 4"
+    end
+  end
+
+  context "when the user belongs to multiple supported schools" do
+    let(:user) { create(:user, :many_supported_schools) }
+
+    it "loads the page" do
+      expect(find("legend")).to have_text "Which school are you buying for?"
+    end
+
+    it "asks them to choose which school" do
+      expect(page).to have_unchecked_field "Specialist School for Testing"
+      expect(page).to have_unchecked_field "Greendale Academy for Bright Sparks"
+    end
+
+    it "requires a school be selected" do
+      click_continue
+      expect(find("h2.govuk-error-summary__title")).to have_text "There is a problem"
+      expect(page).to have_link "You must select a school", href: "#faf-form-school-urn-field-error"
+      expect(find(".govuk-error-message")).to have_text "You must select a school"
+    end
+  end
+end
