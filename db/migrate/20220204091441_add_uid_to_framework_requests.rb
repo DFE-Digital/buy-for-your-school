248cd9f78ac1bd261cd22adfcf2d5f7e9ba7d5a8
Simon Piper <simonpiper@C02FL95VMD6M.local>
2022-02-09 11:47:21 +0000

    removing uid from framework_requests and schema

--- db/migrate/20220204091441_add_uid_to_framework_requests.rb
+++
@@ -1,5 +0,0 @@
-class AddUidToFrameworkRequests < ActiveRecord::Migration[6.1]
-  def change
-    add_column :framework_requests, :uid, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_04_091441) do
+ActiveRecord::Schema.define(version: 2022_01_25_110953) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -105,7 +105,6 @@ ActiveRecord::Schema.define(version: 2022_02_04_091441) do
     t.string "message_body"
     t.boolean "submitted", default: false
     t.uuid "user_id"
-    t.string "uid"
     t.index ["user_id"], name: "index_framework_requests_on_user_id"
   end
 
