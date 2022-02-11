6de4a33414a7349407bc8d523704c0279efa2ead
Chris Gannon <cjgannon@gmail.com>
2022-02-11 13:40:24 +0000

    Extract cage pagination spec into its own feature spec

---
+++ spec/features/support/case_list_pagination_spec.rb
@@ -0,0 +1,72 @@
+RSpec.feature "Case management dashboard pagination", bullet: :skip do
+  include_context "with an agent"
+
+  before do
+    click_button "Agent Login"
+  end
+
+  context '#my-cases' do
+    before do
+      create_list(:support_case, 15, agent: agent)
+      visit "/support/cases#my-cases"
+    end
+
+    it "shows the pagination info" do
+      within "#my-cases" do
+        expect(all(".govuk-table__body .govuk-table__row").count).to eq(10)
+        expect(page).to have_text "Showing 1 to 10 of 15 results"
+      end
+    end
+
+    it "changes to a different page when a new page link is clicked" do
+      within "#my-cases" do
+        find("a", text: "Next").click
+        expect(page).to have_text "Showing 11 to 15 of 15 results"
+      end
+    end
+  end
+
+  context '#all-cases' do
+    before do
+      create_list(:support_case, 20)
+      create_list(:support_case, 15, agent: agent)
+      visit "/support/cases#all-cases"
+    end
+
+    it "shows the pagination info" do
+      within "#all-cases" do
+        expect(all(".govuk-table__body .govuk-table__row").count).to eq(10)
+        expect(page).to have_text "Showing 1 to 10 of 35 results"
+      end
+    end
+
+    it "changes to a different page when a new page link is clicked" do
+      within "#all-cases" do
+        find("a", text: "Next").click
+        expect(page).to have_text "Showing 11 to 20 of 35 results"
+      end
+    end
+  end
+
+  context '#new-cases' do
+    before do
+      create_list(:support_case, 20)
+      create_list(:support_case, 15, agent: agent)
+      visit "/support/cases#new-cases"
+    end
+
+    it "shows the pagination info" do
+      within "#new-cases" do
+        expect(all(".govuk-table__body .govuk-table__row").count).to eq(10)
+        expect(page).to have_text "Showing 1 to 10 of 35 results"
+      end
+    end
+
+    it "changes to a different page when a new page link is clicked" do
+      within "#new-cases" do
+        find("a", text: "Next").click
+        expect(page).to have_text "Showing 11 to 20 of 35 results"
+      end
+    end
+  end
+end
