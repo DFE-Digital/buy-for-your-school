1a29e1db53cab466da8aa3071de8d4351ca15e57
David Georgiou <david.georgiou@gmail.com>
2022-02-11 10:01:23 +0000

    more specs

--- app/services/support/merge_case_emails.rb
+++ app/services/support/merge_case_emails.rb
@@ -4,6 +4,13 @@ module Support
   #
   # Merge a new support cases emails into an existing support case
   #
+  # TODO:
+  #   1. If adding any further merging functionality to this class, consider adding a merged_into_case_id
+  #      to t.support_cases to keep a record of the case a case was merged into.
+  #
+  #   2. The inverse should also be applied to t.support_emails and t.support_interactions to keep a record of their
+  #      original case (e.g. original_case_id).
+  #
   class MergeCaseEmails
     class CaseNotNewError < StandardError; end
 
@@ -11,13 +18,13 @@ module Support
 
     # @!attribute from_case
     #   @return [Support::Case]
-    option :from_case
+    option :from_case, Types.Instance(Support::Case), optional: false
 
     # @!attribute to_case
     #   @return [Support::Case]
-    option :to_case
+    option :to_case, Types.Instance(Support::Case), optional: false
 
-    # @return [Support::CasePresenter, Support::CasePresenter]
+    # @return [TrueClass]
     def call
       from_case.transaction do
         raise CaseNotNewError unless from_case.initial?
