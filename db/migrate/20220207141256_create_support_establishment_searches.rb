f5011590c0c8d9f07d12984a26f70889bdc09231
Ryan Kendall <ryankendallt@gmail.com>
2022-02-15 10:36:18 +0000

    fix migrations on actions

--- db/migrate/20220207141256_create_support_establishment_searches.rb
+++ db/migrate/20220207141256_create_support_establishment_searches.rb
@@ -1,5 +1,8 @@
 class CreateSupportEstablishmentSearches < ActiveRecord::Migration[6.1]
   def change
+    # This should not be needed but the order of migrations has corrupted on github
+    execute "DROP VIEW IF EXISTS support_case_searches;"
+    execute "DROP VIEW IF EXISTS support_establishment_searches;"
     create_view :support_establishment_searches
   end
 end
