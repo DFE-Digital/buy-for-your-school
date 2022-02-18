1d4ac3ce7ce170f7bf739ce397550897eaa57f21
Simon Piper <simonpiper@C02FL95VMD6M.local>
2022-02-18 16:00:31 +0000

    updated case_closing tests and merge_case_emails to use state machine

--- spec/features/support/case_closing_spec.rb
+++ spec/features/support/case_closing_spec.rb
@@ -1,7 +1,7 @@
 RSpec.feature "Case worker can close a case" do
   include_context "with an agent"
 
-  let(:support_case) { create(:support_case, :resolved) }
+  let(:support_case) { create(:support_case, :resolved, agent: agent, ref: "000001") }
 
   before do
     click_button "Agent Login"
@@ -10,12 +10,20 @@ RSpec.feature "Case worker can close a case" do
   end
 
   it "redirects to the case" do
-    expect(page).to have_current_path(support_case_path(support_case), ignore_query: true)
+    expect(page).to have_current_path(support_case_path(support_case))
   end
 
   it "changes status of case to closed" do
     support_case.reload
     expect(support_case.state).to eq "closed"
   end
-  
+
+  it "removes the case from my cases tab" do
+    visit support_cases_path
+    expect(page).not_to have_css("#my-cases .case-row", text: "000001")
+   
+    within "#all-cases .case-row", text: "000001" do
+      expect(page).to have_css(".case-status", text: "Closed")
+    end
+  end
 end
