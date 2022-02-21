e8cc5198aeb458c3fa3afcdc3ad73f45d4ec26a2
Edwin Kruglov <edwin.kruglov@education.gov.uk>
2022-02-21 11:45:44 +0000

    Remove TODOs

--- spec/helpers/self-serve/application_helper_spec.rb
+++ spec/helpers/self-serve/application_helper_spec.rb
@@ -9,7 +9,6 @@ RSpec.describe ApplicationHelper, type: :helper do
   describe "#banner_message" do
     it "returns the beta message by default" do
       # banner.beta.message
-      # TODO: update to link to contact form
       expect(helper.banner_message).to eq "This is a new service – your <a class=\"govuk-link\" target=\"_blank\" rel=\"noopener\" href=\"/feedback/new\">feedback</a> will help us to improve it."
     end
   end
