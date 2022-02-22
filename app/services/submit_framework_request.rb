7fb66dd00d7b69081d3dca382b438564e3c152c6
Simon Piper <simonpiper@C02FL95VMD6M.local>
2022-02-22 14:08:22 +0000

    update documentation

--- app/services/submit_framework_request.rb
+++ app/services/submit_framework_request.rb
@@ -45,7 +45,7 @@ private
     @user ||= FrameworkRequestPresenter.new(request).user
   end
 
-  # @return [Support::Organisation]
+  # @return [Support::Organisation] or [Support::EstablishmentGroup]
   def map_organisation
     Support::Organisation.find_by(urn: request.school_urn) || Support::EstablishmentGroup.find_by(uid: request.group_uid)
   end
