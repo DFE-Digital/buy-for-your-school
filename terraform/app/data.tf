473955d8a4aef0adb682f4abc66b98718eb086ed
Ryan Kendall <ryankendallt@gmail.com>
2022-02-24 13:47:00 +0000

    Updated terraform to include service.gov and s3

--- terraform/app/data.tf
+++ terraform/app/data.tf
@@ -18,7 +18,15 @@ data "cloudfoundry_service" "postgres" {
   name = "postgres"
 }
 
+data "cloudfoundry_service" "s3" {
+  name = "aws-s3-bucket"
+}
+
 data "cloudfoundry_space" "space" {
   name = local.cloudfoundry_space
   org  = data.cloudfoundry_org.org.id
+}
+
+data "cloudfoundry_domain" "publish_service_gov_uk" {
+  name = local.service_gov_uk_domain
 }
