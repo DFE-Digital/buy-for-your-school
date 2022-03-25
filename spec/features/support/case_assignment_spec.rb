1cce6828f973ab4473057157c92e03bf85dfa97b
Edwin Kruglov <edwin.kruglov@education.gov.uk>
2022-03-25 16:05:50 +0000

    Decrease wait time in case_assignment_spec

--- spec/features/support/case_assignment_spec.rb
+++ spec/features/support/case_assignment_spec.rb
@@ -68,7 +68,7 @@ RSpec.feature "Case worker assignment", js: true do
 
   def select_agent(term)
     fill_in "Search for caseworker name", with: term
-    sleep 2
+    sleep 1
     find(".autocomplete__option", text: term).click
   end
 end
