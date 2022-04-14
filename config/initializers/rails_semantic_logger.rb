8ba4cf09647427ed3b6e36fb431a40a88ef66a73
Ryan Kendall <ryankendallt@gmail.com>
2022-04-14 09:26:04 +0100

    Dont initialize rails semantic logger in test

--- config/initializers/rails_semantic_logger.rb
+++ config/initializers/rails_semantic_logger.rb
@@ -1,9 +1,11 @@
 # See https://dfedigital.atlassian.net/wiki/spaces/BaT/pages/2045870109/Configuring+Rails+logger+for+GOVUK+PaaS
 
-Rails.application.configure do
-  config.semantic_logger.application = "" # This is added by logstash from the paas tags
-  config.log_tags = [:request_id] # Prepend all log lines with the following tags.
-end
+unless Rails.env.test?
+  Rails.application.configure do
+    config.semantic_logger.application = "" # This is added by logstash from the paas tags
+    config.log_tags = [:request_id] # Prepend all log lines with the following tags.
+  end
 
-SemanticLogger.add_appender(io: STDOUT, level: Rails.application.config.log_level, formatter: Rails.application.config.log_format)
-Rails.application.config.logger.info('Application logging to STDOUT')
+  SemanticLogger.add_appender(io: STDOUT, level: Rails.application.config.log_level, formatter: Rails.application.config.log_format)
+  Rails.application.config.logger.info('Application logging to STDOUT')
+end
