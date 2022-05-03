bf7b07f71ce9ac1ec32f0dce81b4ba7f3de5b869
Edwin Kruglov <edwin.kruglov@education.gov.uk>
2022-05-03 13:57:42 +0100

    Fix rubocop

--- app/controllers/support/cases/messages_controller.rb
+++ app/controllers/support/cases/messages_controller.rb
@@ -4,11 +4,11 @@ module Support
       @reply_form = Messages::ReplyForm.from_validation(validation)
       if validation.success?
         Messages::Send.new(body: @reply_form.body, kase: current_case, agent: current_agent).call
-        redirect_to support_case_path(@current_case, anchor: "messages")
-      else
-        # redirect back to cases#show but with form errors?
-        redirect_to support_case_path(@current_case, anchor: "messages")
+        # else
+        # redirect back to message preview/edit page
       end
+
+      redirect_to support_case_path(@current_case, anchor: "messages")
     end
 
   private
