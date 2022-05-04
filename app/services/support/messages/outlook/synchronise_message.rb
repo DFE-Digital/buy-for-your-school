df069885658231611aba457c28bbc2577ca594a4
Ryan Kendall <ryankendallt@gmail.com>
2022-05-04 12:03:35 +0100

    Allow more flexible sync process

--- app/services/support/messages/outlook/synchronise_message.rb
+++ app/services/support/messages/outlook/synchronise_message.rb
@@ -5,19 +5,27 @@ module Support
         def self.call(message)
           email = Support::Email.find_or_initialize_by(outlook_id: message.id)
 
-          return unless email.new_record?
-
-          sync_process = [
-            Synchronisation::Steps::PersistEmail,
-            Synchronisation::Steps::AttachEmailToCase,
-            Synchronisation::Steps::PersistEmailAttachments,
-            Synchronisation::Steps::SurfaceEmailOnCase,
-          ]
+          sync_process = sync_process_for(email)
 
           Support::Email.transaction do
             sync_process.each do |step|
               step.call(message, email)
             end
+          end
+        end
+
+        def self.sync_process_for(email)
+          if email.new_record?
+            # Emails imported for the first time must follow all the steps
+            [
+              Synchronisation::Steps::PersistEmail,
+              Synchronisation::Steps::AttachEmailToCase,
+              Synchronisation::Steps::PersistEmailAttachments,
+              Synchronisation::Steps::SurfaceEmailOnCase,
+            ]
+          else
+            # Allow existing emails to update certain fields
+            Array(Synchronisation::Steps::PersistEmail)
           end
         end
       end
--- spec/services/support/messages/outlook/synchronise_message_spec.rb
+++ spec/services/support/messages/outlook/synchronise_message_spec.rb
@@ -6,17 +6,18 @@ describe Support::Messages::Outlook::SynchroniseMessage do
     allow(Support::Messages::Outlook::Synchronisation::Steps::AttachEmailToCase).to receive(:call).and_return(nil)
     allow(Support::Messages::Outlook::Synchronisation::Steps::PersistEmailAttachments).to receive(:call).and_return(nil)
     allow(Support::Messages::Outlook::Synchronisation::Steps::SurfaceEmailOnCase).to receive(:call).and_return(nil)
+    allow(Support::Email).to receive(:find_or_initialize_by).with(outlook_id: "123").and_return(email)
   end
 
   let(:message) { double("message", id: "123") }
 
   context "when this message has already been persisted" do
-    before { create(:support_email, outlook_id: "123") }
+    let(:email) { create(:support_email, outlook_id: "123") }
 
-    it "does not run any synchronisation for the message" do
+    it "allows the email to update its simple fields only but nothing else" do
       described_class.call(message)
 
-      expect(Support::Messages::Outlook::Synchronisation::Steps::PersistEmail).not_to have_received(:call)
+      expect(Support::Messages::Outlook::Synchronisation::Steps::PersistEmail).to have_received(:call).with(message, email).once
       expect(Support::Messages::Outlook::Synchronisation::Steps::AttachEmailToCase).not_to have_received(:call)
       expect(Support::Messages::Outlook::Synchronisation::Steps::PersistEmailAttachments).not_to have_received(:call)
       expect(Support::Messages::Outlook::Synchronisation::Steps::SurfaceEmailOnCase).not_to have_received(:call)
@@ -25,8 +26,6 @@ describe Support::Messages::Outlook::SynchroniseMessage do
 
   context "when this message has not yet been persisted" do
     let(:email) { build(:support_email) }
-
-    before { allow(Support::Email).to receive(:find_or_initialize_by).with(outlook_id: "123").and_return(email) }
 
     it "calls each step of the synchronisation process with the message and new email record" do
       described_class.call(message)
