65df83888cdb8c0847118f66fee167f0f04e3af2
Ryan Kendall <ryankendallt@gmail.com>
2022-05-06 16:30:34 +0100

    reply gform sends emails now

--- app/forms/support/messages/reply_form.rb
+++ app/forms/support/messages/reply_form.rb
@@ -3,8 +3,17 @@ module Support
     extend Dry::Initializer
     include Concerns::ValidatableForm
 
-    # @!attribute [r] body
-    # @return [String]
     option :body, Types::Params::String, optional: true
+
+    def reply_to_email(email, agent)
+      email_body = Messages::Templates.new(params: { body: body, agent: agent.full_name }).call
+
+      reply = Support::Messages::Outlook::SendReplyToEmail.new(
+        reply_to_email: email,
+        reply_text: email_body,
+      )
+
+      reply.call
+    end
   end
 end
---
+++ spec/forms/support/messages/reply_form_spec.rb
@@ -0,0 +1,23 @@
+require "rails_helper"
+
+describe Support::Messages::ReplyForm do
+  describe "sending the reply" do
+    let(:email) { build(:support_email) }
+    let(:agent) { Support::AgentPresenter.new(build(:support_agent)) }
+    let(:body) { "<p>Well hello</p>" }
+
+    it "delegates to SendReplyToEmail with the correct template" do
+      reply = double(call: nil)
+      allow(Support::Messages::Outlook::SendReplyToEmail).to receive(:new).and_return(reply)
+
+      email_body = Support::Messages::Templates.new(params: { body: body, agent: agent.full_name }).call
+
+      described_class.new(body: body).reply_to_email(email, agent)
+
+      expect(Support::Messages::Outlook::SendReplyToEmail).to have_received(:new).with(
+        reply_to_email: email,
+        reply_text: email_body)
+      expect(reply).to have_received(:call).once
+    end
+  end
+end
