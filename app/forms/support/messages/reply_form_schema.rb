5e277231183cbd7b5d47a0ebb5c14797a17c3391
Ryan Kendall <ryankendallt@gmail.com>
2022-05-24 09:39:35 +0100

    Bad file check on email attachments

--- app/forms/support/messages/reply_form_schema.rb
+++ app/forms/support/messages/reply_form_schema.rb
@@ -15,6 +15,9 @@ module Support
       if value.present?
         all_files_safe = Array(value).all? { |upload_file| Support::VirusScanner.uploaded_file_safe?(upload_file) }
         key(:attachments).failure(:infected) unless all_files_safe
+
+        all_files_allowed_type = Array(value).all? { |upload_file| upload_file.content_type.in?(OUTLOOK_MESSAGE_FILE_TYPE_ALLOW_LIST) }
+        key(:attachments).failure(:incorrect_file_type) unless all_files_allowed_type
       end
     end
   end
--- spec/forms/support/messages/reply_form_schema_spec.rb
+++ spec/forms/support/messages/reply_form_schema_spec.rb
@@ -1,6 +1,4 @@
 RSpec.describe Support::Messages::ReplyFormSchema do
-  subject(:schema) { described_class.new.call(nil) }
-
   describe "validates body" do
     context "when it is blank" do
       subject(:schema) { described_class.new.call(body: "") }
@@ -8,6 +6,35 @@ RSpec.describe Support::Messages::ReplyFormSchema do
       it "raises a validation error" do
         expect(schema.errors.messages.size).to eq 1
         expect(schema.errors.messages[0].to_s).to eq "The reply body cannot be blank"
+      end
+    end
+  end
+
+  context "when files have been attached" do
+
+    let(:infected_file)        { fixture_file_upload(Rails.root.join("spec/fixtures/support/text-file.txt"), "text/plain") }
+    let(:ok_file)              { fixture_file_upload(Rails.root.join("spec/fixtures/support/text-file.txt"), "text/plain") }
+    let(:wrong_file_type_file) { fixture_file_upload(Rails.root.join("spec/fixtures/support/javascript-file.js"), "text/javascript") }
+
+
+    subject(:schema) { described_class.new.call(body: "Filled in", attachments: attachments) }
+    let(:attachments) { [infected_file, ok_file] }
+
+    context "when a file is infected with a virus" do
+      before { allow(Support::VirusScanner).to receive(:uploaded_file_safe?).with(infected_file).and_return(false) }
+
+      it "raises a validation error" do
+        expect(schema.errors.messages.size).to eq 1
+        expect(schema.errors.messages[0].to_s).to eq "One or more of the files you uploaded contained a virus and have been rejected"
+      end
+    end
+
+    context "when a file format is not allowed" do
+      let(:attachments) { [ok_file, wrong_file_type_file] }
+
+      it "raises a validation error" do
+        expect(schema.errors.messages.size).to eq 1
+        expect(schema.errors.messages[0].to_s).to eq "One or more of the files you uploaded was an incorrect file type"
       end
     end
   end
