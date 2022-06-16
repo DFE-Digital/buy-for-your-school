6361f96810940977cc1fdab12fad23692fc3da72
Edwin Kruglov <edwin.kruglov@education.gov.uk>
2022-06-16 11:49:21 +0100

    Fix tests

--- spec/features/support/case_resolution_spec.rb
+++ spec/features/support/case_resolution_spec.rb
@@ -7,6 +7,22 @@ describe "Resolving a case" do
   let(:support_case) { create(:support_case, :opened, agent: existing_agent) }
 
   before do
+    # exit survey stub
+    stub_request(:post, "https://api.notifications.service.gov.uk/v2/notifications/email")
+      .with(body: {
+        "email_address": "school@email.co.uk",
+        "template_id": "7206a6c3-1c06-4f9c-8ae7-46879098fea5",
+        "reference": "000001",
+        "personalisation": {
+          "reference": "000001",
+          "first_name": "School",
+          "last_name": "Contact",
+          "email": "school@email.co.uk",
+          "survey_query_string": /\?Q_EED=/,
+        },
+      })
+      .to_return(body: {}.to_json, status: 200, headers: {})
+
     click_button "Agent Login"
     visit support_case_path(support_case)
     click_link "Resolve case"
