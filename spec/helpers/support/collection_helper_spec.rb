8694e8c80965e642a0e26f46564aceb3133bf9cd
Ryan Kendall <ryankendallt@gmail.com>
2022-06-23 10:10:24 +0100

    retain database ordering

--- spec/helpers/support/collection_helper_spec.rb
+++ spec/helpers/support/collection_helper_spec.rb
@@ -8,6 +8,10 @@ describe Support::CollectionHelper do
       expect(helper.procurement_category_grouped_options.flatten).not_to include("No applicable category")
     end
 
+    it "has Or as the last option in the list" do
+      expect(helper.procurement_category_grouped_options.last[0]).to eq("Or")
+    end
+
     context "when a category exists with no sub-categories" do
       before { define_categories("Business Services" => []) }
 
