79af35dd23c09fa7c327e4ac0ee83e464d2bbb3e
Serena Abbott <serena.abbott@hotmail.co.uk>
2022-07-21 15:56:07 +0100

    Fix migration to have enum columns as integer rather than string

--- db/migrate/20220713143145_create_exit_survey_responses.rb
+++ db/migrate/20220713143145_create_exit_survey_responses.rb
@@ -3,12 +3,12 @@ class CreateExitSurveyResponses < ActiveRecord::Migration[6.1]
     create_table :exit_survey_responses, id: :uuid do |t|
       t.string :case_id, null: false
       t.string :case_ref, null: false
-      t.string :satisfaction_level
+      t.integer :satisfaction_level
       t.string :satisfaction_text
-      t.string :saved_time
-      t.string :better_quality
-      t.string :future_support
-      t.string :hear_about_service
+      t.integer :saved_time
+      t.integer :better_quality
+      t.integer :future_support
+      t.integer :hear_about_service
       t.boolean :opt_in
       t.string :opt_in_name
       t.string :opt_in_email
