a32d7216f6dd71cc07fd00d5a0fdf0c7dadba5ec
Serena Abbott <serena.abbott@hotmail.co.uk>
2022-08-19 14:50:15 +0100

    Start case statistics by tower drilldown page

--- app/controllers/support/towers_controller.rb
+++ app/controllers/support/towers_controller.rb
@@ -1,5 +1,16 @@
 module Support
   class TowersController < CaseStatisticsController
+    before_action :set_tower_view_fields, only: :show
+
     def show; end
+
+  private
+
+    def set_tower_view_fields
+      @stages = Procurement.stages.to_h
+      @levels = Case.support_levels.to_h.keys
+      @no_of_live_cases_by_tower_state_and_stage = Case.where(state: %i[initial opened on_hold]).left_outer_joins(:category, :procurement).group(:tower, :state, :stage).count("support_cases.id")
+      @no_of_live_cases_by_tower_state_and_level = Case.where(state: %i[initial opened on_hold]).left_outer_joins(:category).group(:tower, :state, :support_level).count("support_cases.id")
+    end
   end
 end
