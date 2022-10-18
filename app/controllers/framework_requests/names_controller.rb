45372ef27759a1a1538c7c19fb81e73e98415a46
Edwin Kruglov <edwin.kruglov@education.gov.uk>
2022-10-18 08:40:27 +0100

    Extract name controller

---
+++ app/controllers/framework_requests/names_controller.rb
@@ -0,0 +1,19 @@
+module FrameworkRequests
+  class NamesController < BaseController
+    skip_before_action :authenticate_user!
+
+  private
+
+    def update_data
+      { first_name: @form.first_name, last_name: @form.last_name }
+    end
+
+    def create_redirect_path
+      email_framework_requests_path(framework_support_form: validation.to_h)
+    end
+
+    def back_url
+      @back_url = confirm_organisation_framework_requests_path(framework_support_form: validation.to_h)
+    end
+  end
+end
