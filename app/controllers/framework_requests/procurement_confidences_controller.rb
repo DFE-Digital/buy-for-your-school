d1a2f1704b0d87105cf86c0880b6c1cb2f80f16b
Edwin Kruglov <edwin.kruglov@education.gov.uk>
2022-10-18 08:42:19 +0100

    Extract procurement confidence controller

---
+++ app/controllers/framework_requests/procurement_confidences_controller.rb
@@ -0,0 +1,19 @@
+module FrameworkRequests
+  class ProcurementConfidencesController < BaseController
+    skip_before_action :authenticate_user!
+
+  private
+
+    def update_data
+      { confidence_level: @form.confidence_level }
+    end
+
+    def create_redirect_path
+      special_requirements_framework_requests_path(framework_support_form: validation.to_h)
+    end
+
+    def back_url
+      @back_url = procurement_amount_framework_requests_path(framework_support_form: validation.to_h)
+    end
+  end
+end
