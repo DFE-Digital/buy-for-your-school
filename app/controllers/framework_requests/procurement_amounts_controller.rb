b05dc9e70cac0a3308702947214b5363857cc16e
Edwin Kruglov <edwin.kruglov@education.gov.uk>
2022-10-18 08:41:52 +0100

    Extract procurement amount controller

---
+++ app/controllers/framework_requests/procurement_amounts_controller.rb
@@ -0,0 +1,19 @@
+module FrameworkRequests
+  class ProcurementAmountsController < BaseController
+    skip_before_action :authenticate_user!
+
+  private
+
+    def update_data
+      { procurement_amount: @form.procurement_amount }
+    end
+
+    def create_redirect_path
+      procurement_confidence_framework_requests_path(framework_support_form: validation.to_h)
+    end
+
+    def back_url
+      @back_url = message_framework_requests_path(framework_support_form: validation.to_h)
+    end
+  end
+end
