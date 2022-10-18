23b157cd198a0e2d173d61a3d814f2889e628a7f
Ryan Kendall <ryankendallt@gmail.com>
2022-10-18 15:22:51 +0100

    fix rubocop and brakeman

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,16 @@
 class StaticPagesController < ApplicationController
   skip_before_action :authenticate_user!
 
+  STATIC_PAGES = %w[
+    energy-bill-relief-scheme
+    rising-energy-prices
+  ].freeze
+
   def show
-    render params[:page]
-  rescue ActionView::MissingTemplate
-    render "errors/not_found"
+    if STATIC_PAGES.include?(params[:page])
+      render params[:page]
+    else
+      render "errors/not_found"
+    end
   end
 end
