bce6d358e9ee7c1488b0f43889462b4c669186eb
Edwin Kruglov <edwin.kruglov@education.gov.uk>
2022-11-09 15:27:47 +0000

    Add missing organisation validation

--- spec/forms/framework_requests/search_for_organisation_form_spec.rb
+++ spec/forms/framework_requests/search_for_organisation_form_spec.rb
@@ -15,8 +15,30 @@ describe FrameworkRequests::SearchForOrganisationForm, type: :model do
       end
     end
 
+    context "when an invalid school is selected" do
+      subject(:form) { described_class.new(id: framework_request.id, org_id: "invalid", school_type: "school") }
+
+      it "returns the right error message" do
+        form.org_id_validation
+
+        expect(form).not_to be_valid
+        expect(form.errors.messages[:org_id]).to eq ["Select the school you want help buying for"]
+      end
+    end
+
     context "when no group is selected" do
       subject(:form) { described_class.new(id: framework_request.id, school_type: "group") }
+
+      it "returns the right error message" do
+        form.org_id_validation
+
+        expect(form).not_to be_valid
+        expect(form.errors.messages[:org_id]).to eq ["Enter your academy trust or federation name, or UKPRN and select it from the list"]
+      end
+    end
+
+    context "when an invalid group is selected" do
+      subject(:form) { described_class.new(id: framework_request.id, org_id: "invalid", school_type: "group") }
 
       it "returns the right error message" do
         form.org_id_validation
@@ -89,6 +111,48 @@ describe FrameworkRequests::SearchForOrganisationForm, type: :model do
 
       it "returns a formatted org_id with the organisation name" do
         expect(form.formatted_org_name).to eq "123 - Test School"
+      end
+    end
+  end
+
+  describe "#org_exists?" do
+    subject(:form) { described_class.new(id: framework_request.id, org_id:, school_type:) }
+
+    let(:org_id) { "123" }
+
+    describe "when the school exists" do
+      let(:school_type) { "school" }
+
+      before { create(:support_organisation, urn: "123", name: "Test School") }
+
+      it "returns true" do
+        expect(form.org_exists?(org_id, school_type)).to eq true
+      end
+    end
+
+    describe "when the school does not exist" do
+      let(:school_type) { "school" }
+
+      it "returns false" do
+        expect(form.org_exists?(org_id, school_type)).to eq false
+      end
+    end
+
+    describe "when the group exists" do
+      let(:school_type) { "group" }
+
+      before { create(:support_establishment_group, uid: "123", name: "Test Group") }
+
+      it "returns true" do
+        expect(form.org_exists?(org_id, school_type)).to eq true
+      end
+    end
+
+    describe "when the group does not exist" do
+      let(:school_type) { "group" }
+
+      it "returns false" do
+        expect(form.org_exists?(org_id, school_type)).to eq false
       end
     end
   end
