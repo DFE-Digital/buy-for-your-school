7a64ce0764e48ccfd994db93bd674292e0236526
Edwin Kruglov <edwin.kruglov@education.gov.uk>
2023-02-07 10:45:32 +0000

    fix(specify): fix next steps links and redirects
    Fixed the links and redirects to the Next Steps pages by ensuring that
    they go to the right page based on the specification category.
    
    Jira: PWNN-417

--- spec/presenters/self-serve/journey_presenter_spec.rb
+++ spec/presenters/self-serve/journey_presenter_spec.rb
@@ -1,34 +1,38 @@
 RSpec.describe JourneyPresenter do
+  subject(:presenter) { described_class.new(journey) }
+
+  let(:journey) { build(:journey) }
+
+  it { is_expected.to delegate_method(:slug).to(:category).with_prefix(:category) }
+
   describe "#sections" do
+    let(:journey) { build(:journey, :with_sections, sections_count: 2) }
+
     it "returns decorated sections" do
-      journey = build(:journey, :with_sections, sections_count: 2)
-      presenter = described_class.new(journey)
       expect(presenter.sections).to all be_a(SectionPresenter)
     end
   end
 
   describe "#steps" do
+    let(:task) { build(:task, :with_steps, steps_count: 2) }
+    let(:section) { build(:section, tasks: [task]) }
+    let(:journey) { build(:journey, sections: [section]) }
+
     it "returns decorated steps" do
-      task = build(:task, :with_steps, steps_count: 2)
-      section = build(:section, tasks: [task])
-      journey = build(:journey, sections: [section])
-      presenter = described_class.new(journey)
       expect(presenter.steps).to all be_a(StepPresenter)
     end
   end
 
   describe "#created_at" do
+    let(:journey) { build(:journey, created_at: Time.zone.local(2021, 9, 8)) }
+
     it "returns a formatted date" do
-      journey = build(:journey, created_at: Time.zone.local(2021, 9, 8))
-      presenter = described_class.new(journey)
       expect(presenter.created_at).to eq " 8 September 2021"
     end
   end
 
   describe "#category" do
     it "returns a decorated category" do
-      journey = build(:journey)
-      presenter = described_class.new(journey)
       expect(presenter.category).to be_kind_of(CategoryPresenter)
     end
   end
