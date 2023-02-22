e2272bf7d821041c42d6414c5c66617c25c32812
Ryan Kendall <ryankendallt@gmail.com>
2023-02-22 16:56:46 +0000

    feat(messages): case interactions logged

--- app/services/support/messages/outlook/synchronisation/steps/surface_email_on_case.rb
+++ app/services/support/messages/outlook/synchronisation/steps/surface_email_on_case.rb
@@ -5,7 +5,25 @@ module Support
         module Steps
           class SurfaceEmailOnCase
             def self.call(message, email)
-              email.case.update!(action_required: true) if message.inbox?
+              send(message.inbox? ? :on_inbox : :on_outbox, message, email)
+            end
+
+            def self.on_inbox(_message, email)
+              email.case.update!(action_required: true)
+
+              interaction = email.case.interactions.email_from_school.find_or_initialize_by(
+                body: "Received email from school",
+                additional_data: { email_id: email.id },
+              )
+              interaction.save!
+            end
+
+            def self.on_outbox(_message, email)
+              interaction = email.case.interactions.email_to_school.find_or_initialize_by(
+                body: "Sent email to school",
+                additional_data: { email_id: email.id },
+              )
+              interaction.save!
             end
           end
         end
