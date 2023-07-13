b849c595f8dd4f81171b83ba7fe33da7bb0b264b
Ryan Kendall <ryankendallt@gmail.com>
2023-07-13 11:33:27 +0100

    chore: remove category detection code

--- app/forms/support/category_detection_form.rb
+++
@@ -1,8 +0,0 @@
-module Support
-  class CategoryDetectionForm
-    include ActiveModel::Model
-    attr_accessor :request_text, :simulate_energy_request
-
-    def results = CategoryDetection.results_for(request_text, is_energy_request: simulate_energy_request == "true", num_results: 10)
-  end
-end
