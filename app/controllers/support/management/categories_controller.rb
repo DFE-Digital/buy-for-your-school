6a4e083c6fdf141ac891ab193c3bf34840498fc5
Ryan Kendall <ryankendallt@gmail.com>
2023-07-19 13:29:42 +0100

    chore: remove unused code

--- app/controllers/support/management/categories_controller.rb
+++ app/controllers/support/management/categories_controller.rb
@@ -5,22 +5,8 @@ module Support
       @towers = Support::Tower.unique_towers
     end
 
-    def update
-      @category = Support::Category.find(params[:id])
-      @category.update!(category_form_params)
-
-      Rollbar.info("CMS Management: Category details have been updated",
-                   admin_user_id: current_user.id, category_id: @category.id)
-
-      redirect_to support_management_categories_path
-    end
-
   private
 
     def authorize_agent_scope = [super, :access_proc_ops_portal?]
-
-    def category_form_params
-      params.require(:category).permit(:support_tower_id, :parent_id)
-    end
   end
 end
