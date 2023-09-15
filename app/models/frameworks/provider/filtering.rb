3a407161b4ad9ba1e7e67af4509230ed4d573bc4
Ryan Kendall <ryankendallt@gmail.com>
2023-09-15 15:54:05 +0100

    refactor: removed boilerplate from filterables

--- app/models/frameworks/provider/filtering.rb
+++ app/models/frameworks/provider/filtering.rb
@@ -1,31 +1,6 @@
 class Frameworks::Provider::Filtering
-  include ActiveModel::Model
-  include ActiveModel::Attributes
-  include ActiveModel::Validations
+  include FilterForm
 
-  attribute :scoped_providers, default: -> { Frameworks::Provider }
-  attribute :sort_by
-  attribute :sort_order
-
-  def results
-    providers = scoped_providers
-
-    filters.each { |filter| providers = filter.filter(providers) }
-
-    providers.sorted_by(sort_by:, sort_order:)
-  end
-
-  def available_sort_options
-    Frameworks::Provider.available_sort_options
-  end
-
-  def number_of_selected(field)
-    send(field.to_s.singularize).count(&:present?)
-  end
-
-private
-
-  def filters
-    []
-  end
+  initial_scope -> { Frameworks::Provider }
+  sort_options -> { Frameworks::Provider.available_sort_options }
 end
