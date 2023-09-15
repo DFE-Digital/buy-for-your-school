3be98289c8df6f10e40c511745d7de2553f05065
Ryan Kendall <ryankendallt@gmail.com>
2023-09-15 15:54:05 +0100

    refactor: removed boilerplate from filterables

---
+++ app/models/concerns/filter_form.rb
@@ -0,0 +1,62 @@
+module FilterForm
+  extend ActiveSupport::Concern
+
+  included do
+    include ActiveModel::Model
+    include ActiveModel::Validations
+    include ActiveModel::Attributes
+
+    attribute :sort_by
+    attribute :sort_order
+  end
+
+  def results
+    records = scoped_records
+    filters.each { |filter| records = filter.filter(records) }
+    records.sorted_by(sort_by:, sort_order:)
+  end
+
+  def number_of_selected(field)
+    send(field.to_s.singularize).count(&:present?)
+  end
+
+private
+
+  def filters
+    self.class.filter_attibutes.map do |attr, options|
+      Support::Concerns::ScopeFilter.new(send(attr), **options)
+    end
+  end
+
+  class_methods do
+    def initial_scope(proc)
+      define_method :scoped_records do
+        proc.call
+      end
+
+      define_method :available_sort_options do
+        proc.call.available_sort_options
+      end
+    end
+
+    def sort_options(proc)
+      define_method :available_sort_options do
+        proc.call
+      end
+    end
+
+    def filter_by(name, default: -> { [] }, multiple: true, options: -> { [] }, scope: nil)
+      attribute(name, default:)
+
+      filter_attibutes[name] = { scope: scope.presence || "by_#{name}", multiple: }
+
+      define_method "available_#{name}_options" do
+        options.call
+      end
+    end
+
+    def filter_attibutes
+      @filter_attibutes ||= {}
+    end
+  end
+end
