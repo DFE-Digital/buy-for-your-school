245ccb2291b0a995f4ed699fc2f978bd6c702888
Serena Abbott <serenaabbott@TB618918.AD.HQ.DEPT>
2023-09-20 16:49:39 +0100

    feat(rfh): add validation to the email form to exclude certain addresses

--- spec/forms/framework_requests/email_form_spec.rb
+++ spec/forms/framework_requests/email_form_spec.rb
@@ -25,6 +25,34 @@ describe FrameworkRequests::EmailForm, type: :model do
           expect(form.errors.messages[:email]).to eq ["Enter an email in the correct format. For example, 'someone@school.sch.uk'."]
         end
       end
+
+      context "when using a personal email" do
+        invalid_emails = ["@gmail.",
+                          "@yahoo.",
+                          "@aol.",
+                          "@hotmail.",
+                          "@mail.",
+                          "@outlook.",
+                          "@icloud.",
+                          "@lycos.",
+                          "@sky.",
+                          "@btinternet",
+                          "@talktalk",
+                          "@virginmedia",
+                          "@plus.net",
+                          "@btopenworld",
+                          "@talk21",
+                          "@live"]
+
+        invalid_emails.each do |personal_email_domain|
+          it "gives an error message for #{personal_email_domain}" do
+            framework_request = create(:framework_request, email: "invalid_email#{personal_email_domain}")
+            form = described_class.new(id: framework_request.id)
+            expect(form).not_to be_valid
+            expect(form.errors.messages[:email]).to include("Enter a school email address in the correct format, like name@example.sch.uk. You cannot use a personal email address such as #{personal_email_domain}")
+          end
+        end
+      end
     end
   end
 end
