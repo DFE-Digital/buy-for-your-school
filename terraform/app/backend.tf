2ae47f2a2412f12da8d08637f0484ee785c76548
Ryan Kendall <ryankendallt@gmail.com>
2023-09-21 14:31:24 +0100

    chore: cleanup unused gpaas code

--- terraform/app/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "paas-s3-broker-prod-lon-04cdce2c-ebd3-4525-924e-d6ce670d2ee3"
-    key    = "terraform"
-    region = "eu-west-2"
-  }
-}
--- terraform/app/s3.tf
+++
@@ -1,5 +0,0 @@
-resource cloudfoundry_service_instance s3 {
-  name         = "${local.environment}-s3"
-  space        = data.cloudfoundry_space.space.id
-  service_plan = data.cloudfoundry_service.s3.service_plans["default"]
-}
