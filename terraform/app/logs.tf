394d0ae486a6fe869ccddb38079bef76ad420af4
Ryan Kendall <ryankendallt@gmail.com>
2023-09-21 14:31:24 +0100

    chore: cleanup unused gpaas code

--- terraform/app/logs.tf
+++
@@ -1,5 +0,0 @@
-resource "cloudfoundry_user_provided_service" "log_stream" {
-  name             = "${local.project_name}-${local.environment}-log-stream"
-  space            = data.cloudfoundry_space.space.id
-  syslog_drain_url = local.syslog_drain_url
-}
