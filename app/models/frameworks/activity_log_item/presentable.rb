30805ccb8f0cbb88f00e34d5e598485bc9d29c6f
Ryan Kendall <ryankendallt@gmail.com>
2023-09-22 14:08:39 +0100

    feat(fworks): admin activity log shows more detail

--- app/models/frameworks/activity_log_item/presentable.rb
+++ app/models/frameworks/activity_log_item/presentable.rb
@@ -1,5 +1,8 @@
 module Frameworks::ActivityLogItem::Presentable
   extend ActiveSupport::Concern
+  def display_subject
+    "#{subject.try(:activity_log_display_type)} #{subject.try(:activity_log_display_short_name)}"
+  end
 
   def display_created_at
     created_at.strftime("%d/%m/%Y %H:%M:%S")
