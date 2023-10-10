42ee86c5131272aeed07b9c128f19c74b3ef9f12
Ryan Kendall <ryankendallt@gmail.com>
2023-10-10 13:33:31 +0100

    chore: ensure rfh categories seeded

---
+++ db/migrate/20231010122627_re_seed_rfh_categories.rb
@@ -0,0 +1,7 @@
+class ReSeedRfhCategories < ActiveRecord::Migration[7.0]
+  def up
+    Rake::Task['request_for_help:seed_categories'].invoke
+  end
+
+  def down; end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_19_144709) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_10_122627) do
   create_sequence "evaluation_refs"
   create_sequence "framework_refs"
 
