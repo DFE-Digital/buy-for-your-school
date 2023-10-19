c1bb10f8e29426364a49a40bc7b709d3adab9452
Ryan Kendall <ryankendallt@gmail.com>
2023-10-19 16:12:11 +0100

    assorted fixes 14

--- spec/models/support/message_thread_spec.rb
+++ spec/models/support/message_thread_spec.rb
@@ -1,9 +1,9 @@
 RSpec.describe Support::MessageThread do
   describe "#last_received_reply" do
     before do
-      kase = create(:support_case)
-      create(:support_email, :inbox, subject: "Email 1", case: kase, sent_at: Time.zone.parse("01/01/2022 10:32"))
-      create(:support_email, :inbox, subject: "Email 2", case: kase, sent_at: Time.zone.parse("01/01/2022 10:35"))
+      ticket = create(:support_case)
+      create(:support_email, :inbox, subject: "Email 1", ticket:, sent_at: Time.zone.parse("01/01/2022 10:32"))
+      create(:support_email, :inbox, subject: "Email 2", ticket:, sent_at: Time.zone.parse("01/01/2022 10:35"))
     end
 
     it "returns the last received reply" do
