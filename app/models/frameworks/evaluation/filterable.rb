b2a55ea356294245f9db8cc3563c44ae45ccf7ba
Ryan Kendall <ryankendallt@gmail.com>
2023-11-22 13:25:40 +0000

    feat(fworks): additional filters for evals

--- app/models/frameworks/evaluation/filterable.rb
+++ app/models/frameworks/evaluation/filterable.rb
@@ -3,6 +3,8 @@ module Frameworks::Evaluation::Filterable
 
   included do
     scope :by_status, ->(statuses) { where(status: Array(statuses)) }
+    scope :by_provider, ->(provider_ids) { joins(:framework).where(framework: { provider_id: Array(provider_ids) }) }
+    scope :by_category, ->(category_ids) { joins(:framework).merge(Frameworks::Framework.by_category(category_ids)) }
   end
 
   class_methods do
