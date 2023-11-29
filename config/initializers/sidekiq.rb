19c53f5f0ab27834c88436a4a522c4caf4d3d333
Ryan Kendall <ryankendallt@gmail.com>
2023-11-29 11:12:45 +0000

    fix: delete sidekiq cron jobs before starting

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,6 +4,7 @@ Sidekiq.configure_server do |config|
   # Sidekiq Cron
   schedule_file = "config/schedule.yml"
   if File.exist?(schedule_file)
+    Sidekiq::Cron::Job.destroy_all!
     Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
   end
 end
