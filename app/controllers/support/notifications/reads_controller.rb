b68403805ed43c17d4c57f3f0a6231a887102535
Ryan Kendall <ryankendallt@gmail.com>
2024-01-04 13:40:44 +0000

    refactor: notifications redirect cleaner

--- app/controllers/support/notifications/reads_controller.rb
+++ app/controllers/support/notifications/reads_controller.rb
@@ -1,11 +1,11 @@
 module Support
   class Notifications::ReadsController < ApplicationController
     def create
-      Support::Notification.find(params[:notification_id])
-        .send(params.fetch(:mark_as, "read") == "unread" ? :mark_as_unread : :mark_as_read)
+      @notification = Support::Notification.find(params[:notification_id])
+      @notification.send(params.fetch(:mark_as, "read") == "unread" ? :mark_as_unread : :mark_as_read)
 
       respond_to do |format|
-        format.turbo_stream { @notification = Support::Notification.find(params[:notification_id]) }
+        format.turbo_stream
         format.html { redirect_to redirection_path }
       end
     end
@@ -13,12 +13,17 @@ module Support
   private
 
     def redirection_path
-      return support_notifications_path if params[:redirect_to].nil?
+      specified_redirection_path || support_notifications_path
+    end
+
+    def specified_redirection_path
+      return nil if params[:redirect_to].blank?
 
       Rails.application.routes.recognize_path(params[:redirect_to])
+
       params[:redirect_to]
     rescue ActionController::RoutingError
-      support_notifications_path
+      nil
     end
   end
 end
