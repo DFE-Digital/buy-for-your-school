29107188b7d08c808da704e060c7fcb5c530ec37
Ryan Kendall <ryankendallt@gmail.com>
2024-01-04 13:40:44 +0000

    refactor: notifications redirect cleaner

--- spec/requests/support/notifications/mark_as_read_spec.rb
+++ spec/requests/support/notifications/mark_as_read_spec.rb
@@ -3,31 +3,44 @@ require "rails_helper"
 describe "Marking notifications as read/unread" do
   before { agent_is_signed_in }
 
-  describe "marking notifications as read" do
-    let(:notificaction) { create(:support_notification, :case_assigned, :unread) }
+  let(:notificaction) { create(:support_notification, :case_assigned, :read) }
 
-    it "redirects back to notifications with the notification now marked as read" do
+  describe "marking notifications as read" do
+    it "marks the notification marked as read" do
       post support_notification_read_path(notificaction)
       expect(notificaction.reload.read).to be(true)
-      expect(response).to redirect_to(support_notifications_path)
-    end
-
-    context "with a redirect param" do
-      it "redirects to the specified path with the notification now marked as read" do
-        post support_notification_read_path(notificaction, redirect_to: support_cases_path)
-        expect(notificaction.reload.read).to be(true)
-        expect(response).to redirect_to(support_cases_path)
-      end
+      expect(notificaction.read_at).to be_within(1.second).of(Time.zone.now)
     end
   end
 
   describe "marking notifications as unread" do
-    let(:notificaction) { create(:support_notification, :case_assigned, :read) }
-
-    it "redirects back to notifications with the notification now marked as unread" do
+    it "marks the notification as unread" do
       post support_notification_read_path(notificaction, mark_as: :unread)
       expect(notificaction.reload.read).to be(false)
-      expect(response).to redirect_to(support_notifications_path)
+      expect(notificaction.read_at).to be(nil)
+    end
+  end
+
+  describe "redirection" do
+    context "when no redirection param is specified" do
+      it "redirects back to notifications" do
+        post support_notification_read_path(notificaction)
+        expect(response).to redirect_to(support_notifications_path)
+      end
+    end
+
+    context "when redirection param is for a path the system does not recognise" do
+      it "redirects to the notifications list" do
+        post support_notification_read_path(notificaction, redirect_to: "/etc/hosts")
+        expect(response).to redirect_to(support_notifications_path)
+      end
+    end
+
+    context "when redirection param is for a path the system does recognise" do
+      it "redirects to the specified path" do
+        post support_notification_read_path(notificaction, redirect_to: root_path)
+        expect(response).to redirect_to(root_path)
+      end
     end
   end
 end
