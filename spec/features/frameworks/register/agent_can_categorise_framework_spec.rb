206265eb6572eb584b6d156daf90a794803c004f
Ryan Kendall <ryankendallt@gmail.com>
2024-01-10 13:34:25 +0000

    refactor: tests for framework categories

--- spec/features/frameworks/register/agent_can_categorise_framework_spec.rb
+++ spec/features/frameworks/register/agent_can_categorise_framework_spec.rb
@@ -1,23 +1,25 @@
 require "rails_helper"
 
-describe "Agent can categorise framework", js: true do
+describe "Agent can categorise framework" do
   include_context "with a framework evaluation agent"
 
   let(:framework) { create(:frameworks_framework) }
 
   before { define_basic_categories }
 
-  it "saves the categories" do
-    visit frameworks_framework_path(framework)
-    within ".govuk-summary-list__row", text: "Categories" do
-      click_on "Add Categories"
-    end
+  it "assigns the categories" do
+    visit edit_frameworks_framework_categorisations_path(framework)
     check "Laptops"
     check "Electricity"
     click_on "Save changes"
 
-    expect(page).to have_summary("Categories", "Laptops, Electricity")
-    expect(page).to have_content("Category Laptops added")
-    expect(page).to have_content("Category Electricity added")
+    within ".govuk-summary-card", text: "Basic Details" do
+      expect(page).to have_content("Laptops, Electricity")
+    end
+
+    within "#framework-activity" do
+      expect(page).to have_content("Category Laptops added")
+      expect(page).to have_content("Category Electricity added")
+    end
   end
 end
