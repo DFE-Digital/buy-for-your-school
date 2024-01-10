f0df32abddb4d612466fec1d02885df7589b2a66
Ryan Kendall <ryankendallt@gmail.com>
2024-01-10 14:46:15 +0000

    refactor: case resolution tests

--- spec/features/support/case_resolution_spec.rb
+++ spec/features/support/case_resolution_spec.rb
@@ -33,45 +33,19 @@ describe "Resolving a case" do
   end
 
   context "when providing notes" do
-    let(:case_resolved_interaction) { support_case.reload.interactions.last }
-
     before do
       fill_in "Resolve case", with: "A note when resolving the case"
       click_button "Save and close case"
     end
 
     it "resolves the case" do
-      expect(support_case.reload).to be_resolved
-    end
+      expect(page).to have_content("Case resolved successfully")
 
-    it "saves the given note to the case" do
-      expect(case_resolved_interaction.body).to eq("From open to resolved by Procurement Specialist on #{Time.zone.now.to_formatted_s(:short)}: A note when resolving the case")
-    end
+      within "#case-history tr", text: "Status change" do
+        expect(page).to have_text "From open to resolved by Procurement Specialist on #{Time.zone.now.to_formatted_s(:short)}: A note when resolving the case"
+      end
 
-    it "sets the current agent on the case note" do
-      expect(case_resolved_interaction.agent).to eq(agent)
-    end
-
-    it "leaves the agent assigned to the case" do
-      expect(support_case.reload.agent).to eq(existing_agent)
-    end
-
-    it "redirects the user back to the case" do
-      expect(page).to have_current_path(support_case_path(support_case), ignore_query: true)
-    end
-  end
-
-  context "when not providing notes" do
-    before do
-      click_button "Save and close case"
-    end
-
-    it "remains open" do
-      expect(support_case.reload).to be_opened
-    end
-
-    it "tells the user to provide notes" do
-      expect(page).to have_content("Please enter some closing notes to resolve the case")
+      expect(page).to have_content("Case owner: first_name last_name")
     end
   end
 end
