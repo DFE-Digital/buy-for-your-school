1925345b5eabf48e28aa6487dfb22968944d6326
Edwin Kruglov <edwin.kruglov@education.gov.uk>
2024-02-22 17:05:34 +0000

    fix(cms): update exit survey sent flag on old cases
    Jira: PWNN-1939

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_12_113720) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_22_164526) do
   create_sequence "evaluation_refs"
   create_sequence "framework_refs"
 
