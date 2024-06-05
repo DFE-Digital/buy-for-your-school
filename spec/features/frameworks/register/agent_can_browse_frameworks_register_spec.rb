6e1cb902e1980149302b26ee107e547bb75605b4
Serena Abbott <serena.abbott@education.gov.uk>
2024-06-05 17:25:54 +0100

    fix(fworks): fix bugs in edit and creating a framework

--- spec/features/frameworks/register/agent_can_browse_frameworks_register_spec.rb
+++ spec/features/frameworks/register/agent_can_browse_frameworks_register_spec.rb
@@ -5,24 +5,24 @@ describe "Agent can browse frameworks register", js: true do
 
   before do
     provider = create(:frameworks_provider, short_name: "TestProv")
-    create(:frameworks_framework, reference: "books", name: "Books & Stationary")
-    create(:frameworks_framework, reference: "legal", name: "Legal services", provider:)
-    create(:frameworks_framework, reference: "water", name: "Water services", provider:)
+    create(:frameworks_framework, reference: "F1", name: "Books & Stationary")
+    create(:frameworks_framework, reference: "F2", name: "Legal services", provider:)
+    create(:frameworks_framework, reference: "F3", name: "Water services", provider:)
   end
 
   it "lists all frameworks" do
     visit frameworks_root_path
-    expect(page).to have_summary("Framework Ref", "books")
+    expect(page).to have_summary("Framework Ref", "F1")
     expect(page).to have_summary("Framework Name", "Books & Stationary")
-    expect(page).to have_summary("Framework Ref", "legal")
+    expect(page).to have_summary("Framework Ref", "F2")
     expect(page).to have_summary("Framework Name", "Legal services")
-    expect(page).to have_summary("Framework Ref", "water")
+    expect(page).to have_summary("Framework Ref", "F3")
     expect(page).to have_summary("Framework Name", "Water services")
   end
 
   it "can view the details of a framework" do
     visit frameworks_root_path
-    click_on "books"
+    click_on "F1"
     expect(page).to have_title("Books & Stationary")
   end
 
@@ -33,11 +33,11 @@ describe "Agent can browse frameworks register", js: true do
         click_on "Provider"
         check "TestProv"
       end
-      expect(page).to have_summary("Framework Ref", "legal")
+      expect(page).to have_summary("Framework Ref", "F2")
       expect(page).to have_summary("Framework Name", "Legal services")
-      expect(page).to have_summary("Framework Ref", "water")
+      expect(page).to have_summary("Framework Ref", "F3")
       expect(page).to have_summary("Framework Name", "Water services")
-      expect(page).not_to have_summary("Framework Ref", "books")
+      expect(page).not_to have_summary("Framework Ref", "F1")
       expect(page).not_to have_summary("Framework Name", "Books & Stationary")
     end
   end
--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -21,5 +21,23 @@ RSpec.configure do |config|
     DatabaseCleaner.clean
     FactoryBot.reload
     Faker::UniqueGenerator.clear
+    clear_all_sequences
+  end
+
+  def clear_all_sequences
+    statement = <<-SQL
+      DO $$
+      DECLARE
+        seq RECORD;
+      BEGIN
+        FOR seq IN
+          SELECT sequence_schema, sequence_name
+          FROM information_schema.sequences
+        LOOP
+          EXECUTE format('ALTER SEQUENCE %I.%I RESTART WITH 1', seq.sequence_schema, seq.sequence_name);
+        END LOOP;
+      END $$;
+    SQL
+    ActiveRecord::Base.connection.execute(statement)
   end
 end
