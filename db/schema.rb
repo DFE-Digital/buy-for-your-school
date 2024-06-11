5ed003b67674118fd9fa4cb70bb0eb9a432d67e1
Edwin Kruglov <edwin.kruglov@education.gov.uk>
2024-06-11 10:56:58 +0100

    fix(cms): move selected emails from case 3533 to case 2850

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_10_162322) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_11_091424) do
   create_sequence "evaluation_refs"
   create_sequence "framework_refs"
 
