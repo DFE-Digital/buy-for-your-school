426ff287244f5f9b889a018e92cc8d15231b8592
Ken Tabb <theboyken@mac.com>
2024-06-28 22:34:49 +0100

    pwnn-2006 fix(cms): change ProcOps SLA from 2 days to 5

--- spec/features/specify/framework_requests/edit_framework_request_guest_spec.rb
+++ spec/features/specify/framework_requests/edit_framework_request_guest_spec.rb
@@ -22,7 +22,7 @@ RSpec.feature "Editing a 'Find a Framework' request as a guest" do
 
   it "has submission information" do
     expect(find("h1.govuk-heading-l", text: "Send your request")).to be_present
-    expect(find("p.govuk-body", text: "Once you send this request, we will review it and get in touch within 2 working days.")).to be_present
+    expect(find("p.govuk-body", text: "Once you send this request, we will review it and get in touch within 5 working days.")).to be_present
     expect(page).to have_button "Send request"
   end
 
