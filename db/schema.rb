f0d9240b4fdf31008356482f9832b3c7541986b4
Ken Tabb <theboyken@mac.com>
2024-07-31 14:52:27 +0100

    added revert migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_29_132111) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_31_134823) do
   create_sequence "evaluation_refs"
   create_sequence "framework_refs"
 
