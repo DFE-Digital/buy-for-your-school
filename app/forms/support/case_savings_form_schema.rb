e19e394937b269abbe5d8f6cbf73861402dfa2ca
Shafay Javed <shafayjavedwp@gmail.com>
2024-09-04 15:17:38 +0100

    feat(crm): updating precision in support case columns

--- app/forms/support/case_savings_form_schema.rb
+++ app/forms/support/case_savings_form_schema.rb
@@ -1,6 +1,7 @@
 module Support
   class CaseSavingsFormSchema < Dry::Validation::Contract
     include Concerns::TranslatableFormSchema
+    config.messages.top_namespace = :case_savings_form
 
     params do
       optional(:savings_status).value(:symbol)
@@ -8,6 +9,14 @@ module Support
       optional(:savings_actual_method).value(:symbol)
       optional(:savings_estimate).maybe(:decimal)
       optional(:savings_actual).maybe(:decimal)
+    end
+
+    rule :savings_estimate do
+      key.failure(:too_large) if value.to_i > 99_999_999.999
+    end
+
+    rule :savings_actual do
+      key.failure(:too_large) if value.to_i > 99_999_999.999
     end
   end
 end
