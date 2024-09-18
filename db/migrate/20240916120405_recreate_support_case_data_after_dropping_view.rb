2b70a44ffacab0ae39a34d85b2156107bad78e2d
Shafay Javed <shafayjavedwp@gmail.com>
2024-09-18 08:25:28 +0100

    droping and recreating views

--- db/migrate/20240916120405_recreate_support_case_data_after_dropping_view.rb
+++
@@ -1,7 +0,0 @@
-class RecreateSupportCaseDataAfterDroppingView < ActiveRecord::Migration[7.1]
-  def change
-    execute "DROP VIEW IF EXISTS support_tower_cases CASCADE;"
-    create_view :support_tower_cases, version: 4
-    create_view :support_case_data, version: 12
-  end
-end
