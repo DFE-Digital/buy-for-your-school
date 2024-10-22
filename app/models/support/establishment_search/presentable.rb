89f10a46359ccdf0a15002eaed6370f22f9cfe61
Serena Abbott <serena.abbott@education.gov.uk>
2024-10-22 11:04:58 +0100

    feat(cms): add proposed open and closed status and date to establishment search

--- app/models/support/establishment_search/presentable.rb
+++ app/models/support/establishment_search/presentable.rb
@@ -10,7 +10,15 @@ private
   def autocomplete_template_vars
     case source
     when "LocalAuthority" then { name:, establishment_type:, code: }
-    else { name:, postcode:, establishment_type:, urn:, ukprn: }
+    else { name:, postcode:, establishment_type:, proposed_open_or_closed:, urn:, ukprn: }
+    end
+  end
+
+  def proposed_open_or_closed
+    if organisation_status == "opening"
+      "#{I18n.t("support.organisation_statuses.#{organisation_status}")} #{opened_date.strftime('%d-%m-%Y')}"
+    elsif organisation_status == "closing"
+      "#{I18n.t("support.organisation_statuses.#{organisation_status}")} #{closed_date.strftime('%d-%m-%Y')}"
     end
   end
 end
