1f6bcb11beea56c2324268bed4bd4327d915f5c7
Paul Battley <pbattley@gmail.com>
2024-11-12 11:56:11 +0000

    Remove duplicate support agents

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_16_093636) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_12_114859) do
   create_sequence "evaluation_refs"
   create_sequence "framework_refs"
 
