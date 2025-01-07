1269721510998ffaea0f0cc6321552afe54bee08
edwin-jebaraj <edwin.jebaraj@education.gov.uk>
2025-01-07 15:12:55 +0000

    Updated files for evaluator email link verification

--- config/routes.rb
+++ config/routes.rb
@@ -338,7 +338,9 @@ Rails.application.routes.draw do
   end
 
   namespace :evaluation do
-    resources :tasks, only: %i[show]
+    resources :tasks, only: %i[show edit]
+
+    get 'verify/evaluator/link/:id', to: 'tasks#edit', as: :verify_evaluators_unique_link
   end
 
   # E&O Portal
