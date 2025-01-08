43fdf8b016d1f1ac35b477c0a6c3f8faed1563fd
edwin-jebaraj <edwin.jebaraj@education.gov.uk>
2025-01-08 11:29:05 +0000

    updated files for evaluator email link validation and view task list

--- app/controllers/evaluation/tasks_controller.rb
+++ app/controllers/evaluation/tasks_controller.rb
@@ -13,6 +13,11 @@ module Evaluation
       redirect_to evaluation_task_path(@current_case)
     end
 
+    def edit
+      session[:email_evaluator_link] = evaluation_task_path(@current_case, host: request.host)
+      redirect_to evaluation_task_path(@current_case)
+    end
+
     def show; end
 
   private
