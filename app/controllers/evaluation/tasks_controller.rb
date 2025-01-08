d469dd7a41323efcd4f9fbb1b8bf9b387f5f1fdd
edwin-jebaraj <edwin.jebaraj@education.gov.uk>
2025-01-08 11:29:05 +0000

    updated files for evaluator email link validation and view task list

--- app/controllers/evaluation/tasks_controller.rb
+++ app/controllers/evaluation/tasks_controller.rb
@@ -1,13 +1,24 @@
 module Evaluation
   class TasksController < ApplicationController
+    before_action :set_current_case
     before_action :check_user_is_evaluator
+
+    def edit
+      session[:email_evaluator_link] = evaluation_task_path(@current_case, host: request.host)
+      redirect_to evaluation_task_path(@current_case)
+    end
 
     def show; end
 
   private
 
     helper_method def current_evaluator
-      @current_evaluator ||= Support::Evaluator.find(params[:id])
+      @current_evaluator ||= Support::Evaluator.find_by(support_case_id: params[:id], email: current_user.email)
+    end
+
+    def set_current_case
+      @current_case = Support::Case.find(params[:id])
+      @evaluation_due_date = @current_case.evaluation_due_date.strftime("%d %B %Y")
     end
 
     def check_user_is_evaluator
