0ee0d850cc8b5ad99c345ce2e98127aafd0e14b7
edwin-jebaraj <edwin.jebaraj@education.gov.uk>
2025-01-15 18:03:37 +0000

    Added new files for email evaluators functionality

--- app/models/email/template_parser.rb
+++ app/models/email/template_parser.rb
@@ -2,6 +2,10 @@ class Email::TemplateParser
   def initialize(variables: {})
     @variables = {
       "caseworker_full_name" => Current.actor.try(:full_name),
+      "organisation_name" => "{{organisation_name}}",
+      "sub_category" => "{{sub_category}}",
+      "unique_case_specific_link" => "{{unique_case_specific_link}}",
+      "evaluation_due_date" => "{{evaluation_due_date}}",
     }.merge(variables)
   end
 
