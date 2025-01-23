84a80f95cdc6e8ed45d26c8505605840c028e427
edwin-jebaraj <edwin.jebaraj@education.gov.uk>
2025-01-23 16:16:44 +0000

    Updated parsing method for case without sub category

--- spec/features/support/agent_can_email_to_evaluators_spec.rb
+++ spec/features/support/agent_can_email_to_evaluators_spec.rb
@@ -8,6 +8,19 @@ describe "Agent can email to evaluators", :js, :with_csrf_protection do
   let(:file_2) { fixture_file_upload(Rails.root.join("spec/fixtures/support/another-text-file.txt"), "text/plain") }
   let(:document_uploader) { support_case.document_uploader(files: [file_1, file_2]) }
 
+  before do
+    create(:support_email_template, title: "Invitation to complete procurement evaluation", subject: "about energy", body: "Test body {{sub_category}} ")
+  end
+
+  def create_evaluator_and_upload_document
+    support_case.evaluators.create!(first_name: "Momo", last_name: "Taro", email: "email@address")
+    document_uploader.save!
+  end
+
+  def send_email_to_evaluators(support_case)
+    create(:support_email, :inbox, ticket: support_case, outlook_conversation_id: "OCID1", subject: "Email Evaluators", recipients: [{ "name" => "Test 1", "address" => "test1@email.com" }], unique_body: "Email 1", is_read: false)
+  end
+
   specify "When add evaluators, set due date and upload documents status are not complete" do
     visit support_case_path(support_case, anchor: "tasklist")
 
@@ -29,9 +42,9 @@ describe "Agent can email to evaluators", :js, :with_csrf_protection do
 
   specify "When add evaluators, set due date and upload documents status are complete" do
     support_case.update!(evaluation_due_date: Date.tomorrow, has_uploaded_documents: true)
-    support_case.evaluators.create!(first_name: "Momo", last_name: "Taro", email: "email@address")
-    document_uploader.save!
-    create(:support_email_template, title: "Invitation to complete procurement evaluation", subject: "about energy", body: "energy body")
+
+    create_evaluator_and_upload_document
+
     support_case.reload
 
     visit support_case_path(support_case, anchor: "tasklist")
@@ -42,11 +55,66 @@ describe "Agent can email to evaluators", :js, :with_csrf_protection do
 
     expect(page).to have_content("Email evaluators")
 
-    create(:support_email, :inbox, ticket: support_case, outlook_conversation_id: "OCID1", subject: "Email Evaluators", recipients: [{ "name" => "Test 1", "address" => "test1@email.com" }], unique_body: "Email 1", is_read: false)
+    send_email_to_evaluators(support_case)
+
     support_case.update!(sent_email_to_evaluators: true)
 
     visit support_case_path(support_case, anchor: "tasklist")
 
     expect(find("#complete-evaluation-4-status")).to have_text("Complete")
+  end
+
+  context "without sub_category" do
+    it "sends support email" do
+      support_case.update!(evaluation_due_date: Date.tomorrow, has_uploaded_documents: true, category_id: nil)
+
+      create_evaluator_and_upload_document
+
+      support_case.reload
+
+      visit support_case_path(support_case, anchor: "tasklist")
+
+      expect(find("#complete-evaluation-4-status")).to have_text("To do")
+
+      visit edit_support_case_email_evaluators_path(support_case)
+
+      expect(page).not_to have_content("an Audit Accessibility (buildings and digital)")
+
+      send_email_to_evaluators(support_case)
+
+      support_case.update!(sent_email_to_evaluators: true)
+
+      visit support_case_path(support_case, anchor: "tasklist")
+
+      expect(find("#complete-evaluation-4-status")).to have_text("Complete")
+    end
+  end
+
+  context "with sub_category" do
+    let(:support_case) { create(:support_case, :with_support_category) }
+
+    it "sends support email" do
+      support_case.update!(evaluation_due_date: Date.tomorrow, has_uploaded_documents: true)
+
+      create_evaluator_and_upload_document
+
+      support_case.reload
+
+      visit support_case_path(support_case, anchor: "tasklist")
+
+      expect(find("#complete-evaluation-4-status")).to have_text("To do")
+
+      visit edit_support_case_email_evaluators_path(support_case)
+
+      expect(page).to have_content("an Audit Accessibility (buildings and digital)")
+
+      send_email_to_evaluators(support_case)
+
+      support_case.update!(sent_email_to_evaluators: true)
+
+      visit support_case_path(support_case, anchor: "tasklist")
+
+      expect(find("#complete-evaluation-4-status")).to have_text("Complete")
+    end
   end
 end
