043b6b19078c08569380b88321d63aeb6f54f3f2
edwin-jebaraj <edwin.jebaraj@education.gov.uk>
2025-01-24 11:07:52 +0000

    Added new page for evaluator signin

--- app/controllers/evaluation/application_controller.rb
+++ app/controllers/evaluation/application_controller.rb
@@ -1,4 +1,17 @@
 module Evaluation
   class ApplicationController < ::ApplicationController
+  protected
+
+    def authenticate_user!
+      return unless current_user.guest?
+
+      session.delete(:dfe_sign_in_uid)
+
+      if params[:controller] == "evaluation/tasks" && params[:action] == "edit"
+        session[:email_evaluator_link] = evaluation_task_path(id: params[:id], host: request.host)
+      end
+
+      redirect_to root_path, notice: I18n.t("banner.session.visitor")
+    end
   end
 end
