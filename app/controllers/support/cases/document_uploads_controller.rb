74114a3e6bfee86ffc3a45569a68de1ef11ab2cc
edwin-jebaraj <edwin.jebaraj@education.gov.uk>
2025-02-04 18:57:53 +0000

    Fixed review comments

--- app/controllers/support/cases/document_uploads_controller.rb
+++ app/controllers/support/cases/document_uploads_controller.rb
@@ -22,14 +22,14 @@ module Support
     def destroy
       @uploaded_document = Support::CaseUploadDocument.find(params[:document_id])
       @support_document = Support::Document.find(@uploaded_document.attachable_id)
-      evaluator_downloaed_document = Support::EvaluatorsDownloadDocument.find_by(support_case_upload_document_id: params[:document_id])
+      evaluator_download_document = Support::EvaluatorsDownloadDocument.find_by(support_case_upload_document_id: params[:document_id])
       @back_url = edit_support_case_document_uploads_path
       return unless params[:confirm]
 
       @uploaded_document.destroy!
       @support_document.destroy!
-      if evaluator_downloaed_document
-        evaluator_downloaed_document.destroy!
+      if evaluator_download_document
+        evaluator_download_document.destroy!
       end
 
       if @uploaded_files.empty?
