6519bae0328cb7121b9fa5065217abfec498bc72
edwin-jebaraj <edwin.jebaraj@education.gov.uk>
2025-02-04 10:25:11 +0000

    Added new page for evaluator signin

--- spec/features/evaluation/task_list_spec.rb
+++ spec/features/evaluation/task_list_spec.rb
@@ -25,4 +25,27 @@ describe "Evaluator can see task list", :js do
 
     expect(page).not_to have_text("Evaluator task list")
   end
+
+  specify "Verify evaluation unique link when evaluator already authenticated" do
+    create(:support_evaluator, support_case:, dsi_uid: user.dfe_sign_in_uid, email: user.email)
+    Current.user = user
+    user_exists_in_dfe_sign_in(user:)
+
+    user_is_signed_in(user:)
+
+    visit evaluation_verify_evaluators_unique_link_path(support_case)
+
+    expect(page).to have_text("Evaluator task list")
+  end
+
+  specify "Verify evaluation unique link when evaluator not authenticated" do
+    create(:support_evaluator, support_case:, dsi_uid: user.dfe_sign_in_uid, email: user.email)
+    Current.user = user
+    user_exists_in_dfe_sign_in(user:)
+
+    visit evaluation_verify_evaluators_unique_link_path(support_case)
+
+    expect(page).to have_text("Complete procurement evaluation")
+    expect(page).not_to have_text("Evaluator task list")
+  end
 end
