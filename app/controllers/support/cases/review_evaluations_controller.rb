0237007a60fcb41942062a2a0081c9596fd3c957
edwin-jebaraj <edwin.jebaraj@education.gov.uk>
2025-02-05 20:10:43 +0000

    updated code as per review comments

--- app/controllers/support/cases/review_evaluations_controller.rb
+++ app/controllers/support/cases/review_evaluations_controller.rb
@@ -1,7 +1,7 @@
 module Support
   class Cases::ReviewEvaluationsController < Cases::ApplicationController
     before_action :set_current_case
-    before_action :set_evalution_files
+    before_action :set_evaluation_files
     before_action { @back_url = support_case_path(@current_case, anchor: "tasklist") }
 
     def edit; end
@@ -24,8 +24,8 @@ module Support
       @current_evaluator = Support::Evaluator.find(params[:case_id])
     end
 
-    def set_evalution_files
-      @evalution_files = Support::EvaluatorsUploadDocument.where(support_case_id: params[:case_id])
+    def set_evaluation_files
+      @evaluation_files = Support::EvaluatorsUploadDocument.where(support_case_id: params[:case_id])
     end
 
     def reset_evaluation_approvals
@@ -33,24 +33,30 @@ module Support
     end
 
     def approve_selected_evaluations
-      Support::Evaluator.where(email: review_evaluation_params[:evaluation_approved]).update_all(evaluation_approved: true)
+      filtered_params = filter_blank_evaluations(review_evaluations_params)
+      Support::Evaluator.where(email: filtered_params[:evaluation_approved]).update_all(evaluation_approved: true)
+    end
+
+    def evaluation_pending?
+      @current_case.evaluators.where(evaluation_approved: false).any?
     end
 
     def update_procurement_stage_if_needed
-      evaluation_count = @current_case.evaluators.all.count(&:evaluation_approved)
-
-      moderation_stage = if @current_case.evaluators.count == evaluation_count
-                           Support::ProcurementStage.find_by(stage: "3", title: "Moderation")
-                         else
+      moderation_stage = if evaluation_pending?
                            Support::ProcurementStage.find_by(stage: "3", title: "Stage 3 Evaluation")
+                         else
+                           Support::ProcurementStage.find_by(stage: "3", title: "Moderation")
                          end
       @current_case.update!(procurement_stage_id: moderation_stage.id) if moderation_stage
     end
 
-    def review_evaluation_params
-      params.require(:review_evaluations_form).permit(evaluation_approved: []).tap do |whitelisted|
-        whitelisted[:evaluation_approved].reject!(&:blank?) if whitelisted[:evaluation_approved].is_a?(Array)
-      end
+    def review_evaluations_params
+      params.require(:review_evaluations_form).permit(evaluation_approved: [])
+    end
+
+    def filter_blank_evaluations(params)
+      params[:evaluation_approved] = params[:evaluation_approved].reject(&:blank?)
+      params
     end
   end
 end
