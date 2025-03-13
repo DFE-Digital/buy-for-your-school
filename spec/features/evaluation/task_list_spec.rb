39e88d7232e79c1ca252e88920d6f6091ef33acf
edwin-jebaraj <edwin.jebaraj@education.gov.uk>
2025-03-13 14:27:49 +0000

    updated files to restrict upload option when evaluation is approved

--- spec/features/evaluation/task_list_spec.rb
+++ spec/features/evaluation/task_list_spec.rb
@@ -3,9 +3,12 @@ require "rails_helper"
 describe "Evaluator can see task list", :js do
   let(:support_case) { create(:support_case) }
   let(:user) { create(:user) }
+  let(:file_1) { fixture_file_upload(Rails.root.join("spec/fixtures/support/text-file.txt"), "text/plain") }
+  let(:file_2) { fixture_file_upload(Rails.root.join("spec/fixtures/support/another-text-file.txt"), "text/plain") }
+  let(:document_uploader) { support_case.document_uploader(files: [file_1, file_2]) }
+  let!(:support_evaluator) { create(:support_evaluator, support_case:, email: user.email, dsi_uid: user.dfe_sign_in_uid) }
 
   specify "Authenticating and seeing the task list" do
-    create(:support_evaluator, support_case:, dsi_uid: user.dfe_sign_in_uid, email: user.email)
     Current.user = user
     user_exists_in_dfe_sign_in(user:)
 
@@ -17,7 +20,6 @@ describe "Evaluator can see task list", :js do
   end
 
   specify "Authenticating when not an evaluator" do
-    create(:support_evaluator, support_case:)
     Current.user = user
     user_exists_in_dfe_sign_in(user:)
 
@@ -27,7 +29,6 @@ describe "Evaluator can see task list", :js do
   end
 
   specify "Verify evaluation unique link when evaluator already authenticated" do
-    create(:support_evaluator, support_case:, dsi_uid: user.dfe_sign_in_uid, email: user.email)
     Current.user = user
     user_exists_in_dfe_sign_in(user:)
 
@@ -39,7 +40,6 @@ describe "Evaluator can see task list", :js do
   end
 
   specify "Verify evaluation unique link when evaluator not authenticated" do
-    create(:support_evaluator, support_case:, dsi_uid: user.dfe_sign_in_uid, email: user.email)
     Current.user = user
     user_exists_in_dfe_sign_in(user:)
 
@@ -47,5 +47,49 @@ describe "Evaluator can see task list", :js do
 
     expect(page).to have_text("Complete procurement evaluation")
     expect(page).not_to have_text("Evaluator task list")
+  end
+
+  specify "Verify the upload completed documents link when evaluation is not approved" do
+    Current.user = user
+    user_exists_in_dfe_sign_in(user:)
+
+    user_is_signed_in(user:)
+
+    document_uploader.save!
+
+    visit evaluation_download_document_path(support_case)
+
+    find_all(".govuk-summary-list__row a")[0].click
+    find_all(".govuk-summary-list__row a")[1].click
+
+    support_evaluator.update!(has_uploaded_documents: true)
+
+    document_uploader.save_evaluation_document!(user.email, true)
+
+    visit evaluation_task_path(support_case)
+
+    expect(page).to have_link("Upload completed documents")
+  end
+
+  specify "Verify the upload completed documents link when evaluation is approved" do
+    Current.user = user
+    user_exists_in_dfe_sign_in(user:)
+
+    user_is_signed_in(user:)
+
+    document_uploader.save!
+
+    visit evaluation_download_document_path(support_case)
+
+    find_all(".govuk-summary-list__row a")[0].click
+    find_all(".govuk-summary-list__row a")[1].click
+
+    support_evaluator.update!(has_uploaded_documents: true, evaluation_approved: true)
+
+    document_uploader.save_evaluation_document!(user.email, true)
+
+    visit evaluation_task_path(support_case)
+
+    expect(page).not_to have_link("Upload completed documents")
   end
 end
