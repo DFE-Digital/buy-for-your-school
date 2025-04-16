65aa92421166cc81fb628f7edbd883b6a3cd77b1
Andy <andy@wireworldmedia.co.uk>
2025-04-16 13:23:16 +0100

    Finish guidance screen & rationalise some view components

--- app/components/paragraph_component.rb
+++ app/components/paragraph_component.rb
@@ -3,7 +3,12 @@
 # @para text can be a string or an array, in which case a paragraph is rendered
 # for each element
 class ParagraphComponent < ViewComponent::Base
-  def initialize(text:)
+  def initialize(text:, size: :m)
     @texts = [text].flatten
+    @class = {
+      s: "govuk-body-s",
+      m: "govuk-body",
+      l: "govuk-body-l",
+    }[size]
   end
 end
