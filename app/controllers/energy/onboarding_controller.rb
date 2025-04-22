d8f0b8a24493194542a65d647b13b54b9903019c
Andy <andy@wireworldmedia.co.uk>
2025-04-22 10:40:45 +0100

    Move flag checking from routes.rb to controller
    This was causing an error during assets:precompile

--- app/controllers/energy/onboarding_controller.rb
+++ app/controllers/energy/onboarding_controller.rb
@@ -1,7 +1,7 @@
 module Energy
   class OnboardingController < ApplicationController
     skip_before_action :authenticate_user!
-    before_action :set_routing
+    before_action :check_flag, :set_routing
 
     # /energy/onboarding/:step
     def show
@@ -9,6 +9,11 @@ module Energy
     end
 
     private
+
+    # Remove this and before_action reference when flag removed
+    def check_flag
+      render "errors/not_found", status: :not_found unless Flipper.enabled?(:energy)
+    end
 
     # This will probably end up as a separate class - a routing brain
     def set_routing
