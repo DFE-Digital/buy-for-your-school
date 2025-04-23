cd73e88b048db54f29f8cf853f8d1fbb36b57b58
Andy <andy@wireworldmedia.co.uk>
2025-04-23 12:18:27 +0100

    Appease rubocop

--- spec/factories/energy/onboarding_case_organisations.rb
+++ spec/factories/energy/onboarding_case_organisations.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
-  factory :energy_onboarding_case_organisation, class: 'Energy::OnboardingCaseOrganisation' do
+  factory :energy_onboarding_case_organisation, class: "Energy::OnboardingCaseOrganisation" do
     energy_onboarding_case { nil }
     support_case_organisation { nil }
     what_are_you_switching { "MyString" }
