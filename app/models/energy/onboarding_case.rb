2972c108d001c339aa7bff94e7d1162b919881ec
Andy <andy@wireworldmedia.co.uk>
2025-04-24 12:26:24 +0100

    Remove creation of Energy::OnboardingCaseOrganisation
    This has been moved to a separate branch

--- app/models/energy/onboarding_case.rb
+++ app/models/energy/onboarding_case.rb
@@ -1,4 +1,3 @@
 class Energy::OnboardingCase < ApplicationRecord
   belongs_to :support_case, class_name: "Support::Case"
-  has_many :energy_onboarding_case_organisations, class_name: "Energy::OnboardingCaseOrganisation", foreign_key: "energy_onboarding_case_id"
 end
--- app/models/support/case_organisation.rb
+++ app/models/support/case_organisation.rb
@@ -2,6 +2,5 @@ module Support
   class CaseOrganisation < ApplicationRecord
     belongs_to :case, class_name: "Support::Case", foreign_key: "support_case_id"
     belongs_to :organisation, class_name: "Support::Organisation", foreign_key: "support_organisation_id"
-    has_one :energy_onboarding_case_organisation, class_name: "Energy::OnboardingCaseOrganisation", foreign_key: "support_case_organisation_id"
   end
 end
