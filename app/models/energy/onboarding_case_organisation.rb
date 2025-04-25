7c334acd4f194c7992623a41eb956bebb4eae8ec
Andy <andy@wireworldmedia.co.uk>
2025-04-25 12:59:44 +0100

    Correct wrong association & convert to polymorphic

--- app/models/energy/onboarding_case_organisation.rb
+++ app/models/energy/onboarding_case_organisation.rb
@@ -2,18 +2,10 @@ class Energy::OnboardingCaseOrganisation < ApplicationRecord
   belongs_to :onboarding_case, class_name: "Energy::OnboardingCase", foreign_key: "energy_onboarding_case_id"
 
   # An onboarding organisation can be one of:
-  #   1. A single school
-  #   2. A school within a trust
-  #   3. A trust itself
-  # hence these 3 foreign keys, they're all 1:1 relations
-  #
-  # This could be a polymorphic relation but I'm keeping it plain until we've
-  # fully nailed down the data structure
-  #
-  # We should probably add an 'organisation_type' attribute to indicate what it is
-  belongs_to :support_organisation, class_name: "Support::Organisation", optional: true # if single school
-  belongs_to :support_case_organisation, class_name: "Support::CaseOrganisation", optional: true # if a school within a trust
-  belongs_to :support_establishment_group, class_name: "Support::EstablishmentGroup", optional: true # if a trust itself
+  #   1. A single school         --> polymorphic association to Support::Organisation
+  #   2. A school within a trust --> polymorphic association to Support::Organisation
+  #   3. A trust itself          --> polymorphic association to Support::EstablishmentGroup
+  belongs_to :onboardable, polymorphic: true
 
   has_many :gas_meters, class_name: "Energy::GasMeter", foreign_key: "energy_onboarding_case_organisation_id"
   has_many :electricity_meters, class_name: "Energy::ElectricityMeter", foreign_key: "energy_onboarding_case_organisation_id"
--- db/migrate/20250422162050_create_energy_onboarding_case_organisations.rb
+++ db/migrate/20250422162050_create_energy_onboarding_case_organisations.rb
@@ -2,11 +2,12 @@ class CreateEnergyOnboardingCaseOrganisations < ActiveRecord::Migration[7.2]
   def change
     create_table :energy_onboarding_case_organisations, id: :uuid do |t|
       t.references :energy_onboarding_case, null: true, type: :uuid
-      t.references :support_case_organisation, null: true, type: :uuid
-      t.references :support_organisation, null: true, type: :uuid
-      t.references :support_establishment_group, null: true, type: :uuid
+      t.uuid :onboardable_id
+      t.string :onboardable_type
       t.integer :switching_energy_type
       t.timestamps
+
+      t.index %i[onboardable_type onboardable_id]
     end
   end
 end
