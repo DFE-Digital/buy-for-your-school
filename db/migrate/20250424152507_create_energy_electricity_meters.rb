0082fbc6acbf9f5ec4dc9cc5261b0be1d0f36c3e
Andy <andy@wireworldmedia.co.uk>
2025-04-25 13:05:29 +0100

    Add missing null: false

--- db/migrate/20250424152507_create_energy_electricity_meters.rb
+++ db/migrate/20250424152507_create_energy_electricity_meters.rb
@@ -2,7 +2,7 @@ class CreateEnergyElectricityMeters < ActiveRecord::Migration[7.2]
   def change
     create_table :energy_electricity_meters, id: :uuid do |t|
       t.references :energy_onboarding_case_organisation, null: false, foreign_key: true, type: :uuid
-      t.string :mpan
+      t.string :mpan, null: false
 
       t.timestamps
     end
