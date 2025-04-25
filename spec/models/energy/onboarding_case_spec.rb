f46717647f1b4c26b2fc5bae59bdaffc8a831a6a
Andy <andy@wireworldmedia.co.uk>
2025-04-25 12:59:44 +0100

    Correct wrong association & convert to polymorphic

--- spec/models/energy/onboarding_case_spec.rb
+++ spec/models/energy/onboarding_case_spec.rb
@@ -5,42 +5,20 @@ RSpec.describe Energy::OnboardingCase, type: :model do
   # very trying.
   # Feel free to delete this down the road
   it "has working relations" do
+    # Given existing support entities
     support_case = create(:support_case)
-    expect(support_case).to be_persisted
-
-    oc = described_class.create!(support_case:, are_you_authorised: true)
-    expect(support_case.energy_onboarding_case).to eq(oc)
-
-    oco = oc.onboarding_case_organisations.create!(switching_energy_type: 1)
-    expect(oco).to be_persisted
-    expect(oco.onboarding_case).to eq(oc)
-
-    support_case_organisation = create(:support_case_organisation)
-    expect(support_case_organisation).to be_persisted
-
-    oco.support_case_organisation = support_case_organisation
-    oco.save!
-    expect(oco.support_case_organisation).to eq(support_case_organisation)
-    expect(support_case_organisation.energy_onboarding_case_organisation).to eq(oco)
-
     support_organisation = create(:support_organisation)
-    expect(support_organisation).to be_persisted
-
-    oco.support_organisation = support_organisation
-    oco.save!
-    expect(oco.support_organisation).to eq(support_organisation)
-    expect(support_organisation.energy_onboarding_case_organisation).to eq(oco)
-
     support_establishment_group = create(:support_establishment_group)
-    expect(support_establishment_group).to be_persisted
 
-    oco.support_establishment_group = support_establishment_group
-    oco.save!
-    expect(oco.support_establishment_group).to eq(support_establishment_group)
-    expect(support_establishment_group.energy_onboarding_case_organisation).to eq(oco)
+    # Create energy entites
+    onboarding_case = described_class.create!(support_case:, are_you_authorised: true)
+    oco1 = onboarding_case.onboarding_case_organisations.create!(switching_energy_type: 1, onboardable: support_organisation)
+    oco2 = onboarding_case.onboarding_case_organisations.create!(switching_energy_type: 2, onboardable: support_establishment_group)
 
-    elec_meter = oco.electricity_meters.create!(mpan: "12345")
-    expect(oco.electricity_meters).to eq([elec_meter])
-    expect(elec_meter.onboarding_case_organisation).to eq(oco)
+    # Check relations
+    expect(support_case.energy_onboarding_case).to eq(onboarding_case)
+    expect(onboarding_case.onboarding_case_organisations.count).to eq(2)
+    expect(support_organisation.energy_onboarding_case_organisation).to eq(oco1)
+    expect(support_establishment_group.energy_onboarding_case_organisation).to eq(oco2)
   end
 end
