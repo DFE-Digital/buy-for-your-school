9213c0b5b834415e582d2e7a3a0625dfcfa1f262
Andy <andy@wireworldmedia.co.uk>
2025-04-28 12:17:38 +0100

    Add foreign key constraints

--- db/migrate/20250424152507_create_energy_electricity_meters.rb
+++ db/migrate/20250424152507_create_energy_electricity_meters.rb
@@ -1,7 +1,7 @@
 class CreateEnergyElectricityMeters < ActiveRecord::Migration[7.2]
   def change
     create_table :energy_electricity_meters, id: :uuid do |t|
-      t.references :energy_onboarding_case_organisation, null: false, foreign_key: true, type: :uuid
+      t.references :energy_onboarding_case_organisation, foreign_key: true, type: :uuid
       t.string :mpan, null: false
 
       t.timestamps
