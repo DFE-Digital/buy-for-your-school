11c604f417c700ae7569d8c10ddb9cc0c2c0ccd0
Dinesh Anthony <danthony@contractor.indeed.com>
2025-04-29 12:08:27 +0100

    add both current gas and electricity fields

--- db/migrate/20250429102915_add_current_supplier_to_onboarding_case_organisations.rb
+++ db/migrate/20250429102915_add_current_supplier_to_onboarding_case_organisations.rb
@@ -1,6 +1,11 @@
 class AddCurrentSupplierToOnboardingCaseOrganisations < ActiveRecord::Migration[7.2]
   def change
-    add_column :energy_onboarding_case_organisations, :current_supplier, :integer, null: true
-    add_column :energy_onboarding_case_organisations, :other_supplier, :string
+    add_column :energy_onboarding_case_organisations, :gas_current_supplier, :integer
+    add_column :energy_onboarding_case_organisations, :gas_current_supplier_other, :string
+    add_column :energy_onboarding_case_organisations, :gas_current_contract_end_date, :date
+
+    add_column :energy_onboarding_case_organisations, :electric_current_supplier, :integer
+    add_column :energy_onboarding_case_organisations, :electric_current_supplier_other, :string
+    add_column :energy_onboarding_case_organisations, :electric_current_contract_end_date, :date
   end
 end
