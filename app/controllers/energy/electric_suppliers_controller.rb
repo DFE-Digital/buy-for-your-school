71a3154b8643ce87da3fda3d05e84d1dd549d830
Dinesh Anthony <danthony@contractor.indeed.com>
2025-05-05 13:42:35 +0100

    add current electricity supplier details

---
+++ app/controllers/energy/electric_suppliers_controller.rb
@@ -0,0 +1,41 @@
+module Energy
+  class ElectricSuppliersController < ApplicationController
+    include HasDateParams
+    before_action :organisation_details
+    before_action :form, only: %i[update]
+    before_action { @back_url = energy_case_switch_energy_path }
+
+    def show
+      @form = Energy::ElectricSupplierForm.new(**@onboarding_case_organisation.to_h.compact)
+    end
+
+    def update
+      if validation.success?
+        @onboarding_case_organisation.update!(**form.data)
+        redirect_to energy_case_electric_supplier_path
+        # single or multi meter site ticket controller
+      else
+        render :show
+      end
+    end
+
+  private
+
+    def form
+      @form = Energy::ElectricSupplierForm.new(
+        messages: validation.errors(full: true).to_h,
+        **validation.to_h,
+      )
+    end
+
+    def validation
+      @validation ||= Energy::ElectricSupplierFormSchema.new.call(**form_params)
+    end
+
+    def form_params
+      electric_supplier_params = params.fetch(:electric_supplier_form, {}).permit(*%i[electric_current_supplier electric_current_contract_end_date])
+      electric_supplier_params[:electric_current_contract_end_date] = date_param(:electric_supplier_form, :electric_current_contract_end_date)
+      electric_supplier_params
+    end
+  end
+end
