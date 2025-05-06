286a45ac5717296e46474f8a14c01e43f1469905
Dinesh Anthony <danthony@contractor.indeed.com>
2025-05-06 11:11:22 +0100

    move duplicate codes to shared context

--- spec/features/energy/current_electric_supplier_spec.rb
+++ spec/features/energy/current_electric_supplier_spec.rb
@@ -1,11 +1,7 @@
 require "rails_helper"
 
 describe "Current Electric supplier", :js do
-  let(:support_organisation) { create(:support_organisation, urn: 100_253) }
-  let(:user) { create(:user, :many_supported_schools_and_groups) }
-  let(:support_case) { create(:support_case, organisation: support_organisation) }
-  let(:onboarding_case) { create(:onboarding_case, support_case:) }
-  let(:case_organisation) { create(:energy_onboarding_case_organisation, onboarding_case:, onboardable: support_organisation) }
+  include_context "with energy suppliers"
 
   specify "Authenticating and seeing the current electric supplier" do
     Current.user = user
@@ -16,14 +12,9 @@ describe "Current Electric supplier", :js do
 
     expect(page).to have_text("#{support_organisation.name}: Current contract details")
     expect(page).to have_text("Current electricity supplier")
-
-    expect(page).to have_text("British Gas")
-    expect(page).to have_text("EDF Energy")
-    expect(page).to have_text("E.ON Next")
-    expect(page).to have_text("Scotish Power")
-    expect(page).to have_text("OVO Energy")
-    expect(page).to have_text("Octopus Energy")
-    expect(page).to have_text("Other")
+    expected_suppliers.each do |supplier|
+      expect(page).to have_text(supplier)
+    end
 
     expect(page).to have_text("When does the contract end?")
 
