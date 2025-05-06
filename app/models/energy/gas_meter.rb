415e4971849ef862c17bb1d7a4ce931733f0b605
edwin-jebaraj <edwin.jebaraj@education.gov.uk>
2025-05-06 10:45:56 +0100

    Updated files for MPRN summary page

--- app/models/energy/gas_meter.rb
+++ app/models/energy/gas_meter.rb
@@ -2,10 +2,20 @@ class Energy::GasMeter < ApplicationRecord
   belongs_to :onboarding_case_organisation, class_name: "Energy::OnboardingCaseOrganisation",
                                             foreign_key: "energy_onboarding_case_organisation_id"
 
+  MAX_METER_COUNT = 5
+
   validates :mprn,
             presence: true,
             format: { with: /\A[1-9][0-9]{5,11}\z/ }
   validates :gas_usage,
             presence: true,
             numericality: { greater_than_or_equal_to: 0, less_than: 1_000_000 }
+
+  validate :maximum_mprn_per_organisation, on: :create
+
+  def maximum_mprn_per_organisation
+    if onboarding_case_organisation.gas_meters.count >= MAX_METER_COUNT
+      errors.add(:base, :maximum_mprn_per_organisation)
+    end
+  end
 end
