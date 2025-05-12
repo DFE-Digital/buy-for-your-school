43a3649318f89338b939619384e28448676e0591
Dinesh Anthony <danthony@contractor.indeed.com>
2025-05-12 13:21:37 +0100

    ES-163 | Add energy Task list

--- spec/factories/energy/gas_meters.rb
+++ spec/factories/energy/gas_meters.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :energy_gas_meter, class: "Energy::GasMeter" do
     energy_onboarding_case_organisation { nil }
-    mprn { "MyString" }
+    mprn { "123456" }
+    gas_usage { 1000 }
   end
 end
