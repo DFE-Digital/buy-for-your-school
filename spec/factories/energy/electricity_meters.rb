9937ca5a088deaf4b95342eda90546d0ba12ccdd
Dinesh Anthony <danthony@contractor.indeed.com>
2025-05-12 15:54:06 +0100

    set task list based on switching_energy_type flag

--- spec/factories/energy/electricity_meters.rb
+++ spec/factories/energy/electricity_meters.rb
@@ -1,6 +1,8 @@
 FactoryBot.define do
   factory :energy_electricity_meter, class: "Energy::ElectricityMeter" do
     energy_onboarding_case_organisation { nil }
-    mpan { "MyString" }
+    mpan { "1234512345123" }
+    is_half_hourly { false }
+    electricity_usage { 1000 }
   end
 end
