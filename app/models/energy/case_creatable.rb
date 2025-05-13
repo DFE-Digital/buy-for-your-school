20c9b35b414f368817f334df622b30ac9d309213
Serena Abbott <serena.abbott@education.gov.uk>
2025-05-13 13:31:10 +0100

    Adds correct values to support case created via energy onboarding

--- app/models/energy/case_creatable.rb
+++ app/models/energy/case_creatable.rb
@@ -7,9 +7,14 @@ module Energy::CaseCreatable
         first_name: current_user.first_name,
         last_name: current_user.last_name,
         email: current_user.email,
-        source: :digital,
+        source: :energy_onboarding,
+        support_level: :L7,
+        category_id: Support::Category.find_by(title: "DfE Energy for Schools service").id,
+        organisation: support_organisation,
+        state: :on_hold,
       }
       kase = Support::CreateCase.new(attrs).call
+      Support::CreateInteraction.new(kase.id, "create_case", nil, { body: "DfE Energy support case created", additional_data: attrs.slice(:source, :category).compact }).call
       onboarding_case = Energy::OnboardingCase.create!(are_you_authorised: true, support_case: kase)
       Energy::OnboardingCaseOrganisation.create!(onboarding_case:, onboardable: support_organisation)
     end
