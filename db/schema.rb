102b5353e2e408863c48f8079f0d3893d6287900
edwin-jebaraj <edwin.jebaraj@education.gov.uk>
2025-05-13 16:03:36 +0100

    Added new files for site contact details

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_05_07_085245) do
+ActiveRecord::Schema[7.2].define(version: 2025_05_13_125900) do
   create_sequence "evaluation_refs"
   create_sequence "framework_refs"
 
@@ -251,6 +251,10 @@ ActiveRecord::Schema[7.2].define(version: 2025_05_07_085245) do
     t.boolean "gas_bill_consolidation"
     t.integer "electricity_meter_type"
     t.boolean "is_electric_bill_consolidated"
+    t.string "site_contact_first_name"
+    t.string "site_contact_last_name"
+    t.string "site_contact_email"
+    t.string "site_contact_phone"
     t.index ["energy_onboarding_case_id"], name: "idx_on_energy_onboarding_case_id_a2b87b0066"
     t.index ["onboardable_type", "onboardable_id"], name: "idx_on_onboardable_type_onboardable_id_aa8b300738"
   end
