6c34e9097f4c71cb65b97a0edbff1f9c3ab8f0ea
Serena Abbott <serena.abbott@education.gov.uk>
2025-05-13 13:31:10 +0100

    Adds correct values to support case created via energy onboarding

--- spec/features/energy/switch_energy_authorisation_spec.rb
+++ spec/features/energy/switch_energy_authorisation_spec.rb
@@ -1,19 +1,47 @@
 require "rails_helper"
 
 describe "Switch energy authorisation", :js do
-  let(:user) { create(:user) }
+  let(:support_organisation) { create(:support_organisation, urn: 100_253) }
+  let(:user) { create(:user, :many_supported_schools_and_groups) }
+  let(:category) { create(:support_category, title: "DfE Energy for Schools service") }
+
+  before do
+    allow(Support::Category).to receive(:find_by).with(title: "DfE Energy for Schools service").and_return(category)
+  end
 
   specify "Are you authorised to switch energy suppliers" do
     Current.user = user
     user_exists_in_dfe_sign_in(user:)
     user_is_signed_in(user:)
 
-    visit energy_school_selection_path
+    visit school_type_energy_authorisation_path(id: support_organisation.urn, type: "single")
 
-    choose "Supported School Name"
+    expect(page).to have_text("Are you authorised to switch energy suppliers for these schools?")
 
-    click_button "Continue"
+    click_link "Continue"
 
-    expect(page).not_to have_text("Are you authorised to switch energy suppliers for these schools?")
+    expect(page).to have_text("Are you switching electricity, gas or both?")
+
+    kase = Support::Case.last
+    expect(kase.first_name).to eq("first_name")
+    expect(kase.last_name).to eq("last_name")
+    expect(kase.email).to eq("test@test")
+    expect(kase.source).to eq("energy_onboarding")
+    expect(kase.support_level).to eq("L7")
+    expect(kase.category_id).to eq(category.id)
+    expect(kase.organisation).to eq(support_organisation)
+    expect(kase.state).to eq("on_hold")
+
+    interaction = Support::Interaction.last
+    expect(interaction.case_id).to eq(kase.id)
+    expect(interaction.body).to eq("DfE Energy support case created")
+
+    onboarding_case = Energy::OnboardingCase.last
+    expect(onboarding_case.are_you_authorised).to be true
+    expect(onboarding_case.support_case).to eq(kase)
+
+    onboarding_case_org = Energy::OnboardingCaseOrganisation.last
+    expect(onboarding_case_org.onboarding_case).to eq(onboarding_case)
+    expect(onboarding_case_org.onboardable).to eq(support_organisation)
   end
 end
