af24c1b22e5024f70a68f2ab303c9b3354f0d89f
Andy <andy@wireworldmedia.co.uk>
2025-05-20 11:01:17 +0100

    Use shared context in spec

--- spec/features/energy/billing_address_confirmation_spec.rb
+++ spec/features/energy/billing_address_confirmation_spec.rb
@@ -1,11 +1,8 @@
 require "rails_helper"
 
 describe "Billing address confirmation", :js do
-  let(:support_organisation) { create(:support_organisation, urn: 100_253) }
-  let(:user) { create(:user, :many_supported_schools_and_groups) }
-  let(:support_case) { create(:support_case, organisation: support_organisation) }
-  let(:onboarding_case) { create(:onboarding_case, support_case:) }
-  let(:case_organisation) { create(:energy_onboarding_case_organisation, onboarding_case:, onboardable: support_organisation) }
+  include_context "with energy suppliers"
+
   let(:formatted_address) { Support::OrganisationPresenter.new(support_organisation).formatted_address }
 
   specify "Select an address" do
