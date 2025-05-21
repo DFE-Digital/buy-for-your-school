40bbfb45355ba098ca118170a866ea748d9ab806
Dinesh Anthony <danthony@contractor.indeed.com>
2025-05-21 13:37:24 +0100

    add test to check feedback link

--- spec/features/energy/confirmations_spec.rb
+++ spec/features/energy/confirmations_spec.rb
@@ -12,5 +12,9 @@ describe "School selection", :js do
     expect(page).to have_text("Information submitted")
     expect(page).to have_text("We've sent you an email containing a copy of this form and details about your new contract.")
     expect(page).to have_text("Depending on your contract’s end date, you may be placed on an interim contract to ensure continued supply to your school.")
+
+    click_link "What did you think of this service?"
+    expect(page).to have_text("Get help buying for schools feedback")
+    expect(page).to have_text("Thank you for taking the time to give us your feedback.")
   end
 end
