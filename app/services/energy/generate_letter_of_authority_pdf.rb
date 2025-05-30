3a16ad9db8a302a2d184cc41b4688b23f99f8f2e
Dinesh Anthony <danthony@contractor.indeed.com>
2025-05-30 13:22:36 +0100

    add specs ad refactor

--- app/services/energy/generate_letter_of_authority_pdf.rb
+++ app/services/energy/generate_letter_of_authority_pdf.rb
@@ -6,6 +6,16 @@ module Energy
     end
 
     def call
+      pdf_data = generate_pdf_data
+      write_pdf_to_file(pdf_data)
+      attach_pdf_to_case
+    ensure
+      delete_temp_file
+    end
+
+  private
+
+    def generate_pdf_data
       html = ApplicationController.render(
         template: "energy/letter_of_authorisations/loa_pdf",
         layout: "pdf",
@@ -15,20 +25,39 @@ module Energy
         },
       )
 
-      WickedPdf.new.pdf_from_string(
-        html,
-        encoding: "UTF-8",
-        page_size: "A4",
-        # margin: { top: 20, bottom: 20, left: 20, right: 20 },
+      WickedPdf.new.pdf_from_string(html, encoding: "UTF-8", page_size: "A4")
+    end
+
+    def write_pdf_to_file(data)
+      File.binwrite(file_path, data)
+    end
+
+    def attach_pdf_to_case
+      @support_case.case_attachments.create!(
+        attachable: document,
+        custom_name: file_name,
+        description: "System uploaded document",
       )
+    end
+
+    def document
+      file = File.open(file_path)
+      Support::Document.create!(case: @support_case, file_type: "application/pdf", file:)
+    end
+
+    def file_name
+      "DfE Energy for Schools Letter of Agreement_#{@support_case.ref}_#{Date.current}.pdf"
+    end
+
+    def file_path
+      Rails.root.join("tmp", file_name)
+    end
+
+    def delete_temp_file
+      File.delete(file_path) if File.exist?(file_path)
     end
   end
 end
 
-# loa_submitted and update successfully.
-# Redirect to submitted page
-# At background
-# generate LOA pdf
-# attached LOA to support case
-# Draft email
-# send email with LOA pdf attachment
+# onboarding_case_organisation = Energy::OnboardingCaseOrganisation.last
+# Energy::GenerateLetterOfAuthorityPdf.new(onboarding_case_organisation).call
