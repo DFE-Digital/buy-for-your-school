04ae4ecf19791f7c595bccc76cf110722fe59d90
Serena Abbott <serena.abbott@education.gov.uk>
2025-05-30 17:33:51 +0100

    Add redirect and error message if tasks not complete from task list

--- app/controllers/energy/tasks_controller.rb
+++ app/controllers/energy/tasks_controller.rb
@@ -6,13 +6,25 @@ module Energy
     def show; end
 
     def update
-      render :show
+      if all_tasks_complete?
+        redirect_to energy_case_check_your_answers_path
+      else
+        flash[:error] = { message: I18n.t("energy.tasks.error"), class: "govuk-error" }
+        render :show
+        flash.clear
+      end
     end
 
   private
 
     def set_task_list
       @task_list = Energy::TaskList.new(@onboarding_case_organisation.energy_onboarding_case_id).call
+    end
+
+    def all_tasks_complete?
+      incomplete_tasks = @task_list.select { |task| %i[in_progress not_started].include?(task.status) }
+
+      incomplete_tasks.none?
     end
   end
 end
