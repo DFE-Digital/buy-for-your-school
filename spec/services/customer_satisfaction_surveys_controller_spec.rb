070d4e419f00e1500ff4ff0bfaa94c48b3e78faa
Bhavana <bhavana.kaushik@gmail.com>
2025-06-04 15:44:38 +0100

    Fixed rubocop errors

--- spec/services/customer_satisfaction_surveys_controller_spec.rb
+++ spec/services/customer_satisfaction_surveys_controller_spec.rb
@@ -1,20 +1,21 @@
 require "rails_helper"
 
   describe CustomerSatisfactionSurveysController, type: :controller do
-    let(:find_a_framework_flow) { %w[recommendation_likelihood improvement easy_to_use_rating helped_how clear_to_use_rating research_opt_in satisfaction_level thank_you]}
-    let(:create_a_spec_flow) { %w[recommendation_likelihood improvement easy_to_use_rating helped_how clear_to_use_rating research_opt_in satisfaction_level thank_you]}
-    let(:supported_journey_flow) { %w[recommendation_likelihood improvement easy_to_use_rating helped_how clear_to_use_rating research_opt_in satisfaction_level thank_you]}
-    let(:request_for_help_form_flow) { %w[recommendation_likelihood improvement easy_to_use_rating helped_how clear_to_use_rating research_opt_in satisfaction_level thank_you]}
-    let(:energy_for_schools_flow) { %w[recommendation_likelihood improvement easy_to_use_rating helped_how clear_to_use_rating research_opt_in satisfaction_level thank_you]}
-    let(:find_a_buying_solution_flow) { %w[satisfaction_level easy_to_use_rating helped_how clear_to_use_rating recommendation_likelihood improvement thank_you]}
+    let(:find_a_framework_flow) { %w[recommendation_likelihood improvement easy_to_use_rating helped_how clear_to_use_rating research_opt_in satisfaction_level thank_you] }
+    let(:create_a_spec_flow) { %w[recommendation_likelihood improvement easy_to_use_rating helped_how clear_to_use_rating research_opt_in satisfaction_level thank_you] }
+    let(:supported_journey_flow) { %w[recommendation_likelihood improvement easy_to_use_rating helped_how clear_to_use_rating research_opt_in satisfaction_level thank_you] }
+    let(:request_for_help_form_flow) { %w[recommendation_likelihood improvement easy_to_use_rating helped_how clear_to_use_rating research_opt_in satisfaction_level thank_you] }
+    let(:energy_for_schools_flow) { %w[recommendation_likelihood improvement easy_to_use_rating helped_how clear_to_use_rating research_opt_in satisfaction_level thank_you] }
+    let(:find_a_buying_solution_flow) { %w[satisfaction_level easy_to_use_rating helped_how clear_to_use_rating recommendation_likelihood improvement thank_you] }
 
     describe "questions_flow" do
       context "when the customer survey is created for find_a_buying_solution" do
         let(:survey) { create(:customer_satisfaction_survey_response, source: :exit_survey) }
+
         it "goes back to the first customer satisfaction question" do
           controller.set_service("find_a_buying_solution")
           flow = controller.get_flow
-          current_path =  controller.get_flow.current_path
+          current_path = controller.get_flow.current_path
           back_url = controller.get_flow.back_path
           next_path = controller.get_flow.next_path
 
@@ -28,10 +29,11 @@ require "rails_helper"
 
       context "when the customer survey is created for find_a_framework_flow" do
         let(:survey) { create(:customer_satisfaction_survey_response, source: :exit_survey) }
+
         it "goes back to the first customer satisfaction question" do
           controller.set_service("find_a_framework")
           flow = controller.get_flow
-          current_path =  controller.get_flow.current_path
+          current_path = controller.get_flow.current_path
           back_url = controller.get_flow.back_path
           next_path = controller.get_flow.next_path
 
@@ -45,10 +47,11 @@ require "rails_helper"
 
     context "when the customer survey is created for create_a_spec" do
       let(:survey) { create(:customer_satisfaction_survey_response, source: :exit_survey) }
+
       it "goes back to the first customer satisfaction question" do
         controller.set_service("create_a_spec")
         flow = controller.get_flow
-        current_path =  controller.get_flow.current_path
+        current_path = controller.get_flow.current_path
         back_url = controller.get_flow.back_path
         next_path = controller.get_flow.next_path
 
@@ -61,10 +64,11 @@ require "rails_helper"
 
     context "when the customer survey is created for supported_journey" do
       let(:survey) { create(:customer_satisfaction_survey_response, source: :exit_survey) }
+
       it "goes back to the first customer satisfaction question" do
         controller.set_service("supported_journey")
         flow = controller.get_flow
-        current_path =  controller.get_flow.current_path
+        current_path = controller.get_flow.current_path
         back_url = controller.get_flow.back_path
         next_path = controller.get_flow.next_path
 
@@ -77,10 +81,11 @@ require "rails_helper"
 
     context "when the customer survey is created for request_for_help_form" do
       let(:survey) { create(:customer_satisfaction_survey_response, source: :exit_survey) }
+
       it "goes back to the first customer satisfaction question" do
         controller.set_service("request_for_help_form")
         flow = controller.get_flow
-        current_path =  controller.get_flow.current_path
+        current_path = controller.get_flow.current_path
         back_url = controller.get_flow.back_path
         next_path = controller.get_flow.next_path
 
@@ -93,10 +98,11 @@ require "rails_helper"
 
     context "when the customer survey is created for energy_for_schools" do
       let(:survey) { create(:customer_satisfaction_survey_response, source: :exit_survey) }
+
       it "goes back to the first customer satisfaction question" do
         controller.set_service("energy_for_schools")
         flow = controller.get_flow
-        current_path =  controller.get_flow.current_path
+        current_path = controller.get_flow.current_path
         back_url = controller.get_flow.back_path
         next_path = controller.get_flow.next_path
 
@@ -107,4 +113,3 @@ require "rails_helper"
       end
     end
   end
-
