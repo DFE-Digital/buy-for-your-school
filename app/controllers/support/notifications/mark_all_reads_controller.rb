aa6cf9131971f9cb65deafa15c26fda136d75691
edwin-jebaraj <edwin.jebaraj@education.gov.uk>
2025-06-05 13:35:41 +0100

    Updated files to enable notifications to the cec portal

--- app/controllers/support/notifications/mark_all_reads_controller.rb
+++ app/controllers/support/notifications/mark_all_reads_controller.rb
@@ -1,9 +1,15 @@
 module Support
-  class Notifications::MarkAllReadsController < ApplicationController
+  class Notifications::MarkAllReadsController < Support::NotificationsController
     def create
       current_agent.assigned_to_notifications.mark_as_read
 
-      redirect_to support_notifications_path
+      redirect_to redirect_path
+    end
+
+  private
+
+    def redirect_path
+      (current_agent.roles & %w[cec cec_admin]).any? ? cec_notifications_path : support_notifications_path
     end
   end
 end
--- app/controllers/support/notifications_controller.rb
+++ app/controllers/support/notifications_controller.rb
@@ -6,5 +6,17 @@ module Support
         .order("created_at DESC")
         .paginate(page: params[:page])
     end
+
+  private
+
+    def authorize_agent_scope = :access_individual_cases?
+
+    def notifications_portal
+      (current_agent.roles & %w[cec cec_admin]).any? ? "cec" : "support"
+    end
+
+    helper_method def portal_notifications_mark_all_read_path
+      send("#{notifications_portal}_notifications_mark_all_read_path")
+    end
   end
 end
