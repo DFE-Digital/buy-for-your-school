d92ef42dafed6a7182eeb0a446b4832f4d4980a1
edwin-jebaraj <edwin.jebaraj@education.gov.uk>
2025-06-09 17:07:30 +0100

    enabled quick edit for cec onboarding cases

---
+++ spec/features/cec/cec_agent_can_quick_edit_case_spec.rb
@@ -0,0 +1,38 @@
+require "rails_helper"
+
+describe "Agent can quick edit a case", :js do
+  include_context "with a cec agent"
+
+  let(:dfe_energy_category) { create(:support_category, title: "DfE Energy for Schools service") }
+  let(:energy_stage) { create(:support_procurement_stage, key: "onboarding_form", title: "Onboarding form", stage: "6") }
+  let(:support_case) { create(:support_case, ref: "000001", agent:, category: dfe_energy_category, support_level: "L6", source: :energy_onboarding, procurement_stage: energy_stage) }
+
+  before do
+    define_basic_categories
+    define_basic_procurement_stages
+    support_case
+
+    visit cec_onboarding_cases_path(anchor: "my-cases")
+    within("#my-cases") { click_link "Quick edit" }
+  end
+
+  context "when changing the note, next key date, and 'with school' flag" do
+    before do
+      fill_in "Add a note to case 000001", with: "New note"
+      choose "7 - DfE Energy for Schools onboarding case"
+      fill_in "Day", with: "10"
+      fill_in "Month", with: "08"
+      fill_in "Year", with: "2026"
+      fill_in "Description of next key date", with: "Key event"
+      choose "Yes"
+      click_button "Save"
+    end
+
+    it "persists the changes" do
+      expect(support_case.reload.support_level).to eq("L7")
+      expect(support_case.reload.next_key_date).to eq(Date.parse("2026-08-10"))
+      expect(support_case.reload.next_key_date_description).to eq("Key event")
+      expect(support_case.reload.with_school).to eq(true)
+    end
+  end
+end
