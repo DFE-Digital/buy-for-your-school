4574954a0dad85275eb6afa577f910c32c28b1de
Dinesh Anthony <danthony@contractor.indeed.com>
2025-06-17 10:56:36 +0100

    send form energy submission email

--- spec/factories/energy/onboarding_cases.rb
+++ spec/factories/energy/onboarding_cases.rb
@@ -2,5 +2,9 @@ FactoryBot.define do
   factory :onboarding_case, class: "Energy::OnboardingCase" do
     association :support_case
     are_you_authorised { true }
+
+    trait :submitted do
+      submitted_at { Time.zone.now }
+    end
   end
 end
