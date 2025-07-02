ee57a179e36a229f8b3dea6e028cc84141c2113a
edwin-jebaraj <edwin.jebaraj@education.gov.uk>
2025-07-02 15:29:12 +0100

    Updated CEC Email Template Filter files to include Subgroup functionality

--- spec/services/support/emails/templates/seed_groups_spec.rb
+++ spec/services/support/emails/templates/seed_groups_spec.rb
@@ -8,8 +8,8 @@ describe Support::Emails::Templates::SeedGroups do
 
   it "populates the tables" do
     expect { service.call }
-      .to change(groups, :count).from(0).to(6)
-      .and change(sub_groups, :count).from(0).to(13)
+      .to change(groups, :count).from(0).to(7)
+      .and change(sub_groups, :count).from(0).to(14)
   end
 
   context "when a group has sub-groups" do
