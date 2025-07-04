60971df2e73ababd84e40e338318fd275677b390
Dinesh Anthony <danthony@contractor.indeed.com>
2025-07-04 17:27:11 +0100

    update edf portal access and fix specs

--- app/services/energy/documents/portal_access_form_edf.rb
+++ app/services/energy/documents/portal_access_form_edf.rb
@@ -11,11 +11,12 @@ module Energy
       STARTING_ROW_NUMBER = 1
       WORKSHEET_INDEX = 0
 
-      def initialize(onboarding_case:)
+      def initialize(onboarding_case:, current_user:)
         @onboarding_case = onboarding_case
         @support_case = onboarding_case.support_case
         @organisation = @support_case.organisation
         @onboarding_case_organisation = onboarding_case.onboarding_case_organisations.first
+        @current_user = current_user
       end
 
       def call
@@ -38,7 +39,8 @@ module Energy
       end
 
       def output_file_xl
-        @output_file_xl ||= OUTPUT_XL_PATH.join("Total portal Access_#{@support_case.ref}_#{Date.current}.xlsx")
+        # @output_file_xl ||= OUTPUT_XL_PATH.join("EDF Power portal Access_#{@support_case.ref}_#{Date.current}.xlsx")
+        @output_file_xl ||= OUTPUT_XL_PATH.join("edf_access_#{@support_case.ref}_#{Date.current}.xlsx")
       end
 
     private
@@ -55,25 +57,57 @@ module Energy
         @electricity_meters ||= Energy::ElectricityMeter.includes(:onboarding_case_organisation).where(energy_onboarding_case_organisation_id: @onboarding_case_organisation.id)
       end
 
-      def portal_access_data(electricity_meter, onboarding_org)
+      def portal_access_data(electricity_meter)
         {
           "Account No" => "",
-          "5% VAT (Y/N)" => onboarding_org.vat_rate == 5 ? "Y" : "N",
+          "5% VAT (Y/N)" => vat_rate_yes_no,
           "CCL Exempt (Y/N)" => "Y",
-          "Direct Debit (Y/N)" => onboarding_org.billing_payment_method == "direct_debit" ? "Y" : "N",
-          "Trust" => "foo",
-          "School name" => onboarding_org.onboardable.name,
-          "post code" => onboarding_org.onboardable.postcode,
+          "Direct Debit (Y/N)" => direct_debit_yes_no,
+          "Trust" => establishment_group&.name,
+          "School name" => @organisation&.name,
+          "post code" => postcode,
           "Power MPAN" => electricity_meter.mpan,
-          "Start date" => onboarding_org.electric_current_contract_end_date + 1,
+          "Start date" => supply_start_date,
           "Joining Type" => "Site Addition",
-          "Email Address for supplier portal access" => "foo",
-          "Title" => "foo",
-          "Forename" => "foo",
-          "Surname" => "foo",
+          "Email Address for supplier portal access" => email_addresses,
+          "Title" => "",
+          "Forename" => @current_user.first_name,
+          "Surname" => @current_user.last_name,
           "Phone Number" => "foo",
           "Job Title" => "foo",
         }
+      end
+
+      def establishment_group
+        @establishment_group ||= Support::EstablishmentGroup.find_by(uid: @organisation.trust_code)
+      end
+
+      def postcode
+        @organisation.address["postcode"]
+      end
+
+      def contract_end_date
+        @contract_end_date ||= @onboarding_case_organisation.gas_current_contract_end_date
+      end
+
+      def supply_start_date
+        (contract_end_date + 1.day).strftime("%d/%m/%Y")
+      end
+
+      def email_addresses
+        @current_user.email
+      end
+
+      def vat_rate_yes_no
+        @onboarding_case_organisation.vat_rate == 5 ? "Y" : "N"
+      end
+
+      def direct_debit_yes_no
+        @onboarding_case_organisation.billing_payment_method_direct_debit? ? "Y" : "N"
+      end
+
+      def access_type
+        @onboarding_case_organisation.billing_invoicing_method_email? ? "Email Push" : "Email Pull"
       end
     end
   end
