9eb66c2e29a9029aa265192f0c0d521b8e0e70cf
Dinesh Anthony <danthony@contractor.indeed.com>
2025-07-16 17:16:19 +0100

    ES-588 | Allow Milldene Primary School to resubmit energy form

---
+++ db/migrate/20250716154247_revert_form_submitted_at_to_case_6299.rb
@@ -0,0 +1,32 @@
+class RevertFormSubmittedAtToCase6299 < ActiveRecord::Migration[7.2]
+  def up
+    update_support_case
+  end
+
+  def down; end
+
+private
+
+  def update_support_case
+    return if support_case.nil?
+
+    onboarding_case = support_case.energy_onboarding_case
+    return if onboarding_case.nil?
+
+    onboarding_case.update!(form_submitted_email_sent: false, submitted_at: nil)
+  end
+
+  def support_case
+    Support::Case.find_by(ref: case_ref)
+  end
+
+  def case_ref
+    if ENV["APPLICATION_URL"]&.include? "dev"
+      "000804"
+    elsif ENV["APPLICATION_URL"]&.include? "staging"
+      "000839"
+    else
+      "006299"
+    end
+  end
+end
