62d9c68e2e745a953983b182e107a4cbb0ebb3a6
Dinesh Anthony <anthony.dinesh16@gmail.com>
2025-08-05 10:31:30 +0100

    allow CEC user to change contact name and email to enquiry case

--- spec/features/cec/case_school_details_spec.rb
+++ spec/features/cec/case_school_details_spec.rb
@@ -8,26 +8,52 @@ describe "Case school details", :js do
   end
 
   context "when the case is an energy for schools case" do
+    let(:support_level) { "L7" }
     let(:dfe_energy_category) { create(:support_category, title: "DfE Energy for Schools service") }
-    let(:support_case) { create(:support_case, category: dfe_energy_category, organisation: support_organisation, first_name: "Bill", last_name: "Jones", email: "bill.jones@email.com") }
+    let(:support_case) { create(:support_case, category: dfe_energy_category, organisation: support_organisation, first_name: "Bill", last_name: "Jones", email: "bill.jones@email.com", support_level:) }
 
-    it "has no change links" do
-      within "#school-details" do
-        expect(all("dt.govuk-summary-list__key")[0]).to have_text "Contact name"
-        expect(all("dd.govuk-summary-list__value")[0]).to have_text "Bill Jones"
-        expect(all("dd.govuk-summary-list__actions")[0]).not_to have_link "Change"
+    context "when the case is energy sub_category" do
+      let(:summary_list_keys) { "dt.govuk-summary-list__key" }
+      let(:summary_list_values) { "dd.govuk-summary-list__value" }
+      let(:summary_list_actions) { "dd.govuk-summary-list__actions" }
 
-        expect(all("dt.govuk-summary-list__key")[1]).to have_text "Contact email"
-        expect(all("dd.govuk-summary-list__value")[1]).to have_text "bill.jones@email.com"
-        expect(all("dd.govuk-summary-list__actions")[1]).not_to have_link "Change"
+      it "has school details fields and values" do
+        within "#school-details" do
+          expect(all(summary_list_keys)[0]).to have_text "Contact name"
+          expect(all(summary_list_values)[0]).to have_text "Bill Jones"
 
-        expect(all("dt.govuk-summary-list__key")[2]).to have_text "Organisation name"
-        expect(all("dd.govuk-summary-list__value")[2]).to have_text "School #1"
-        expect(all("dd.govuk-summary-list__actions")[2]).not_to have_link "Change"
+          expect(all(summary_list_keys)[1]).to have_text "Contact email"
+          expect(all(summary_list_values)[1]).to have_text "bill.jones@email.com"
 
-        expect(all("dt.govuk-summary-list__key")[3]).to have_text "Organisation type"
-        expect(all("dd.govuk-summary-list__value")[3]).to have_text "name 1"
-        expect(all("dd.govuk-summary-list__actions")[3]).not_to have_link "Change"
+          expect(all(summary_list_keys)[2]).to have_text "Organisation name"
+          expect(all(summary_list_values)[2]).to have_text "School #1"
+
+          expect(all(summary_list_keys)[3]).to have_text "Organisation type"
+          expect(all(summary_list_values)[3]).to have_text "name 1"
+        end
+      end
+
+      context "when the support level is L7" do
+        it "has no change links" do
+          within "#school-details" do
+            expect(all(summary_list_actions)[0]).not_to have_link "Change" # Contact name
+            expect(all(summary_list_actions)[1]).not_to have_link "Change" # Contact email
+            expect(all(summary_list_actions)[2]).not_to have_link "Change" # Organisation name
+            expect(all(summary_list_actions)[3]).not_to have_link "Change" # Organisation type
+          end
+        end
+      end
+
+      context "when the support level is NOT L7 (inquiry case)" do
+        let(:support_level) { "L6" }
+
+        it "has change links" do
+          within "#school-details" do
+            expect(all(summary_list_actions)[0]).to have_link "Change"
+            expect(all(summary_list_actions)[1]).to have_link "Change"
+            expect(all(summary_list_actions)[2]).to have_link "Change"
+          end
+        end
       end
     end
 
