cf978f6627f3268ba94fc35b354cba58adb462d6
Dinesh Anthony <anthony.dinesh16@gmail.com>
2025-08-11 16:22:53 +0100

    ES-706 | Ability for Proc-ops to create email templates for CEC use

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_07_16_154247) do
+ActiveRecord::Schema[7.2].define(version: 2025_08_11_151829) do
   create_sequence "evaluation_refs"
   create_sequence "framework_refs"
 
