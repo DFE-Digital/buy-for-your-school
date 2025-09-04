2b9f1ce6218dd3f272b5afaf0e4f8ae468f08e1a
edwin-jebaraj <edwin.jebaraj@education.gov.uk>
2025-09-04 17:06:22 +0100

    Updated files for auto-sending DD to school – Gas (Total)

--- app/models/email/template_parser.rb
+++ app/models/email/template_parser.rb
@@ -13,6 +13,7 @@ class Email::TemplateParser
       "electricity_contract_start_date" => "{{electricity_contract_start_date}}",
       "case_creator_first_name" => "{{case_creator_first_name}}",
       "electricity_contract_end_date" => "{{electricity_contract_end_date}}",
+      "gas_contract_end_date" => "{{gas_contract_end_date}}",
       "billing_payment_terms" => "{{billing_payment_terms}}",
     }.merge(variables)
   end
