6a5ec4075d609c3d61fa43083e4e8cc54be78c4b
Serena Abbott <serena.abbott@education.gov.uk>
2025-09-09 15:35:56 +0100

    Fix test running errors

--- spec/support/capybara_driver_helper.rb
+++ spec/support/capybara_driver_helper.rb
@@ -5,6 +5,7 @@ Capybara.register_driver :headless_chrome do |app|
   chrome_options.add_argument("no-sandbox")
   chrome_options.add_argument("headless")
   chrome_options.add_argument("disable-gpu")
+  chrome_options.add_argument("disable-dev-shm-usage")
   chrome_options.add_argument("window-size=1920,1080")
   # chrome_options.add_option("goog:loggingPrefs", browser: "ALL")
 
