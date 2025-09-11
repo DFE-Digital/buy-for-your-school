513ea6e895853c4cd91fdfdbf490e1db01a1dcf8
Dinesh Anthony <anthony.dinesh16@gmail.com>
2025-09-11 18:29:23 +0100

    ES-722, ES-723 | generate site addition and portal access forms and send email to supplier, this will be handled in a single background job now.

---
+++ app/jobs/energy/generate_site_addition_and_portal_access_xl_documents_job.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Energy
+  class GenerateSiteAdditionAndPortalAccessXlDocumentsJob < ApplicationJob
+    queue_as :default
+
+    def perform(onboarding_case_id:, current_user_id:)
+      onboarding_case = Energy::OnboardingCase.find(onboarding_case_id)
+      current_user = User.find(current_user_id)
+
+      Energy::GenerateSiteAdditionAndPortalAccessXlDocuments.new(onboarding_case:, current_user:).call
+    rescue ActiveRecord::RecordNotFound => e
+      Rails.logger.error("GenerateSiteAdditionAndPortalAccessXlDocuments failed: #{e.message}")
+    rescue StandardError => e
+      Rails.logger.error("Unhandled error: #{e.message}")
+      raise e
+    end
+  end
+end
