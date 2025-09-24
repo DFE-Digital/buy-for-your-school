08c0406a2a1bf742043e50daec944bfbe0a32ddd
Andy <andy@wireworldmedia.co.uk>
2025-09-24 12:58:00 +0100

    ES-747: Added form validation

--- app/controllers/energy/mat_school_picker_controller.rb
+++ app/controllers/energy/mat_school_picker_controller.rb
@@ -1,26 +1,36 @@
 module Energy
   class MatSchoolPickerController < ApplicationController
-    before_action :form
+    before_action :back_url, :mat_schools, :form
     skip_before_action :check_if_submitted
 
     # GET /energy/which-mat-schools-buying-for/:uid
-    def edit
-      @back_url = energy_school_selection_path
-      @group = Support::EstablishmentGroup.find_by(uid: params[:uid])
-      @mat_schools = @group.organisations_for_multi_school_picker.order(:name)
-    end
+    def edit; end
 
     # POST /energy/which-mat-schools-buying-for/:uid
     def update
       mat_school_urn = params[:mat_school_picker_form][:mat_school_urn]
+      @form.mat_school_urn = mat_school_urn
 
-      redirect_to school_type_energy_authorisation_path(id: mat_school_urn, type: "single")
+      if @form.validate
+        redirect_to school_type_energy_authorisation_path(id: mat_school_urn, type: "single")
+      else
+        render :edit
+      end
     end
 
   private
 
     def form
-      @form = nil
+      @form = MatSchoolPickerForm.new(current_user: UserPresenter.new(current_user))
+    end
+
+    def back_url
+      @back_url ||= energy_school_selection_path
+    end
+
+    def mat_schools
+      @group ||= Support::EstablishmentGroup.find_by(uid: params[:uid])
+      @mat_schools ||= @group.organisations_for_multi_school_picker.order(:name)
     end
   end
 end
