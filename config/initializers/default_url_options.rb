6a92ae9aba5ab474f3cb775d22f7d900da5ac8e3
Serena Abbott <serena.abbott@education.gov.uk>
2025-10-16 16:24:02 +0100

    Redirects from old domain and subdirectories to new

--- config/initializers/default_url_options.rb
+++ config/initializers/default_url_options.rb
@@ -1,7 +1,13 @@
 if ENV["APPLICATION_URL"]
-  uri = URI(ENV["APPLICATION_URL"])
+  first_url = ENV["APPLICATION_URL"].to_s.split(",").map(&:strip).first
 
-  Rails.application.routes.default_url_options[:host] = uri.host
-  Rails.application.routes.default_url_options[:port] = uri.port
-  Rails.application.routes.default_url_options[:protocol] = uri.scheme
+  begin
+    uri = URI.parse(first_url)
+
+    Rails.application.routes.default_url_options[:host] = uri.host
+    Rails.application.routes.default_url_options[:port] = uri.port unless [80, 443].include?(uri.port)
+    Rails.application.routes.default_url_options[:protocol] = uri.scheme
+  rescue URI::InvalidURIError => e
+    Rails.logger.warn("[default_url_options] Invalid APPLICATION_URL: #{first_url} (#{e.message})")
+  end
 end
