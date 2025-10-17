9c2bd29e9b519171182b4c0666f5431eb7c3cfd7
Dinesh Anthony <anthony.dinesh16@gmail.com>
2025-10-17 09:36:57 +0100

    add conditional form submission email templates

--- app/services/energy/emails/onboarding_form_submission_mailer.rb
+++ app/services/energy/emails/onboarding_form_submission_mailer.rb
@@ -1,25 +1,25 @@
 module Energy
   class Emails::OnboardingFormSubmissionMailer < Energy::Emails::BaseMailer
     FORM_SUBMISSION_EMAIL_TEMPLATE = "Email notification of Energy for Schools form completed".freeze
-
-    def default_template
-      ApplicationController.renderer.render(
-        partial: "energy/letter_of_authorisations/onboarding_form_submission_email_template",
-      )
-    end
+    FORM_SUBMISSION_EMAIL_TEMPLATE_NEW = "AUTO usage: Email notification of Energy for Schools form completed".freeze
 
   private
 
     def default_email_subject = "Case #{@support_case.ref} - form submission: Energy for Schools"
 
     def email_template
-      @email_template ||= Support::EmailTemplate.find_by(title: FORM_SUBMISSION_EMAIL_TEMPLATE, archived: false)
+      cms_template = Flipper.enabled?(:auto_email_vat_dd) ? FORM_SUBMISSION_EMAIL_TEMPLATE_NEW : FORM_SUBMISSION_EMAIL_TEMPLATE
+      @email_template ||= Support::EmailTemplate.find_by(title: cms_template, archived: false)
     end
 
     def default_email_template
-      ApplicationController.renderer.render(
-        partial: "energy/letter_of_authorisations/onboarding_form_submission_email_template",
-      )
+      ruby_template = if Flipper.enabled?(:auto_email_vat_dd)
+                        "energy/letter_of_authorisations/onboarding_form_submission_email_template_new"
+                      else
+                        "energy/letter_of_authorisations/onboarding_form_submission_email_template"
+                      end
+
+      ApplicationController.renderer.render(partial: ruby_template)
     end
 
     def parse_template
