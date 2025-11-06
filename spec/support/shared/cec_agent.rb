20d7710e85de944c5077165cf5989df1f8007a87
hrmtl <hrishi.mittal@education.gov.uk>
2025-11-06 17:01:33 +0000

    Add sign in step for shared cms helpers

--- spec/support/shared/cec_agent.rb
+++ spec/support/shared/cec_agent.rb
@@ -12,6 +12,7 @@ RSpec.shared_context "with a cec agent" do |roles: %w[cec]|
   before do
     Current.actor = agent
     user_exists_in_dfe_sign_in(user:)
+    user_is_signed_in(user:)
     visit "/cms"
   end
 end
