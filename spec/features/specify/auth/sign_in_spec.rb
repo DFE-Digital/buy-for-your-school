e0d6f64dd2963f9ab53a0aa92a3bac9679a800c3
hrmtl <hrishi.mittal@education.gov.uk>
2025-11-13 18:29:19 +0000

    Fix rubocop error

--- spec/features/specify/auth/sign_in_spec.rb
+++ spec/features/specify/auth/sign_in_spec.rb
@@ -1,9 +1,9 @@
 feature "DfE Sign-in" do
   context "with valid DfE Sign In credentials" do
     let(:user) { create(:user, :caseworker, first_name: "Generic", last_name: "User") }
-    let!(:agent) { Support::Agent.find_or_create_by_user(user).tap { |agent| agent.update!(roles: %w[procops]) } }
 
     before do
+      Support::Agent.find_or_create_by_user(user).tap { |agent| agent.update!(roles: %w[procops]) }
       user_exists_in_dfe_sign_in(user:)
       user_is_signed_in(user:)
       visit support_cases_path
