7e8da935a438ca4808a926ab472b0bb288be524a
Serena Abbott <serena.abbott@education.gov.uk>
2026-01-13 09:40:29 +0000

    Add smart meter installation text onto guidance page

--- spec/features/energy/guidance_page_spec.rb
+++ spec/features/energy/guidance_page_spec.rb
@@ -7,5 +7,6 @@ describe "Guidance page", :js do
     expect(page).to have_text("All state-funded schools and trusts in England can join Energy for Schools.")
     expect(page).to have_text("Individual schools, like local authority-maintained schools and single academy trusts, can join using the online service.")
     expect(page).to have_link("Register your interest form", href: "https://submit.forms.service.gov.uk/form/8895/multi-academy-trusts-register-your-interest-in-energy-for-schools/1049539")
+    expect(page).to have_text("Smart meter installation")
   end
 end
