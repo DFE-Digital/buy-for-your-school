a675c542b275dd11d6feb4e297f7c6494670fe71
hrmtl <hrishi.mittal@education.gov.uk>
2026-01-27 08:41:30 +0000

    Add FABS base controller and enhance search toggle controller

--- app/javascript/controllers/search_toggle_controller.js
+++ app/javascript/controllers/search_toggle_controller.js
@@ -1,22 +1,64 @@
 import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
-  static targets = ['openButton', 'closeButton']
+  static targets = ['openButton', 'closeButton', 'backgroundColor']
   static classes = ['hidden']
 
   connect () {
+    // Get the hidden class from data attribute (defaults to 'is-not-visible')
+    // data-search-toggle-hidden-class becomes searchToggleHiddenClass in dataset
+    this.hiddenClassName = this.element.dataset.searchToggleHiddenClass || 'is-not-visible'
+    this.backgroundClassName = this.element.dataset.searchToggleBackgroundClass || 'search-header-background'
+    
+    // Get the search box element
+    this.searchBox = document.getElementById('wrap-search')
+    this.searchArea = document.querySelector('.search-area')
+    
     // Ensure initial state
-    this.openButtonTarget.classList.remove(this.hiddenClass) // Make sure open button is visible
-    this.closeButtonTarget.classList.add(this.hiddenClass) // Make sure close button is hidden
+    if (this.openButtonTarget) {
+      this.openButtonTarget.classList.remove(this.hiddenClassName)
+    }
+    if (this.closeButtonTarget) {
+      this.closeButtonTarget.classList.add(this.hiddenClassName)
+    }
+    if (this.searchBox) {
+      this.searchBox.classList.add(this.hiddenClassName)
+    }
   }
 
   toggleOpen () {
-    this.openButtonTarget.classList.add(this.hiddenClass)
-    this.closeButtonTarget.classList.remove(this.hiddenClass)
+    if (this.openButtonTarget) {
+      this.openButtonTarget.classList.add(this.hiddenClassName)
+    }
+    if (this.closeButtonTarget) {
+      this.closeButtonTarget.classList.remove(this.hiddenClassName)
+    }
+    if (this.searchBox) {
+      this.searchBox.classList.remove(this.hiddenClassName)
+    }
+    if (this.searchArea) {
+      this.searchArea.classList.add(this.backgroundClassName)
+    }
+    if (this.hasBackgroundColorTarget) {
+      this.backgroundColorTarget.classList.add(this.backgroundClassName)
+    }
   }
 
   toggleClose () {
-    this.openButtonTarget.classList.remove(this.hiddenClass)
-    this.closeButtonTarget.classList.add(this.hiddenClass)
+    if (this.openButtonTarget) {
+      this.openButtonTarget.classList.remove(this.hiddenClassName)
+    }
+    if (this.closeButtonTarget) {
+      this.closeButtonTarget.classList.add(this.hiddenClassName)
+    }
+    if (this.searchBox) {
+      this.searchBox.classList.add(this.hiddenClassName)
+    }
+    if (this.searchArea) {
+      this.searchArea.classList.remove(this.backgroundClassName)
+    }
+    if (this.hasBackgroundColorTarget) {
+      this.backgroundColorTarget.classList.remove(this.backgroundClassName)
+    }
   }
 }
