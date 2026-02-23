3b8bde0f450fe3b28a805e7a33cdc76aa6f83f1c
hrmtl <hrishi.mittal@education.gov.uk>
2026-02-23 17:18:26 +0000

    Fix Rubocop error

--- config/initializers/i18n_backend_chain.rb
+++ config/initializers/i18n_backend_chain.rb
@@ -7,12 +7,10 @@ require Rails.root.join("lib/i18n/backend/ghbs_contentful")
 # Ensure we retain the default Simple backend (and therefore gem-provided locale
 # files such as Faker's) while layering on top the Contentful-driven keys that
 # were introduced with the FABS import.
-unless I18n.backend.is_a?(I18n::Backend::Chain)
-  if Rails.configuration.x.public_frontend_contentful_enabled
-    namespaces = Rails.configuration.x.public_frontend_translation_namespaces
-    contentful_backend = I18n::Backend::GhbsContentful.new(namespaces:)
-    fallback_backend = I18n.backend
+if !I18n.backend.is_a?(I18n::Backend::Chain) && Rails.configuration.x.public_frontend_contentful_enabled
+  namespaces = Rails.configuration.x.public_frontend_translation_namespaces
+  contentful_backend = I18n::Backend::GhbsContentful.new(namespaces:)
+  fallback_backend = I18n.backend
 
-    I18n.backend = I18n::Backend::Chain.new(contentful_backend, fallback_backend)
-  end
+  I18n.backend = I18n::Backend::Chain.new(contentful_backend, fallback_backend)
 end
