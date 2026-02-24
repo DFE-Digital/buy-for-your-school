339a4b1fd3cda6fe88662bbc7953a047e2f06dd0
hrmtl <hrishi.mittal@education.gov.uk>
2026-02-24 17:51:46 +0000

    Fix FABS breadcrumbs to work with loaf gem

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -10,7 +10,7 @@ class CategoriesController < ApplicationController
   end
 
   def show
-    add_breadcrumb :home_breadcrumb_name, :root_path
+    add_breadcrumb home_breadcrumb_name, root_path
 
     @category = FABS::Category.find_by_slug!(params[:slug])
     @subcategories = @category.subcategories
---
+++ app/controllers/fabs/pages_controller.rb
@@ -0,0 +1,45 @@
+class Fabs::PagesController < ApplicationController
+  skip_before_action :authenticate_user!
+
+  def show
+    @page = FABS::Page.find_by_slug!(params[:slug])
+    @page_title = @page.title
+    add_breadcrumb home_breadcrumb_name, home_breadcrumb_path
+    build_page_breadcrumbs(@page)
+
+    render layout: "fabs_application"
+  end
+
+private
+
+  def build_page_breadcrumbs(page)
+    node = page.parent
+    trail = []
+    max_depth = 4
+
+    max_depth.times do
+      break unless node
+
+      trail << node
+
+      case node
+      when FABS::Category
+        @category = node
+        break
+      when FABS::Page
+        node = node.parent
+      else
+        break
+      end
+    end
+
+    trail.reverse_each do |n|
+      case n
+      when FABS::Category
+        add_breadcrumb category_breadcrumb_name, category_breadcrumb_path
+      when FABS::Page
+        add_breadcrumb n.title, page_path(n.slug)
+      end
+    end
+  end
+end
