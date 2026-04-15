7674ca357142f2b71fdadab52a85007eaa817862
Andrew Retmanski <aretmanski@gmail.com>
2026-04-15 14:10:11 +0100

    Determine redirect host for DfE Authentication automatically from the incoming requests host header. This approach allows support of two different domain names during the forthcoming domain swap - and means we no longer need to explicitly set DFE_SIGN_IN_REDIRECT_URL env var. It's also safe to use this approach since we explicitly define allowed hostnames in `config.hosts` and permitted redirect paths in DfE Authentication service.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -5,12 +5,18 @@ OmniAuth.config.logger = Rails.logger
 dfe_sign_in_issuer_uri = Dsi::Uri.new(subdomain: "oidc").call
 dfe_sign_in_identifier = ENV.fetch("DFE_SIGN_IN_IDENTIFIER", "example")
 dfe_sign_in_secret = ENV.fetch("DFE_SIGN_IN_SECRET", "example")
-dfe_sign_in_redirect_uri = ENV.fetch("DFE_SIGN_IN_REDIRECT_URL", "example")
 
 dfe_sign_in_issuer_url = "#{dfe_sign_in_issuer_uri}:#{dfe_sign_in_issuer_uri.port}" if dfe_sign_in_issuer_uri.port
 
+set_dfe_sign_in_redirect_uri = lambda do |env|
+  request = Rack::Request.new(env)
+  strategy = env["omniauth.strategy"]
+  strategy.options.client_options.redirect_uri = "#{request.base_url}/auth/dfe/callback"
+end
+
 options = {
   name: :dfe,
+  setup: set_dfe_sign_in_redirect_uri,
   discovery: true,
   response_type: :code,
   issuer: dfe_sign_in_issuer_url,
@@ -22,7 +28,6 @@ options = {
     host: dfe_sign_in_issuer_uri.host,
     identifier: dfe_sign_in_identifier,
     secret: dfe_sign_in_secret,
-    redirect_uri: dfe_sign_in_redirect_uri,
   },
 }
 
