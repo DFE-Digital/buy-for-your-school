26d07f1282a52398216e84d83d0593f341a8bbd0
Andrew Retmanski <aretmanski@gmail.com>
2026-04-21 11:10:11 +0100

    Omit front end images from asset manifest too This fixes `assets:precompile` task.

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -8,6 +8,3 @@
 //= link mpan-half-hourly.png
 //= link department-for-education_white.png
 //= link department-for-education_black.png
-
-//= link_tree ../images/govuk-frontend
-//= link_tree ../images/dfe-frontend
