f2828093c6aa54ca1102df8809a8ad1ba31dfb3f
Andrew Retmanski <aretmanski@gmail.com>
2026-05-11 10:59:35 +0100

    Click first case link on page 2 rather than assuming a fixed case ref

--- spec/features/support/case_navigation_spec.rb
+++ spec/features/support/case_navigation_spec.rb
@@ -3,7 +3,6 @@ RSpec.feature "Case list navigation", :js do
 
   let(:category) { create(:support_category, :fixed_title) }
   let!(:support_cases) { create_list(:support_case, 13, category:) }
-  let(:target_case) { support_cases.first }
   let(:newest_case) { support_cases.last }
 
   before do
@@ -16,14 +15,14 @@ RSpec.feature "Case list navigation", :js do
         click_on "All cases"
         within "#all-cases" do
           check "Example Category"
+          find(".pagination__results", text: "Showing 1 to 10 of 13 results")
           click_on "Next"
         end
 
         within "#all-cases" do
-          find_link(target_case.ref)
           find("em.current", text: "2")
-          find("*", text: "Showing 11 to 13 of 13 results")
-          click_on target_case.ref
+          find(".pagination__results", text: "Showing 11 to 13 of 13 results")
+          first("a.govuk-link--no-visited-state", text: /^\d{6}$/).click
         end
       end
 
@@ -32,7 +31,7 @@ RSpec.feature "Case list navigation", :js do
         within "#all-cases" do
           expect(page).to have_checked_field "Example Category"
           expect(page).to have_selector "em", text: "2", class: "current"
-          expect(page).to have_text "Showing 11 to 13 of 13 results"
+          expect(page).to have_selector ".pagination__results", text: "Showing 11 to 13 of 13 results"
         end
       end
     end
