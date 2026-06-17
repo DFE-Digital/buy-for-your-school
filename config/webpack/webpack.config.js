9225f8051a04e44fb9f30fdd6b11b5945c21e94c
Andrew Retmanski <aretmanski@gmail.com>
2026-06-17 14:08:16 +0100

    Include govuk_publishing_components gem javascript in Webpack build path The gem isn't available as a node module and assumes use of sprockets to build the application javascript, so we need to point webpack at the js files within the installed gem instead.

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -1,7 +1,14 @@
 const path    = require("path")
 const webpack = require("webpack")
+const { execFileSync } = require("child_process")
 
 const mode = process.env.NODE_ENV === 'development' ? 'development' : 'production';
+const govukPublishingComponentsPath = execFileSync("bundle", [
+  "exec",
+  "ruby",
+  "-e",
+  "print Gem::Specification.find_by_name('govuk_publishing_components').gem_dir"
+], { encoding: "utf8" });
 
 module.exports = {
   mode,
@@ -15,6 +22,14 @@ module.exports = {
     filename: "[name].js",
     sourceMapFilename: "[file].map",
     path: path.resolve(__dirname, '..', '..', 'app/assets/builds'),
+  },
+  resolve: {
+    alias: {
+      govuk_publishing_components: path.join(
+        govukPublishingComponentsPath,
+        "app/assets/javascripts/govuk_publishing_components"
+      ),
+    },
   },
   plugins: [
     new webpack.optimize.LimitChunkCountPlugin({
@@ -31,3 +46,5 @@ module.exports = {
     ]
   },
 }
+
+
