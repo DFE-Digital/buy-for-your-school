27024263e547d48111f99b8c87c41fa4822542b0
Andrew Retmanski <aretmanski@gmail.com>
2026-07-09 09:07:21 +0100

    Update example ENV vars file and fix default value for semantic config

--- app/services/azure_ai_search/solution_index_schema.rb
+++ app/services/azure_ai_search/solution_index_schema.rb
@@ -1,7 +1,7 @@
 module AzureAiSearch
   class SolutionIndexSchema
     INDEX = ENV.fetch("AZURE_AI_SEARCH_INDEX_NAME", "solution-data")
-    SEMANTIC_CONFIGURATION = ENV.fetch("AZURE_AI_SEARCH_SEMANTIC_CONFIGURATION", "solution-semantic-config")
+    SEMANTIC_CONFIGURATION = ENV.fetch("AZURE_AI_SEARCH_SEMANTIC_CONFIGURATION", "solutions-v1")
     ENGLISH_ANALYZER = "en.microsoft".freeze
 
     def self.to_h
