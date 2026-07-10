451e6d2b10572f6f8c468202e5dd1d721d7a93bb
Andrew Retmanski <aretmanski@gmail.com>
2026-07-10 12:45:02 +0100

    Modify Azure search indexer to explicitly remove any stale solutions that are no longer in Contentful This is a key weakness of the existing Elasticsearch indexing; that relies solely on the webhook from Contentful to remove unpublished content. Whilst this should work it's not entirely guaranteed so this approach provides a backup and is cleaner than simply deleting and recreating the index (so search remains available whilst the task is running).

--- app/services/azure_ai_search/solution_indexer.rb
+++ app/services/azure_ai_search/solution_indexer.rb
@@ -20,6 +20,20 @@ module AzureAiSearch
       responses.compact
     end
 
+    def sync_all(solutions = Solution.all)
+      contentful_solutions = solutions.select(&:presentable?)
+      current_ids = contentful_solutions.map(&:id)
+      stale_ids = existing_document_ids - current_ids
+
+      deleted_responses = delete_documents(stale_ids)
+      indexed_responses = index_all(contentful_solutions)
+
+      {
+        deleted: deleted_responses,
+        indexed: indexed_responses,
+      }
+    end
+
     def index_document(id)
       solution = Solution.find_by_id!(id)
       return false unless solution&.presentable?
@@ -39,6 +53,26 @@ module AzureAiSearch
       )
 
       successful_response?(response)
+    end
+
+    def delete_documents(ids)
+      ids.each_slice(BATCH_SIZE).map do |batch|
+        client.index_documents(
+          index_name: INDEX,
+          documents: batch.map { |id| SolutionDocument.delete(id) },
+        )
+      end.compact
+    end
+
+    def existing_document_ids
+      client.search(
+        index_name: INDEX,
+        body: {
+          search: "*",
+          select: "id",
+          top: 1000,
+        },
+      )["value"].map { |document| document["id"] }
     end
 
   private
