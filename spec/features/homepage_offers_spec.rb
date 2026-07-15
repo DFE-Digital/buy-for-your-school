e4197a537ca05aefe7a82074e7e96ab39b7edaa6
Andrew Retmanski <aretmanski@gmail.com>
2026-07-15 18:09:14 +0100

    Refactor Homepage Offers feature spec to use stubbed models rather than VCR recordings

--- spec/features/homepage_offers_spec.rb
+++ spec/features/homepage_offers_spec.rb
@@ -1,6 +1,22 @@
 require "rails_helper"
 
-RSpec.describe "Homepage Offers Section", :vcr, type: :feature do
+RSpec.describe "Homepage Offers Section", type: :feature do
+  let(:featured_offer) do
+    instance_double(
+      Offer,
+      title: "Featured offer",
+      description: "Featured offer description",
+      url: "https://example.com/featured-offer",
+      sort_order: 1,
+    )
+  end
+
+  before do
+    allow(FABS::Category).to receive(:all).and_return([])
+    allow(Offer).to receive(:featured_offers).and_return([featured_offer])
+    allow(Banner).to receive(:find_by_slug).and_return(nil)
+  end
+
   context "when visiting the homepage" do
     before do
       visit root_path
