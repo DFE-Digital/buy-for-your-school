6c5312cb625f432120cba9cf6897bfa1368516b9
Andrew Retmanski <aretmanski@gmail.com>
2026-07-16 12:27:55 +0100

    Fix remaining specs that invoke Contentful API calls

--- spec/features/specify/cookie_preferences/visitor_sees_cookie_banner_spec.rb
+++ spec/features/specify/cookie_preferences/visitor_sees_cookie_banner_spec.rb
@@ -1,6 +1,12 @@
 require "rails_helper"
 
 describe "Visitor to GHBS service sees cookie banner", :js do
+  before do
+    allow(FABS::Category).to receive(:all).and_return([])
+    allow(Offer).to receive(:featured_offers).and_return([])
+    allow(Banner).to receive(:find_by_slug).and_return(nil)
+  end
+
   scenario "Visitor accepting cookie notice can be tracked by GA" do
     Given :"I am a new visitor to the GHBS service"
     When :"I accept the tracking of cookies"
