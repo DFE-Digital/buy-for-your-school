2735c323a3ff00fb0b21bb42f2b06955d887dbab
Andrew Retmanski <aretmanski@gmail.com>
2026-07-17 16:29:12 +0100

    Fix breadcrumbs spec This was previously checking breadcrumbs on a GHBS rather than FABS page, so updated to use mock Contentful models.

--- spec/features/pages/breadcrumbs_spec.rb
+++ spec/features/pages/breadcrumbs_spec.rb
@@ -1,17 +1,58 @@
 RSpec.feature "Page with breadcrumbs" do
+  def contentful_entry(id:, fields:, content_type:)
+    double(
+      "Contentful::Entry",
+      id:,
+      fields:,
+      content_type: double("Contentful::ContentType", id: content_type),
+    )
+  end
+
+  let(:parent_page_entry) do
+    contentful_entry(
+      id: "page-parent",
+      content_type: "page",
+      fields: {
+        title: "Test Page 1",
+        description: "Parent page",
+        body: "Parent body",
+        slug: "test-page-1",
+        parent: nil,
+        related_content: [],
+      },
+    )
+  end
+
+  let(:our_page_entry) do
+    contentful_entry(
+      id: "page-child",
+      content_type: "page",
+      fields: {
+        title: "Test Page 2",
+        description: "Child page",
+        body: "Child body",
+        slug: "test-page-2",
+        parent: parent_page_entry,
+        related_content: [],
+      },
+    )
+  end
+
   before do
-    create(:page, title: "Test Page 1", slug: "test-page-1")
-    create(:page, title: "Test Page 2", slug: "test-page-2", breadcrumbs: ["Test Page 1,test-page-1", "Test Page 2,test-page-2"])
-    visit "/test-page-2"
+    allow(FABS::Page).to receive(:find_by_slug!).with("test-page-1").and_return(FABS::Page.new(parent_page_entry))
+    allow(FABS::Page).to receive(:find_by_slug!).with("test-page-2").and_return(FABS::Page.new(our_page_entry))
   end
 
   it "shows the breadcrumbs" do
-    expect(page).to have_breadcrumbs ["Test Page 1", "Test Page 2"]
+    visit "/test-page-2"
+    expect(page).to have_breadcrumbs ["Home", "Test Page 1"]
   end
 
   it "takes user to the right breadcrumb page" do
+    visit "/test-page-2"
+
     click_link "Test Page 1"
     expect(page).to have_current_path "/test-page-1"
-    expect(page).not_to have_breadcrumbs
+    expect(page).to have_breadcrumbs %w[Home]
   end
 end
