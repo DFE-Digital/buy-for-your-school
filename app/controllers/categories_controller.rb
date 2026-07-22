3c6303346ee0c8478c408df27d88fcea4b646e12
Andrew Retmanski <aretmanski@gmail.com>
2026-07-22 12:42:11 +0100

    Remove unused logic to hide search

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,4 @@
 class CategoriesController < Fabs::ApplicationController
-  before_action :disable_search_in_header, only: :index
   include Redirectable
 
   before_action :redirect_legacy_slugs, only: :show
@@ -62,9 +61,5 @@ private
 
   def ways_to_buy
     category.solutions.map(&:ways_to_buy).compact.map { |entry| WaysToBuy.new(entry) }.sort_by(&:title).uniq(&:title)
-  end
-
-  def disable_search_in_header
-    @show_search_in_header = false
   end
 end
