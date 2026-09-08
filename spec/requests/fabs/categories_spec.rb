65ee91146904296ccb9daeb0b82a5993d7266446
Andrew Retmanski <aretmanski@gmail.com>
2026-09-08 13:47:24 +0100

    Fix failing spec after rebase

--- spec/requests/fabs/categories_spec.rb
+++ spec/requests/fabs/categories_spec.rb
@@ -106,8 +106,6 @@ RSpec.describe "Categories pages", type: :request do
         body_description: nil,
         banner: nil,
         subcategories: [],
-        solutions: [],
-        filtered_solutions: [],
         seo_description: "ICT business systems seo description",
       )
     end
@@ -157,7 +155,7 @@ RSpec.describe "Categories pages", type: :request do
     )
   end
 
-  def build_category(title:, description:, slug:, body_title: nil, body_description: nil, banner: nil, subcategories: [])
+  def build_category(title:, description:, slug:, body_title: nil, body_description: nil, banner: nil, subcategories: [], seo_description: nil)
     FABS::Category.new(
       OpenStruct.new(
         id: slug,
@@ -169,6 +167,7 @@ RSpec.describe "Categories pages", type: :request do
           body_description:,
           banner:,
           subcategories:,
+          seo_description:,
         },
       ),
     )
