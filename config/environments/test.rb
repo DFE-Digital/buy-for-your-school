eaad246801c30951f7a713a6d6109cfc64474077
Andrew Retmanski <aretmanski@gmail.com>
2026-09-11 16:34:44 +0100

    Tweak config for exception templates in test mode This is required for some of the specs to pass e.g. `spec/requests/favicon_spec.rb`

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -24,8 +24,8 @@ Rails.application.configure do
   config.consider_all_requests_local = true
   config.cache_store = :null_store
 
-  # Render exception templates for rescuable exceptions and raise for other exceptions.
-  config.action_dispatch.show_exceptions = :rescuable
+  # Raise exceptions instead of rendering exception templates.
+  config.action_dispatch.show_exceptions = :none
 
   # Disable request forgery protection in test environment.
   config.action_controller.allow_forgery_protection = false
